6820d746a16077ccad13a2693f33d37609509474
msrocka <michael.srocka@gmail.com>
2013-05-21 12:26:09 +0200

    comment updated in model navigation element

--- olca-app-core/src/org/openlca/core/application/navigation/ModelNavigationElement.java
+++ olca-app-core/src/org/openlca/core/application/navigation/ModelNavigationElement.java
@@ -1,22 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2007 - 2010 GreenDeltaTC. All rights reserved. This program and
- * the accompanying materials are made available under the terms of the Mozilla
- * Public License v1.1 which accompanies this distribution, and is available at
- * http://www.openlca.org/uploads/media/MPL-1.1.html
- * 
- * Contributors: GreenDeltaTC - initial API and implementation
- * www.greendeltatc.com tel.: +49 30 4849 6030 mail: gdtc@greendeltatc.com
- ******************************************************************************/
 package org.openlca.core.application.navigation;
 
 import org.openlca.core.model.modelprovider.IModelComponent;
 
 /**
- * Extension of the {@link AbstractNavigationElement} for a model component in
- * the navigator
- * 
- * @author Sebastian Greve
- * 
+ * Content of this navigation element is a reference to a model (Process, Flow,
+ * Unit group etc.).
  */
 public class ModelNavigationElement extends AbstractNavigationElement {
 
