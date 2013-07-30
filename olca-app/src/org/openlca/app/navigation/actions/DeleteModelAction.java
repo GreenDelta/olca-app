4cfaaa18add50afacc2a9cc0790a0603cb6f34e0
Sebastian Greve <greve@greendelta.com>
2013-07-30 19:00:03 +0200

    Created delete model action

---
+++ olca-app/src/org/openlca/app/navigation/actions/DeleteModelAction.java
@@ -0,0 +1,22 @@
+package org.openlca.app.navigation.actions;
+
+import java.util.List;
+
+import org.eclipse.jface.action.Action;
+import org.openlca.app.navigation.INavigationElement;
+
+public class DeleteModelAction extends Action implements INavigationAction {
+
+	@Override
+	public boolean accept(INavigationElement<?> element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean accept(List<INavigationElement<?>> elements) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
