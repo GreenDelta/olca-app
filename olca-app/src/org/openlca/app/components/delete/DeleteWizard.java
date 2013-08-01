fab85f1fceea29297b550cd9250c61012f6a8d87
Sebastian Greve <greve@greendelta.com>
2013-08-01 19:27:38 +0200

    Moved Delete & Problem Wizard

---
+++ olca-app/src/org/openlca/app/components/delete/DeleteWizard.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2007 - 2010 GreenDeltaTC. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Mozilla
+ * Public License v1.1 which accompanies this distribution, and is available at
+ * http://www.openlca.org/uploads/media/MPL-1.1.html
+ * 
+ * Contributors: GreenDeltaTC - initial API and implementation
+ * www.greendeltatc.com tel.: +49 30 4849 6030 mail: gdtc@greendeltatc.com
+ ******************************************************************************/
+package org.openlca.app.components.delete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.PlatformUI;
+import org.openlca.app.Messages;
+import org.openlca.app.components.JobListenerWithProgress;
+import org.openlca.core.database.usage.IUseSearch;
+import org.openlca.core.jobs.JobHandler;
+import org.openlca.core.jobs.Jobs;
+import org.openlca.core.model.RootEntity;
+import org.openlca.core.model.descriptors.BaseDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Wizard for displaying warnings and errors while deleting an object
+ * 
+ * @see ProblemWizard
+ * 
+ * @author Sebastian Greve
+ * 
+ */
+public class DeleteWizard<T extends RootEntity> extends ProblemWizard {
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * Creates a new delete wizard for the given object and problem handler
+	 * 
+	 */
+	public DeleteWizard(IUseSearch<T> search, T model) {
+		initializeProblems(search, model);
+		setNeedsProgressMonitor(true);
+		setWindowTitle(Messages.DeleteWizard_WindowTitle);
+	}
+
+	/**
+	 * Initializes the problems
+	 */
+	private void initializeProblems(final IUseSearch<T> search, final T model) {
+		if (search != null) {
+			try {
+				// initialize problems
+				PlatformUI.getWorkbench().getProgressService()
+						.busyCursorWhile(new JobListenerWithProgress() {
+
+							@Override
+							public void run() {
+								// initialize job handler
+								final JobHandler handler = Jobs
+										.getHandler(Jobs.MAIN_JOB_HANDLER);
+								handler.addJobListener(this);
+								handler.startJob(
+										Messages.DeleteWizard_Analyzing,
+										IProgressMonitor.UNKNOWN);
+								final List<Problem> problems = new ArrayList<>();
+								// for each found reference
+								for (final BaseDescriptor descriptor : search
+										.findUses(model)) {
+									// create problem
+									String used = model.getClass()
+											.getSimpleName()
+											+ " "
+											+ model.getName();
+									String user = descriptor.getModelType()
+											.getModelClass().getSimpleName()
+											+ " " + descriptor.getName();
+									String message = used + " is used by "
+											+ user;
+
+									final Problem problem = new Problem(
+											Problem.ERROR, message) {
+
+										@Override
+										public void solve() {
+
+										}
+									};
+									problems.add(problem);
+								}
+								setProblems(problems
+										.toArray(new Problem[problems.size()]));
+								handler.done();
+								handler.removeJobListener(this);
+							}
+						});
+			} catch (final Exception e) {
+				log.error("Initialize problems failed", e);
+			}
+		}
+	}
+}
