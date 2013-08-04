a792abfad837ecec21524ff3d8fa10e1939366e5
Sebastian Greve <greve@greendelta.com>
2013-08-04 20:18:23 +0200

    Added new project editor

---
+++ olca-app/src/org/openlca/app/editors/ProjectEditor.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2007 - 2010 GreenDeltaTC. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Mozilla
+ * Public License v1.1 which accompanies this distribution, and is available at
+ * http://www.openlca.org/uploads/media/MPL-1.1.html
+ * 
+ * Contributors: GreenDeltaTC - initial API and implementation
+ * www.greendeltatc.com tel.: +49 30 4849 6030 mail: gdtc@greendeltatc.com
+ ******************************************************************************/
+package org.openlca.app.editors;
+
+import org.openlca.core.editors.IEditor;
+import org.openlca.core.model.Project;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProjectEditor extends ModelEditor<Project> implements IEditor {
+
+	public static String ID = "editors.project";
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	public ProjectEditor() {
+		super(Project.class);
+	}
+
+	@Override
+	protected void addPages() {
+		try {
+			addPage(new ProjectInfoPage(this));
+		} catch (Exception e) {
+			log.error("failed to add page", e);
+		}
+	}
+
+}
---
+++ olca-app/src/org/openlca/app/editors/ProjectInfoPage.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2007 - 2010 GreenDeltaTC. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Mozilla
+ * Public License v1.1 which accompanies this distribution, and is available at
+ * http://www.openlca.org/uploads/media/MPL-1.1.html
+ * 
+ * Contributors: GreenDeltaTC - initial API and implementation
+ * www.greendeltatc.com tel.: +49 30 4849 6030 mail: gdtc@greendeltatc.com
+ ******************************************************************************/
+package org.openlca.app.editors;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
+import org.openlca.app.Messages;
+import org.openlca.app.db.Database;
+import org.openlca.app.util.UI;
+import org.openlca.app.viewers.table.ProductSystemViewer;
+import org.openlca.core.model.ModelType;
+import org.openlca.core.model.Project;
+
+class ProjectInfoPage extends ModelPage<Project> {
+
+	private FormToolkit toolkit;
+
+	ProjectInfoPage(ProjectEditor editor) {
+		super(editor, "ProjectInfoPage", Messages.GeneralInformation);
+	}
+
+	@Override
+	protected void createFormContent(IManagedForm managedForm) {
+		ScrolledForm form = UI.formHeader(managedForm, Messages.Project + ": "
+				+ getModel().getName());
+		toolkit = managedForm.getToolkit();
+		Composite body = UI.formBody(form, toolkit);
+		InfoSection infoSection = new InfoSection(getModel(), getBinding());
+		infoSection.render(body, toolkit);
+
+		createGoalAndScopeSection(body);
+		createTimeInfoSection(body);
+		createProductSystemSection(body);
+
+		body.setFocus();
+		form.reflow(true);
+	}
+
+	private void createGoalAndScopeSection(Composite parent) {
+		Composite composite = UI.formSection(parent, toolkit,
+				Messages.GoalAndScopeInfoSectionLabel);
+
+		createMultiText(Messages.Goal, "goal", composite);
+		createMultiText(Messages.FunctionalUnit, "functionalUnit", composite);
+	}
+
+	private void createTimeInfoSection(Composite parent) {
+		Composite composite = UI.formSection(parent, toolkit,
+				Messages.ProjectInfoSectionLabel);
+
+		createDate(Messages.CreationDate, "creationDate", composite);
+		createDate(Messages.LastModificationDate, "lastModificationDate",
+				composite);
+		createDropComponent(Messages.Author, "author", ModelType.ACTOR,
+				composite);
+	}
+
+	private void createProductSystemSection(Composite parent) {
+		Section section = UI.section(parent, toolkit,
+				Messages.ProductSystemsInfoSectionLabel);
+		UI.gridData(section, true, true);
+		Composite client = UI.sectionClient(section, toolkit);
+
+		ProductSystemViewer viewer = new ProductSystemViewer(client,
+				Database.get());
+		viewer.bindTo(section);
+		getBinding().on(getModel(), "productSystems", viewer);
+	}
+
+}
