7e04991a8626a0b89f2ab04d40cc613708c44021
msrocka <michael.srocka@gmail.com>
2013-08-15 11:07:47 +0200

    removed projects folders that are not needed anymore

--- olca-app-core/src/org/openlca/core/application/update/PreUpdateHook.java
+++
@@ -1,8 +0,0 @@
-package org.openlca.core.application.update;
-
-import org.openlca.updater.Updater;
-
-public interface PreUpdateHook {
-
-	void customizeUpdater(Updater updater, VersionInfo newAppVersionInfo);
-}
--- olca-app-core/src/org/openlca/ui/viewer/ISelectionChangedListener.java
+++
@@ -1,7 +0,0 @@
-package org.openlca.ui.viewer;
-
-public interface ISelectionChangedListener<T> {
-
-	public void selectionChanged(T selection);
-
-}
