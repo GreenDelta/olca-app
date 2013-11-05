28d92d6349d0df90f495847f03763510715e5596
msrocka <michael.srocka@gmail.com>
2013-11-05 16:08:38 +0100

    added column sorting for process contributions

--- olca-app/src/org/openlca/app/analysis/ProcessContributionViewer.java
+++ olca-app/src/org/openlca/app/analysis/ProcessContributionViewer.java
@@ -7,7 +7,10 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.openlca.app.Messages;
+import org.openlca.app.util.TableColumnSorter;
 import org.openlca.app.util.Tables;
+
+import com.google.common.primitives.Doubles;
 
 /** Table viewer for process contributions. */
 class ProcessContributionViewer extends TableViewer {
@@ -27,9 +30,8 @@ class ProcessContributionViewer extends TableViewer {
 	/** Index of the column with the units. */
 	static final int UNIT = 4;
 
-	private String[] columnLabels = { Messages.Contribution,
-			Messages.Process, Messages.TotalAmount,
-			Messages.SingleAmount, Messages.Unit };
+	private String[] columnLabels = { Messages.Contribution, Messages.Process,
+			Messages.TotalAmount, Messages.SingleAmount, Messages.Unit };
 
 	private double[] columnWidths = { 0.17, 0.37, 0.17, 0.17, 0.10 };
 
@@ -40,7 +42,9 @@ class ProcessContributionViewer extends TableViewer {
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
 		setContentProvider(ArrayContentProvider.getInstance());
-		setLabelProvider(new ProcessContributionLabel());
+		ProcessContributionLabel label = new ProcessContributionLabel();
+		setLabelProvider(label);
+		createColumnSorters(label);
 		Tables.bindColumnWidths(table, columnWidths);
 	}
 
@@ -50,6 +54,49 @@ class ProcessContributionViewer extends TableViewer {
 			column.getColumn().setResizable(true);
 			column.getColumn().setMoveable(true);
 			column.getColumn().setText(columnLabels[i]);
+		}
+	}
+
+	private void createColumnSorters(ProcessContributionLabel p) {
+		TableColumnSorter<?> first = new AmountSorter(CONTRIBUTION);
+		first.setAscending(false);
+		// @formatter:off
+		Tables.registerSorters(
+				this,
+				first,
+				new TableColumnSorter<>(ProcessContributionItem.class, NAME, p),
+				new AmountSorter(TOTAL_AMOUNT),
+				new AmountSorter(SINGLE_AMOUNT), 
+				new TableColumnSorter<>(ProcessContributionItem.class, UNIT, p));
+		// @formatter:on
+	}
+
+	private class AmountSorter extends
+			TableColumnSorter<ProcessContributionItem> {
+
+		public AmountSorter(int column) {
+			super(ProcessContributionItem.class, column);
+		}
+
+		@Override
+		public int compare(ProcessContributionItem item1,
+				ProcessContributionItem item2) {
+			double val1 = getVal(item1);
+			double val2 = getVal(item2);
+			return Doubles.compare(val1, val2);
+		}
+
+		private double getVal(ProcessContributionItem item) {
+			switch (getColumn()) {
+			case CONTRIBUTION:
+				return item.getContribution();
+			case SINGLE_AMOUNT:
+				return item.getSingleAmount();
+			case TOTAL_AMOUNT:
+				return item.getTotalAmount();
+			default:
+				return 0;
+			}
 		}
 	}
 
