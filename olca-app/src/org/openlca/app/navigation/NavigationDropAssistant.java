aed7298003d81b76afbd89f37ef925b837a97047
msrocka <michael.srocka@gmail.com>
2013-11-22 10:50:59 +0100

    fixed out-of-sync bug in category tree when elements are dragged and dropped

--- olca-app/src/org/openlca/app/navigation/NavigationDropAssistant.java
+++ olca-app/src/org/openlca/app/navigation/NavigationDropAssistant.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2007 - 2010 GreenDeltaTC. All rights reserved. This program and
- * the accompanying materials are made available under the terms of the Mozilla
- * Public License v1.1 which accompanies this distribution, and is available at
- * http://www.openlca.org/uploads/media/MPL-1.1.html
- * 
- * Contributors: GreenDeltaTC - initial API and implementation
- * www.greendeltatc.com tel.: +49 30 4849 6030 mail: gdtc@greendeltatc.com
- ******************************************************************************/
 package org.openlca.app.navigation;
 
 import java.util.ArrayList;
@@ -25,18 +16,14 @@ import org.eclipse.ui.navigator.CommonDropAdapterAssistant;
 import org.openlca.app.RcpActivator;
 import org.openlca.app.util.CopyPaste;
 
+import com.google.common.base.Objects;
+
 /**
  * Extension of the {@link CommonDropAdapterAssistant} to support drop
  * assistance for the common viewer of the applications navigator
  */
 
 public class NavigationDropAssistant extends CommonDropAdapterAssistant {
-
-	private int operation;
-
-	public NavigationDropAssistant() {
-		// nothing to initialize
-	}
 
 	@Override
 	public IStatus handleDrop(CommonDropAdapter dropAdapter,
@@ -45,23 +32,26 @@ public class NavigationDropAssistant extends CommonDropAdapterAssistant {
 				.getActiveWorkbenchWindow().getActivePage()
 				.findView(Navigator.ID);
 		DropTarget dropTarget = (DropTarget) dropTargetEvent.getSource();
-
 		INavigationElement<?> targetElement = (INavigationElement<?>) target;
-		if (dropTarget.getControl() == navigator.getCommonViewer().getTree()) {
-			List<INavigationElement<?>> elements = new ArrayList<>();
-			IStructuredSelection selection = (IStructuredSelection) dropTargetEvent.data;
-			for (Object o : selection.toArray())
-				if (o instanceof ModelElement || o instanceof CategoryElement)
-					elements.add((INavigationElement<?>) o);
-			if (CopyPaste.canMove(elements, targetElement)) {
-				if (operation == DND.DROP_COPY)
-					CopyPaste.copy(elements);
-				else
-					CopyPaste.cut(elements);
-				CopyPaste.pasteTo(targetElement);
-			}
-		}
+		if (dropTarget.getControl() == navigator.getCommonViewer().getTree())
+			doIt(dropTargetEvent, targetElement);
 		return null;
+	}
+
+	private void doIt(DropTargetEvent event, INavigationElement<?> targetElement) {
+		List<INavigationElement<?>> elements = new ArrayList<>();
+		IStructuredSelection selection = (IStructuredSelection) event.data;
+		for (Object o : selection.toArray())
+			if ((o instanceof ModelElement || o instanceof CategoryElement)
+					&& !Objects.equal(o, targetElement))
+				elements.add((INavigationElement<?>) o);
+		if (CopyPaste.canMove(elements, targetElement)) {
+			if ((event.detail & DND.DROP_COPY) == DND.DROP_COPY)
+				CopyPaste.copy(elements);
+			else
+				CopyPaste.cut(elements);
+			CopyPaste.pasteTo(targetElement);
+		}
 	}
 
 	@Override
@@ -72,9 +62,7 @@ public class NavigationDropAssistant extends CommonDropAdapterAssistant {
 	@Override
 	public IStatus validateDrop(Object target, int operation,
 			TransferData transferType) {
-		this.operation = operation;
 		IStatus status = null;
-
 		if (target instanceof CategoryElement
 				|| target instanceof ModelTypeElement)
 			status = new Status(IStatus.OK, RcpActivator.PLUGIN_ID, "");
