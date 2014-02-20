0817c56ad4174e2f3692dea93663f095c0a9fbad
msrocka <michael.srocka@gmail.com>
2014-02-20 14:41:03 +0100

    added parameter tables to LCIA editor

--- olca-app/src/org/openlca/app/editors/lcia_methods/ImpactMethodEditor.java
+++ olca-app/src/org/openlca/app/editors/lcia_methods/ImpactMethodEditor.java
@@ -1,11 +1,16 @@
 package org.openlca.app.editors.lcia_methods;
 
-import com.google.common.eventbus.EventBus;
-import org.openlca.app.Event;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
 import org.openlca.app.FeatureFlag;
 import org.openlca.app.editors.IEditor;
 import org.openlca.app.editors.ModelEditor;
+import org.openlca.app.editors.ParameterPage;
+import org.openlca.app.editors.ParameterPageListener;
+import org.openlca.app.editors.ParameterPageSupport;
 import org.openlca.core.model.ImpactMethod;
+import org.openlca.core.model.ParameterScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,18 +30,28 @@ public class ImpactMethodEditor extends ModelEditor<ImpactMethod> implements
 	final String IMPACT_FACTOR_CHANGE = "IMPACT_FACTOR_CHANGE";
 
 	private Logger log = LoggerFactory.getLogger(getClass());
-	private EventBus eventBus = new EventBus();
+	private ParameterPageSupport parameterSupport;
 
 	public ImpactMethodEditor() {
 		super(ImpactMethod.class);
 	}
 
-	public EventBus getEventBus() {
-		return eventBus;
-	}
-
-	public void postEvent(String message, Object source) {
-		eventBus.post(new Event(message, source));
+	@Override
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		super.init(site, input);
+		parameterSupport = new ParameterPageSupport(this, getModel()
+				.getParameters(), ParameterScope.IMPACT_METHOD);
+		// it is important that this listener is added before the listener
+		// in the LCIA factor page, otherwise the factor table will be
+		// refreshed with old values
+		parameterSupport.addListener(new ParameterPageListener() {
+			@Override
+			public void parameterChanged() {
+				log.trace("evaluate LCIA factor formulas");
+				// TODO: evaluate LCIA factor formulas
+			}
+		});
 	}
 
 	@Override
@@ -45,6 +60,7 @@ public class ImpactMethodEditor extends ModelEditor<ImpactMethod> implements
 			addPage(new ImpactMethodInfoPage(this));
 			addPage(new ImpactFactorsPage(this));
 			addPage(new ImpactNwPage(this));
+			addPage(new ParameterPage(parameterSupport));
 			if (FeatureFlag.LOCALISED_LCIA.isEnabled()) {
 				addPage(new ShapeFilePage(this));
 				addPage(new ImpactLocalisationPage(this));
