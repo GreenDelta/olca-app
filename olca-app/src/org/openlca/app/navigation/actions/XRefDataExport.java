349b0d9a27d7af33436ced609d2ac2fb185143f5
msrocka <michael.srocka@gmail.com>
2014-02-25 16:49:12 +0100

    added actions for import and export of reference data in CSV format

---
+++ olca-app/src/org/openlca/app/navigation/actions/XRefDataExport.java
@@ -0,0 +1,44 @@
+package org.openlca.app.navigation.actions;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.jface.action.Action;
+import org.openlca.app.App;
+import org.openlca.app.components.FileChooser;
+import org.openlca.app.db.Database;
+import org.openlca.app.navigation.DatabaseElement;
+import org.openlca.app.navigation.INavigationElement;
+import org.openlca.app.resources.ImageType;
+import org.openlca.io.refdata.RefDataExport;
+
+public class XRefDataExport extends Action implements INavigationAction {
+
+	public XRefDataExport() {
+		setImageDescriptor(ImageType.EXTENSION_ICON.getDescriptor());
+		setText("Export reference data");
+	}
+
+	@Override
+	public boolean accept(INavigationElement<?> element) {
+		if (!(element instanceof DatabaseElement))
+			return false;
+		DatabaseElement e = (DatabaseElement) element;
+		return Database.isActive(e.getContent());
+	}
+
+	@Override
+	public boolean accept(List<INavigationElement<?>> elements) {
+		return false;
+	}
+
+	@Override
+	public void run() {
+		File dir = FileChooser.forExport(FileChooser.DIRECTORY_DIALOG);
+		if (dir == null)
+			return;
+		RefDataExport export = new RefDataExport(dir, Database.get());
+		App.run("Export reference data", export);
+	}
+
+}
---
+++ olca-app/src/org/openlca/app/navigation/actions/XRefDataImport.java
@@ -0,0 +1,53 @@
+package org.openlca.app.navigation.actions;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.jface.action.Action;
+import org.openlca.app.App;
+import org.openlca.app.components.FileChooser;
+import org.openlca.app.db.Cache;
+import org.openlca.app.db.Database;
+import org.openlca.app.navigation.DatabaseElement;
+import org.openlca.app.navigation.INavigationElement;
+import org.openlca.app.navigation.Navigator;
+import org.openlca.app.resources.ImageType;
+import org.openlca.io.refdata.RefDataImport;
+
+public class XRefDataImport extends Action implements INavigationAction {
+
+	public XRefDataImport() {
+		setImageDescriptor(ImageType.EXTENSION_ICON.getDescriptor());
+		setText("Import reference data");
+	}
+
+	@Override
+	public boolean accept(INavigationElement<?> element) {
+		if (!(element instanceof DatabaseElement))
+			return false;
+		DatabaseElement e = (DatabaseElement) element;
+		return Database.isActive(e.getContent());
+	}
+
+	@Override
+	public boolean accept(List<INavigationElement<?>> elements) {
+		return false;
+	}
+
+	@Override
+	public void run() {
+		File dir = FileChooser.forImport(FileChooser.DIRECTORY_DIALOG);
+		if (dir == null)
+			return;
+		Cache.evictAll();
+		RefDataImport refImport = new RefDataImport(dir, Database.get());
+		App.run("Import reference data", refImport, new Runnable() {
+			@Override
+			public void run() {
+				Cache.evictAll();
+				Navigator.refresh();
+			}
+		});
+	}
+
+}
