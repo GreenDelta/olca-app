07da2c8034390cb835d7d88651e45fe604bf9d75
msrocka <michael.srocka@gmail.com>
2014-03-14 11:27:25 +0100

    removed JobListener from image export

--- olca-app/src/org/openlca/app/editors/graphical/action/SaveImageAction.java
+++ olca-app/src/org/openlca/app/editors/graphical/action/SaveImageAction.java
@@ -2,7 +2,6 @@ package org.openlca.app.editors.graphical.action;
 
 import java.io.File;
 
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.SWTGraphics;
@@ -10,20 +9,16 @@ import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.LayerConstants;
 import org.eclipse.gef.editparts.ScalableRootEditPart;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
-import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.ui.PlatformUI;
+import org.openlca.app.App;
 import org.openlca.app.Messages;
-import org.openlca.app.components.JobListenerWithProgress;
+import org.openlca.app.components.FileChooser;
 import org.openlca.app.editors.graphical.ProductSystemGraphEditor;
 import org.openlca.app.resources.ImageType;
-import org.openlca.app.util.UI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,58 +41,37 @@ public class SaveImageAction extends Action {
 	public void run() {
 		if (editor == null)
 			return;
-		FileDialog dialog = new FileDialog(UI.shell(), SWT.SAVE);
-		dialog.setText(Messages.SaveAsImage);
-		dialog.setFileName("graph.png");
-		dialog.setFilterExtensions(new String[] { "*.png" });
-		dialog.setFilterNames(new String[] { "*.png (Portable Network Graphics (PNG)" });
-		final String fileName = dialog.open();
-
-		if (fileName == null)
+		File file = FileChooser.forExport("*.png", "graph.png");
+		if (file == null)
 			return;
-		if (fileName.length() == 0)
-			return;
-
-		final File file = new File(fileName);
-		boolean write = false;
-		if (file.exists()) {
-			write = MessageDialog.openQuestion(UI.shell(),
-					Messages.FileAlreadyExists, Messages.OverwriteFileQuestion);
-		} else
-			write = true;
-		if (!write)
-			return;
-
-		try {
-			PlatformUI.getWorkbench().getProgressService()
-					.busyCursorWhile(new JobListenerWithProgress() {
-
-						@Override
-						public void run() {
-							jobStarted(NLS.bind(Messages.Systems_SavingAsImage,
-									file), IProgressMonitor.UNKNOWN);
-							ScalableRootEditPart rootEditPart = (ScalableRootEditPart) editor
-									.getGraphicalViewer().getRootEditPart();
-
-							IFigure rootFigure = rootEditPart
-									.getLayer(LayerConstants.PRINTABLE_LAYERS);
-							Rectangle rootFigureBounds = rootFigure.getBounds();
-							Image img = new Image(null, rootFigureBounds.width,
-									rootFigureBounds.height);
-							GC imageGC = new GC(img);
-							Graphics graphics = new SWTGraphics(imageGC);
-							rootFigure.paint(graphics);
-
-							ImageLoader imgLoader = new ImageLoader();
-							imgLoader.data = new ImageData[] { img
-									.getImageData() };
-							imgLoader.save(fileName, SWT.IMAGE_PNG);
-							done();
-						}
-					});
-		} catch (final Exception e) {
-			log.error("Run error", e);
-		}
+		App.run(Messages.SaveAsImage, new Runner(file));
 	}
 
+	private class Runner implements Runnable {
+
+		private File file;
+
+		public Runner(File file) {
+			this.file = file;
+		}
+
+		@Override
+		public void run() {
+			if (file == null)
+				return;
+			log.trace("export product graph as image: {}", file);
+			ScalableRootEditPart editPart = (ScalableRootEditPart) editor
+					.getGraphicalViewer().getRootEditPart();
+			IFigure rootFigure = editPart
+					.getLayer(LayerConstants.PRINTABLE_LAYERS);
+			Rectangle bounds = rootFigure.getBounds();
+			Image img = new Image(null, bounds.width, bounds.height);
+			GC imageGC = new GC(img);
+			Graphics graphics = new SWTGraphics(imageGC);
+			rootFigure.paint(graphics);
+			ImageLoader imgLoader = new ImageLoader();
+			imgLoader.data = new ImageData[] { img.getImageData() };
+			imgLoader.save(file.getAbsolutePath(), SWT.IMAGE_PNG);
+		}
+	}
 }
