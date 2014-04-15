b78a3154d68e089daa57750a840524b3cdad4905
msrocka <michael.srocka@gmail.com>
2014-04-15 19:29:07 +0200

    added Excel import wizard

---
+++ olca-app/src/org/openlca/app/wizards/io/ExcelImportWizard.java
@@ -0,0 +1,70 @@
+package org.openlca.app.wizards.io;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+import org.openlca.app.db.Cache;
+import org.openlca.app.db.Database;
+import org.openlca.app.navigation.Navigator;
+import org.openlca.app.resources.ImageType;
+import org.openlca.io.xls.process.input.ExcelImport;
+
+public class ExcelImportWizard extends Wizard implements IImportWizard {
+
+	private FileImportPage importPage;
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		setWindowTitle("Import processes from MS Excel");
+		setDefaultPageImageDescriptor(ImageType.IMPORT_ZIP_WIZARD
+				.getDescriptor());
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public void addPages() {
+		importPage = new FileImportPage(new String[] { "xlsx", "xls" }, false);
+		addPage(importPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+		File[] files = importPage.getFiles();
+		if (files == null)
+			return false;
+		try {
+			doRun(files);
+			return true;
+		} catch (final Exception e) {
+			return false;
+		} finally {
+			Navigator.refresh();
+			Cache.evictAll();
+		}
+	}
+
+	private void doRun(final File[] files) throws Exception {
+		getContainer().run(true, true, new IRunnableWithProgress() {
+			@Override
+			public void run(IProgressMonitor monitor)
+					throws InvocationTargetException, InterruptedException {
+				monitor.beginTask("Import: ", files.length);
+				for (File file : files) {
+					monitor.subTask(file.getName());
+					ExcelImport excelImport = new ExcelImport(file, Database
+							.get());
+					excelImport.run();
+					monitor.worked(1);
+				}
+				monitor.done();
+			}
+		});
+
+	}
+}
