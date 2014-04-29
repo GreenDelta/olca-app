46be0db130b268c5e0239c83a04eb1bf92302f8e
msrocka <michael.srocka@gmail.com>
2014-04-29 11:37:47 +0200

    added Warning class for showing message boxes and popups

--- olca-app/src/org/openlca/app/util/Warning.java
+++ olca-app/src/org/openlca/app/util/Warning.java
@@ -2,24 +2,25 @@ package org.openlca.app.util;
 
 import org.openlca.app.util.MessageBox.Type;
 
-public class Info {
+public class Warning {
 
-	private Info() {
+	private Warning() {
 	}
 
 	public static void showPopup(String message) {
-		new InformationPopup(message).show();
+		new WarningPopup(message).show();
 	}
 
 	public static void showPopup(String title, String message) {
-		new InformationPopup(title, message).show();
+		new WarningPopup(title, message).show();
 	}
 
 	public static void showBox(String message) {
-		MessageBox.show(message, Type.INFO);
+		MessageBox.show(message, Type.WARNING);
 	}
 
 	public static void showBox(String title, String message) {
-		MessageBox.show(title, message, Type.INFO);
+		MessageBox.show(title, message, Type.WARNING);
 	}
+
 }
