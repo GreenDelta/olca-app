ee5e24c14840e7eddea3740cd05b09963043e40a
Sebastian Greve <greve@greendelta.com>
2014-06-04 13:55:31 +0200

    Added equals to ConnectionInput to avoid multiple connection links for same inputs (list.contains now works)

--- olca-app/src/org/openlca/app/editors/graphical/command/ConnectionInput.java
+++ olca-app/src/org/openlca/app/editors/graphical/command/ConnectionInput.java
@@ -12,4 +12,19 @@ public class ConnectionInput {
 		this.flowId = flowId;
 	}
 
+	@Override
+	public boolean equals(Object arg0) {
+		if (!(arg0 instanceof ConnectionInput))
+			return false;
+		if (this == arg0)
+			return true;
+		ConnectionInput input = (ConnectionInput) arg0;
+		if (sourceId != input.sourceId)
+			return false;
+		if (targetId != input.targetId)
+			return false;
+		if (flowId != input.flowId)
+			return false;
+		return true;
+	}
 }
