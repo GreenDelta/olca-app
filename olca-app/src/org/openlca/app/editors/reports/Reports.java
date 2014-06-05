7b626b86141b30413fae537c44f9fd1326aff573
msrocka <michael.srocka@gmail.com>
2014-06-05 13:32:03 +0200

    create default report variants

--- olca-app/src/org/openlca/app/editors/reports/Reports.java
+++ olca-app/src/org/openlca/app/editors/reports/Reports.java
@@ -4,7 +4,9 @@ import org.openlca.app.App;
 import org.openlca.app.editors.reports.model.Report;
 import org.openlca.app.editors.reports.model.ReportCalculator;
 import org.openlca.app.editors.reports.model.ReportSection;
+import org.openlca.app.editors.reports.model.ReportVariant;
 import org.openlca.core.model.Project;
+import org.openlca.core.model.ProjectVariant;
 
 public final class Reports {
 
@@ -21,9 +23,21 @@ public final class Reports {
 		}
 		report.setProject(project);
 		report.setTitle(project.getName());
+		createVariants(project, report);
 		App.run("Calculate results",
 				new ReportCalculator(project, report),
 				() -> ReportEditor.open(report));
+	}
+
+	private static void createVariants(Project project, Report report) {
+		if (project == null)
+			return;
+		for (ProjectVariant variant : project.getVariants()) {
+			ReportVariant reportVariant = new ReportVariant();
+			reportVariant.setName(variant.getName());
+			reportVariant.setUserFriendlyName(variant.getName());
+			report.getVariants().add(reportVariant);
+		}
 	}
 
 	private static void createDefaultSections(Report report) {
