86952cf3940eafd59cad5d6bf02acfacfc2585f0
msrocka <michael.srocka@gmail.com>
2014-09-09 16:09:22 +0200

    added html resources of script editors

--- olca-app-html/gulpfile.js
+++ olca-app-html/gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('pages', function() {
 	gulp.src('src/start_page/*').pipe(gulp.dest('build'));
 	gulp.src('src/bubble_chart/bubble_chart.html').pipe(gulp.dest('build'));
 	gulp.src('src/*.html').pipe(gulp.dest('build'));
+	gulp.src('src/devtools/*').pipe(gulp.dest('build'));
 	gulp.src('images/*').pipe(gulp.dest('build/images'));
 });
 
@@ -25,6 +26,9 @@ gulp.task('libs', function() {
 	// min-files
 	gulp.src(['./bower_components/angular/angular.min.js',
 		'./bower_components/angular-sanitize/angular-sanitize.min.js',
+		'./bower_components/codemirror/lib/*',
+		'./bower_components/codemirror/mode/javascript/javascript.js',
+		'./bower_components/codemirror/mode/python/python.js',
 		'./bower_components/d3/d3.min.js',
 		'./bower_components/jquery/dist/jquery.min.js'
 	]).pipe(gulp.dest('./build/libs'));
