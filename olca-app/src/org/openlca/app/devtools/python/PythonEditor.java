ea36580af227333467fb521811d5dfad3afd8b8b
msrocka <michael.srocka@gmail.com>
2014-09-12 15:20:38 +0200

    load python library from jar

--- olca-app/src/org/openlca/app/devtools/python/PythonEditor.java
+++ olca-app/src/org/openlca/app/devtools/python/PythonEditor.java
@@ -3,11 +3,9 @@ package org.openlca.app.devtools.python;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.openlca.app.App;
-import org.openlca.app.db.Database;
 import org.openlca.app.devtools.IScriptEditor;
 import org.openlca.app.devtools.ScriptEditorInput;
 import org.openlca.app.util.Editors;
-import org.python.util.PythonInterpreter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,27 +45,8 @@ public class PythonEditor extends FormEditor implements IScriptEditor {
 
 	@Override
 	public void evalContent() {
-		try {
-			// TODO: copy Lib with Python modules to workspace when used
-			// the first time
-			// set system properties on first use
-			// PyScriptEngineFactory factory = new PyScriptEngineFactory();
-			System.setProperty("python.path",
-					"C:\\Users\\Besitzer\\Downloads\\xpy\\Lib");
-			System.setProperty("python.home",
-					"C:\\Users\\Besitzer\\Downloads\\xpy\\Lib");
-
-			PythonInterpreter interpreter = new PythonInterpreter();
-			interpreter.set("log", LoggerFactory.getLogger(getClass()));
-			if (Database.get() != null)
-				interpreter.set("db", Database.get());
-			interpreter.set("app", App.class);
-			interpreter.exec(page.getScript());
-			interpreter.close();
-		} catch (Exception e) {
-			Logger log = LoggerFactory.getLogger(getClass());
-			log.error("failed to evaluate script", e);
-		}
+		String script = page.getScript();
+		App.run("Eval script", () -> Python.eval(script));
 	}
 
 }
