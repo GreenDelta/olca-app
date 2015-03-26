a33fdc717e8ee698e4e0f097fbb332cdf62e8195
Sebastian Greve <greve@greendelta.com>
2015-03-26 08:41:52 +0100

    Fixed gulp build and updated nodejs dependencies

--- olca-app-html/gulpfile.js
+++ olca-app-html/gulpfile.js
@@ -1,48 +1,111 @@
 var gulp = require('gulp');
-var del = require('del');
+var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
 var fileinclude = require('gulp-file-include');
 var zip = require('gulp-zip');
-var concat = require('gulp-concat')
-var stylus = require('gulp-stylus')
-var nib = require('nib')
-var jade = require('gulp-jade')
-var jadeClient = require('gulp-clientjade')
-var coffee = require('gulp-coffee')
+var concat = require('gulp-concat');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var jade = require('gulp-jade');
+var jadeClient = require('gulp-clientjade');
+var coffee = require('gulp-coffee');
 
-gulp.task('default', [], function(callback) {
-	runSequence('libs', 'pages', 'precompilePluginManager', 'pluginManager', callback);
+gulp.task('default', function() {
+	runSequence('clean', 'build', 'zip');
 });
-// compile the html pages and copy them to the build directory
-gulp.task('pages', function() {
-	gulp.src('src/report_view/report_view.html')
-			.pipe(fileinclude({
-				prefix: '//@',
-				basepath: '@file'
-			}))
-			.pipe(gulp.dest('build'));
-	gulp.src('src/start_page/*.html')
-			.pipe(fileinclude({
-				prefix: '//@',
-				basepath: '@file'
-			}))
-			.pipe(gulp.dest('build'));	
-	gulp.src('src/bubble_chart/bubble_chart.html').pipe(gulp.dest('build'));
-	gulp.src('src/*.html').pipe(gulp.dest('build'));
-	gulp.src('src/devtools/*').pipe(gulp.dest('build'));
-	gulp.src('images/*').pipe(gulp.dest('build/images'));
+gulp.task('build', ['pages', 'resources', 'libs']);
+gulp.task('pages', ['report_view', 'start_page', 'bubble_chart', 'devtools', 'plugin_manager', 'other_pages']);
+gulp.task('resources', ['images']);
+gulp.task('libs', ['bower_components', 'bootstrap', 'other_libs']);
+
+gulp.task('clean', function() {
+	return gulp.src(['build', 'dist', 'src/plugin_manager/precompiled'], {read: false})
+		.pipe(clean());
 });
 
-gulp.task('precompilePluginManager', function() {
-	gulp.src('src/plugin_manager/coffeescript/*.coffee')
+gulp.task('report_view', function() {
+	return gulp.src('src/report_view/report_view.html')
+		.pipe(fileinclude({
+			prefix: '//@',
+			basepath: '@file'
+		}))
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('start_page', function() {
+	return gulp.src('src/start_page/*.html')
+		.pipe(fileinclude({
+			prefix: '//@',
+			basepath: '@file'
+		}))
+		.pipe(gulp.dest('build'));	
+});
+
+gulp.task('bubble_chart', function() {
+	return gulp.src('src/bubble_chart/bubble_chart.html')
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('devtools', function() {
+	return gulp.src('src/devtools/*')
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('other_pages', function() {
+	return gulp.src('src/*.html')
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('images', function() {
+	return gulp.src('images/*')
+		.pipe(gulp.dest('build/images'));
+});
+
+gulp.task('bower_components', function() {
+	return gulp.src(['bower_components/angular/angular.min.js',
+		'bower_components/angular-sanitize/angular-sanitize.min.js',
+		'bower_components/codemirror/lib/*',
+		'bower_components/codemirror/mode/javascript/javascript.js',
+		'bower_components/codemirror/mode/python/python.js',
+		'bower_components/d3/d3.min.js',
+		'bower_components/jquery/dist/jquery.min.js'
+	]).pipe(gulp.dest('build/libs'));
+});
+
+gulp.task('bootstrap', function() {
+	return gulp.src(['bower_components/bootstrap/dist/**'])
+		.pipe(gulp.dest('build/libs/bootstrap'));
+});
+
+gulp.task('other_libs', function() {
+	return gulp.src(['other_libs/*.js'])
+		.pipe(gulp.dest('build/libs'));
+});
+
+gulp.task('plugin_manager', ['plugin_manager_scripts', 'plugin_manager_templates', 'plugin_manager_styles'], function() {
+	return gulp.src('src/plugin_manager/jade/plugin_manager.jade')
+		.pipe(jade({
+			locals: {}
+		}))
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('plugin_manager_scripts', function() {
+	return gulp.src('src/plugin_manager/coffeescript/*.coffee')
 		.pipe(coffee({
 			bare: true
 		}))
 		.pipe(gulp.dest('src/plugin_manager/precompiled'));
-	gulp.src('src/plugin_manager/jade/templates/*.jade')
+});
+
+gulp.task('plugin_manager_templates', function() {
+	return gulp.src('src/plugin_manager/jade/templates/*.jade')
 		.pipe(jadeClient('templates.js'))
 		.pipe(gulp.dest('src/plugin_manager/precompiled'));
-	gulp.src('src/plugin_manager/stylus/*.styl')
+});
+
+gulp.task('plugin_manager_styles', function() {
+	return gulp.src('src/plugin_manager/stylus/*.styl')
 		.pipe(stylus({
 			use: [nib()]
 		}))
@@ -50,42 +113,8 @@ gulp.task('precompilePluginManager', function() {
 		.pipe(gulp.dest('src/plugin_manager/precompiled'));
 });
 
-gulp.task('pluginManager', function() {
-	gulp.src('src/plugin_manager/jade/plugin_manager.jade')
-		.pipe(jade({
-			locals: {}
-		}))
-		.pipe(gulp.dest('build'));
-});
-
-// copy the libraries to the build/lib folder
-gulp.task('libs', function() {
-	
-	// min-files
-	gulp.src(['./bower_components/angular/angular.min.js',
-		'./bower_components/angular-sanitize/angular-sanitize.min.js',
-		'./bower_components/codemirror/lib/*',
-		'./bower_components/codemirror/mode/javascript/javascript.js',
-		'./bower_components/codemirror/mode/python/python.js',
-		'./bower_components/d3/d3.min.js',
-		'./bower_components/jquery/dist/jquery.min.js'
-	]).pipe(gulp.dest('./build/libs'));
-	
-	// bootstrap folders
-	gulp.src(['./bower_components/bootstrap/dist/**'])
-			.pipe(gulp.dest('./build/libs/bootstrap'));
-	
-	// other libs
-	gulp.src(['./other_libs/*.js']).pipe(gulp.dest("./build/libs"));
-	
-});
-
-gulp.task('clean', function(cb) {
-	del(['build/*'], cb);
-});
-
 gulp.task('zip', function() {
 	return gulp.src('build/**')
-        .pipe(zip('base_html.zip'))
-        .pipe(gulp.dest('dist'));	
+		.pipe(zip('base_html.zip'))
+		.pipe(gulp.dest('dist'));
 });
