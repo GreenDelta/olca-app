f68277fe1361569e0ed5ce1e688e1fda24bfa482
Sebastian Greve <greve@greendelta.com>
2015-03-26 09:08:10 +0100

    Further gulp file refactoring

--- olca-app-html/gulpfile.js
+++ olca-app-html/gulpfile.js
@@ -11,20 +11,30 @@ var jadeClient = require('gulp-clientjade');
 var coffee = require('gulp-coffee');
 
 gulp.task('default', function() {
-	runSequence('clean', 'build', 'zip');
+	runSequence('clean', 'precompile', 'build', 'zip');
 });
-gulp.task('build', ['pages', 'resources', 'libs']);
-gulp.task('pages', ['report_view', 'start_page', 'bubble_chart', 'devtools', 'plugin_manager', 'other_pages']);
+gulp.task('precompile', ['plugin_manager_scripts', 'plugin_manager_templates', 'plugin_manager_styles']);
+gulp.task('build', ['html_pages', 'jade_pages', 'resources', 'libs']);
 gulp.task('resources', ['images']);
-gulp.task('libs', ['bower_components', 'bootstrap', 'other_libs']);
+gulp.task('libs', ['base_libs', 'bootstrap']);
 
 gulp.task('clean', function() {
-	return gulp.src(['build', 'dist', 'src/plugin_manager/precompiled'], {read: false})
+	return gulp.src([
+			'build', 
+			'dist', 
+			'src/plugin_manager/precompiled'
+		], {read: false})
 		.pipe(clean());
 });
 
-gulp.task('report_view', function() {
-	return gulp.src('src/report_view/report_view.html')
+gulp.task('html_pages', function() {
+	return gulp.src([
+			'src/report_view/report_view.html', 
+			'src/start_page/*.html', 
+			'src/bubble_chart/bubble_chart.html', 
+			'src/devtools/*', 
+			'src/*.html'
+		])
 		.pipe(fileinclude({
 			prefix: '//@',
 			basepath: '@file'
@@ -32,27 +42,11 @@ gulp.task('report_view', function() {
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('start_page', function() {
-	return gulp.src('src/start_page/*.html')
-		.pipe(fileinclude({
-			prefix: '//@',
-			basepath: '@file'
+gulp.task('jade_pages', function() {
+	return gulp.src('src/plugin_manager/jade/plugin_manager.jade')
+		.pipe(jade({
+			locals: {}
 		}))
-		.pipe(gulp.dest('build'));	
-});
-
-gulp.task('bubble_chart', function() {
-	return gulp.src('src/bubble_chart/bubble_chart.html')
-		.pipe(gulp.dest('build'));
-});
-
-gulp.task('devtools', function() {
-	return gulp.src('src/devtools/*')
-		.pipe(gulp.dest('build'));
-});
-
-gulp.task('other_pages', function() {
-	return gulp.src('src/*.html')
 		.pipe(gulp.dest('build'));
 });
 
@@ -61,33 +55,23 @@ gulp.task('images', function() {
 		.pipe(gulp.dest('build/images'));
 });
 
-gulp.task('bower_components', function() {
-	return gulp.src(['bower_components/angular/angular.min.js',
-		'bower_components/angular-sanitize/angular-sanitize.min.js',
-		'bower_components/codemirror/lib/*',
-		'bower_components/codemirror/mode/javascript/javascript.js',
-		'bower_components/codemirror/mode/python/python.js',
-		'bower_components/d3/d3.min.js',
-		'bower_components/jquery/dist/jquery.min.js'
-	]).pipe(gulp.dest('build/libs'));
+gulp.task('base_libs', function() {
+	return gulp.src([
+			'bower_components/angular/angular.min.js',
+			'bower_components/angular-sanitize/angular-sanitize.min.js',
+			'bower_components/codemirror/lib/*',
+			'bower_components/codemirror/mode/javascript/javascript.js',
+			'bower_components/codemirror/mode/python/python.js',
+			'bower_components/d3/d3.min.js',
+			'bower_components/jquery/dist/jquery.min.js',
+			'other_libs/*.js'
+		])
+		.pipe(gulp.dest('build/libs'));
 });
 
 gulp.task('bootstrap', function() {
 	return gulp.src(['bower_components/bootstrap/dist/**'])
 		.pipe(gulp.dest('build/libs/bootstrap'));
-});
-
-gulp.task('other_libs', function() {
-	return gulp.src(['other_libs/*.js'])
-		.pipe(gulp.dest('build/libs'));
-});
-
-gulp.task('plugin_manager', ['plugin_manager_scripts', 'plugin_manager_templates', 'plugin_manager_styles'], function() {
-	return gulp.src('src/plugin_manager/jade/plugin_manager.jade')
-		.pipe(jade({
-			locals: {}
-		}))
-		.pipe(gulp.dest('build'));
 });
 
 gulp.task('plugin_manager_scripts', function() {
