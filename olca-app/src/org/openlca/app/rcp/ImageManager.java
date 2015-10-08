96d44b3002b55c604c266e11772488d835956ea1
greve <greve@greendelta.com>
2015-10-08 12:21:38 +0200

    Added option to create images with overlay

--- olca-app/src/org/openlca/app/rcp/ImageManager.java
+++ olca-app/src/org/openlca/app/rcp/ImageManager.java
@@ -2,6 +2,8 @@ package org.openlca.app.rcp;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.viewers.DecorationOverlayIcon;
+import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.swt.graphics.Image;
 
 /**
@@ -14,10 +16,10 @@ public class ImageManager {
 	public static Image getImage(ImageType imageType) {
 		String path = imageType.getPath();
 		Image image = imageRegistry.get(path);
-		if (image == null || image.isDisposed()) {
-			image = imageType.createImage();
-			imageRegistry.put(path, image);
-		}
+		if (image != null && !image.isDisposed())
+			return image;
+		image = imageType.createImage();
+		imageRegistry.put(path, image);
 		return image;
 	}
 
@@ -31,4 +33,18 @@ public class ImageManager {
 		return d;
 	}
 
+	public static Image getImageWithOverlay(ImageType imageType,
+			ImageType overlayType) {
+		String path = imageType.getPath() + "-" + overlayType.getPath();
+		Image imageWithOverlay = imageRegistry.get(path);
+		if (imageWithOverlay != null && !imageWithOverlay.isDisposed())
+			return imageWithOverlay;
+		Image image = getImage(imageType);
+		ImageDescriptor overlay = getImageDescriptor(overlayType);
+		DecorationOverlayIcon imageWithIcon = new DecorationOverlayIcon(image,
+				overlay, IDecoration.BOTTOM_RIGHT);
+		imageWithOverlay = imageWithIcon.createImage();
+		imageRegistry.put(path, imageWithOverlay);
+		return imageWithOverlay;
+	}
 }
