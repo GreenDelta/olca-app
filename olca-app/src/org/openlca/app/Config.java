f1decd98e4ab1f56f4faea6ed3b6ccd2a1a54f7a
greve <greve@greendelta.com>
2015-12-10 03:14:44 +0100

    Updated alpha version no

--- olca-app/src/org/openlca/app/Config.java
+++ olca-app/src/org/openlca/app/Config.java
@@ -13,7 +13,7 @@ public final class Config {
 	 * The version that is shown in the application to the user. Note that this
 	 * can be another version then the internal build version.
 	 */
-	public static final String VERSION = "1.5.0alpha3";
+	public static final String VERSION = "1.5.0alpha4";
 
 	/**
 	 * The name of the application that is shown to the user.
@@ -26,7 +26,7 @@ public final class Config {
 	 * "openLCA-data", but it can be renamed in development versions (so that
 	 * the users can run multiple versions of openLCA in parallel).
 	 */
-	public static final String WORK_SPACE_FOLDER_NAME = "openLCA-data-1.5-alpha3";
+	public static final String WORK_SPACE_FOLDER_NAME = "openLCA-data-1.5-alpha4";
 
 	/**
 	 * Indicates if the workspace folder should be located in the user dir. If
