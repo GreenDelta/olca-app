e79a6ef78774797c204788e5d6ed45341b6d2e8e
Sebastian Greve <greve@greendelta.com>
2016-01-28 11:00:37 +0100

    Removed obsolete comments for formatting

--- olca-app/src/org/openlca/app/rcp/images/FileIcon.java
+++ olca-app/src/org/openlca/app/rcp/images/FileIcon.java
@@ -2,15 +2,15 @@ package org.openlca.app.rcp.images;
 
 enum FileIcon {
 
-	DEFAULT("file.png"), //
-	CSV("file/csv.png"), //
-	EXCEL("file/excel.png"), //
-	IMAGE("file/image.png"), //
-	MARKUP("file/markup.png"), //
-	PDF("file/pdf.png"), //
-	POWERPOINT("file/powerpoint.png"), //
-	WORD("file/word.png"), //
-	XML("file/xml.png"), //
+	DEFAULT("file.png"),
+	CSV("file/csv.png"),
+	EXCEL("file/excel.png"),
+	IMAGE("file/image.png"),
+	MARKUP("file/markup.png"),
+	PDF("file/pdf.png"),
+	POWERPOINT("file/powerpoint.png"),
+	WORD("file/word.png"),
+	XML("file/xml.png"),
 	ZIP("file/zip.png");
 
 	final String fileName;
--- olca-app/src/org/openlca/app/rcp/images/Overlay.java
+++ olca-app/src/org/openlca/app/rcp/images/Overlay.java
@@ -2,16 +2,16 @@ package org.openlca.app.rcp.images;
 
 public enum Overlay {
 
-	ADDED("overlay/cloud/added.png"), //
-	ADD_TO_LOCAL("overlay/cloud/add_local.png"), //
-	ADD_TO_REMOTE("overlay/cloud/add_remote.png"), //
-	CONFLICT("overlay/cloud/conflict.png"), //
-	DELETED("overlay/cloud/deleted.png"), //
-	DELETE_FROM_LOCAL("overlay/cloud/delete_local.png"), //
-	DELETE_FROM_REMOTE("overlay/cloud/delete_remote.png"), //
-	MERGED("overlay/cloud/merged.png"), //
-	MODIFY_IN_LOCAL("overlay/cloud/modify_local.png"), //
-	MODIFY_IN_REMOTE("overlay/cloud/modify_remote.png"), //
+	ADDED("overlay/cloud/added.png"),
+	ADD_TO_LOCAL("overlay/cloud/add_local.png"),
+	ADD_TO_REMOTE("overlay/cloud/add_remote.png"),
+	CONFLICT("overlay/cloud/conflict.png"),
+	DELETED("overlay/cloud/deleted.png"),
+	DELETE_FROM_LOCAL("overlay/cloud/delete_local.png"),
+	DELETE_FROM_REMOTE("overlay/cloud/delete_remote.png"),
+	MERGED("overlay/cloud/merged.png"),
+	MODIFY_IN_LOCAL("overlay/cloud/modify_local.png"),
+	MODIFY_IN_REMOTE("overlay/cloud/modify_remote.png"),
 	NEW("overlay/new.png");
 
 	final String fileName;
