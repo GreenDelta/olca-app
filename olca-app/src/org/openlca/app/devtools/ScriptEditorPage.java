c61e4e72fd35535174981b8ac72a9fddfabbec51
msrocka <michael.srocka@gmail.com>
2016-10-05 13:03:52 +0200

    use JavaFX WebView in script editors

--- olca-app/src/org/openlca/app/devtools/ScriptEditorPage.java
+++ olca-app/src/org/openlca/app/devtools/ScriptEditorPage.java
@@ -1,6 +1,5 @@
 package org.openlca.app.devtools;
 
-import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
@@ -8,22 +7,25 @@ import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
-import org.openlca.app.rcp.html.HtmlPage;
+import org.openlca.app.rcp.html.WebPage;
 import org.openlca.app.util.UI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class ScriptEditorPage extends FormPage implements HtmlPage {
+import javafx.scene.web.WebEngine;
+
+public abstract class ScriptEditorPage extends FormPage implements WebPage {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
-	private Browser browser;
+	private WebEngine webkit;
 
 	public ScriptEditorPage(FormEditor editor, String id, String title) {
 		super(editor, id, title);
 	}
 
 	@Override
-	public void onLoaded() {
+	public void onLoaded(WebEngine webkit) {
+		this.webkit = webkit;
 	}
 
 	@Override
@@ -32,12 +34,12 @@ public abstract class ScriptEditorPage extends FormPage implements HtmlPage {
 		FormToolkit toolkit = managedForm.getToolkit();
 		Composite body = UI.formBody(form, toolkit);
 		body.setLayout(new FillLayout());
-		browser = UI.createBrowser(body, this);
+		UI.createWebView(body, this);
 	}
 
 	public String getScript() {
 		try {
-			Object script = browser.evaluate("return getContent();");
+			Object script = webkit.executeScript("getContent();");
 			if (script == null)
 				return "";
 			return script.toString();
