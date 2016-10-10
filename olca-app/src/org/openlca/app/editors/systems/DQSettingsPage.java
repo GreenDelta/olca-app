7b0223440ee9b8c3643b37dcb7d15c634029a8da
Sebastian Greve <greve@greendelta.com>
2016-10-10 14:11:47 +0200

    Refactored creation of DQ calculation setup

--- olca-app/src/org/openlca/app/editors/systems/DQSettingsPage.java
+++ olca-app/src/org/openlca/app/editors/systems/DQSettingsPage.java
@@ -64,17 +64,21 @@ class DQSettingsPage extends WizardPage {
 	}
 
 	public DQCalculationSetup getSetup(ProductSystem system) {
-		long psId = system.getId();
+		DQCalculationSetup setup = new DQCalculationSetup();
+		setup.productSystemId = system.getId();
 		DQSystemDao dqDao = new DQSystemDao(Database.get());
-		DQSystem pSystem = null;
 		DQSystemDescriptor pSystemDesc = processSystemViewer.getSelected();
-		if (pSystemDesc != null)
-			pSystem = dqDao.getForId(pSystemDesc.getId());
-		DQSystem eSystem = null;
+		if (pSystemDesc != null) {
+			setup.processDqSystem = dqDao.getForId(pSystemDesc.getId());
+		}
 		DQSystemDescriptor eSystemDesc = exchangeSystemViewer.getSelected();
-		if (eSystemDesc != null)
-			eSystem = dqDao.getForId(eSystemDesc.getId());
-		return new DQCalculationSetup(psId, aggregationType, roundingMode, processingType, pSystem, eSystem);
+		if (eSystemDesc != null) {
+			setup.exchangeDqSystem = dqDao.getForId(eSystemDesc.getId());
+		}
+		setup.aggregationType = aggregationType;
+		setup.roundingMode = roundingMode;
+		setup.processingType = processingType;
+		return setup;
 	}
 
 	private void loadDefaults() {
