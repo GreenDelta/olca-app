2f3f6ccc6fc180b580fdc2c035c2ab151c5913b5
Sebastian Greve <greve@greendelta.com>
2017-02-17 09:57:53 +0100

    Resetting calculation context when evaluating python script

--- olca-app/src/org/openlca/app/devtools/python/Python.java
+++ olca-app/src/org/openlca/app/devtools/python/Python.java
@@ -24,6 +24,7 @@ public class Python {
 			if (!initialized)
 				initialize();
 			python.setDatabase(Database.get());
+			python.setContext(App.getCalculationContext());
 			python.eval(script);
 		} catch (Exception e) {
 			Logger log = LoggerFactory.getLogger(Python.class);
@@ -48,7 +49,7 @@ public class Python {
 		try {
 			if (!matchVersion(pyDir))
 				initPythonDir(pyDir);
-			python = new org.openlca.updates.script.Python(App.getCalculationContext(), pyDir);
+			python = new org.openlca.updates.script.Python(pyDir);
 			python.register("app", App.class);
 			File dataDir = new File(workspace, "script_data");
 			if (!dataDir.exists())
