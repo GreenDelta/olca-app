500f807f8165b2d3f4b6555ddee835f8a9e32087
msrocka <michael.srocka@gmail.com>
2017-06-20 16:21:55 +0200

    fix issue #23: Allow to execute Python scripts without an open database

--- olca-app/src/org/openlca/app/devtools/Toolbar.java
+++ olca-app/src/org/openlca/app/devtools/Toolbar.java
@@ -3,28 +3,20 @@ package org.openlca.app.devtools;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.part.EditorActionBarContributor;
-import org.openlca.app.M;
-import org.openlca.app.db.Database;
 import org.openlca.app.rcp.images.Icon;
 import org.openlca.app.util.Actions;
 import org.openlca.app.util.Editors;
-import org.openlca.app.util.Info;
 
 public class Toolbar extends EditorActionBarContributor {
 
 	@Override
 	public void contributeToToolBar(IToolBarManager manager) {
-		manager.add(Actions.create("Run", Icon.RUN.descriptor(),
-				() -> {
-					if (Database.get() == null) {
-						Info.showBox(M.NoDatabaseOpened, M.NeedOpenDatabase);
-						return;
-					}
-					FormEditor editor = Editors.getActive();
-					if (!(editor instanceof IScriptEditor))
-						return;
-					IScriptEditor scriptEditor = (IScriptEditor) editor;
-					scriptEditor.evalContent();
-				}));
+		manager.add(Actions.create("Run", Icon.RUN.descriptor(), () -> {
+			FormEditor editor = Editors.getActive();
+			if (!(editor instanceof IScriptEditor))
+				return;
+			IScriptEditor scriptEditor = (IScriptEditor) editor;
+			scriptEditor.evalContent();
+		}));
 	}
 }
