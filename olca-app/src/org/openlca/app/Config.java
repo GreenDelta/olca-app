554f007f50abdbee8e80a04b0dd367d6027609e9
msrocka <michael.srocka@gmail.com>
2017-07-07 13:26:28 +0200

    Bumped version number to 1.7.0

--- olca-app/src/org/openlca/app/Config.java
+++ olca-app/src/org/openlca/app/Config.java
@@ -13,7 +13,7 @@ public final class Config {
 	 * The version that is shown in the application to the user. Note that this
 	 * can be another version then the internal build version.
 	 */
-	public static final String VERSION = "1.6.3";
+	public static final String VERSION = "1.7.0";
 
 	/**
 	 * The name of the application that is shown to the user.
