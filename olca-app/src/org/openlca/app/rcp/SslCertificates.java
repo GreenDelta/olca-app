ec0cbb31ab1fb86a4e8c79ad2d34cce972ea9402
Sebastian Greve <greve@greendelta.com>
2017-08-15 14:40:03 +0200

    Added debugging and minimal checks

--- olca-app/src/org/openlca/app/rcp/SslCertificates.java
+++ olca-app/src/org/openlca/app/rcp/SslCertificates.java
@@ -14,23 +14,32 @@ class SslCertificates {
 	private static final Logger log = LoggerFactory.getLogger(SslCertificates.class);
 
 	static void load() {
+		log.debug("Loading external ssl certificates");
 		File workspace = Workspace.getDir();
 		File certificates = new File(workspace, "ssl-certificates");
 		if (!certificates.exists() || !certificates.isDirectory()) {
 			log.debug("No certificates found in workspace");
 			return;
 		}
-		int count = 0;
 		for (File file : certificates.listFiles()) {
-			if (file.isDirectory())
+			if (file.isDirectory()) {
+				log.debug("Skipping directory: " + file.getName());
 				continue;
+			}
+			String name = file.getName();
+			if (!name.endsWith(".cer") && !name.endsWith(".crt") && !name.endsWith(".pem")) {
+				log.debug("Skipping file: " + file.getName() + " (filename must end with .cer, .crt or .pem)");
+				continue;
+			}
+			name = name.substring(0, file.getName().lastIndexOf("."));
+			log.debug("Loading certificate " + name);
 			try (InputStream stream = new FileInputStream(file)) {
-				Ssl.addCertificate("custom-cert-" + ++count, stream);
+				Ssl.addCertificate(name, stream);
+				log.debug("Sucessfully added certificate " + name);
 			} catch (IOException e) {
-				log.error("Error loading certificate", e);
+				log.error("Error loading certificate " + name, e);
 			}
 		}
-		log.debug("Added " + count + " certificates");
 	}
 
 }
