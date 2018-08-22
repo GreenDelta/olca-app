512df81419a30a7535f0ac770c5feddaa5e34245
msrocka <michael.srocka@gmail.com>
2018-08-22 13:18:01 +0200

    switch back to GeoTools 10.3 to solve #34

--- olca-app/src/org/openlca/app/editors/processes/kml/KmlUtil.java
+++ olca-app/src/org/openlca/app/editors/processes/kml/KmlUtil.java
@@ -10,11 +10,11 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 import org.openlca.app.M;
 import org.openlca.geo.kml.FeatureType;
 import org.openlca.util.BinUtils;
