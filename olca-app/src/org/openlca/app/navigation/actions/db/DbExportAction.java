a42ea77e58109bed275f3c66af6ca2fcddde3c84
Sebastian Greve <greve@greendelta.com>
2018-10-22 12:14:51 +0200

    Fixed error when trying to remove repository files during backup if they don't exist #49

--- olca-app/src/org/openlca/app/navigation/actions/db/DbExportAction.java
+++ olca-app/src/org/openlca/app/navigation/actions/db/DbExportAction.java
@@ -1,6 +1,13 @@
 package org.openlca.app.navigation.actions.db;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.action.Action;
@@ -24,6 +31,8 @@ import org.openlca.app.util.Info;
 import org.openlca.cloud.api.RepositoryConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zeroturnaround.zip.FileSource;
+import org.zeroturnaround.zip.ZipEntrySource;
 import org.zeroturnaround.zip.ZipUtil;
 
 public class DbExportAction extends Action implements INavigationAction {
@@ -60,21 +69,18 @@ public class DbExportAction extends Action implements INavigationAction {
 		}
 		if (config == null)
 			return;
-		File file = FileChooser.forExport("*.zolca", config.getName()
-				+ ".zolca");
+		File file = FileChooser.forExport("*.zolca", config.getName() + ".zolca");
 		if (file == null)
 			return;
 		run(config, file, Database.isActive(config));
 	}
 
-	private void run(IDatabaseConfiguration config, final File zip,
-			final boolean active) {
+	private void run(IDatabaseConfiguration config, final File zip, final boolean active) {
 		if (zip.exists()) {
 			log.trace("delete existing file {}", zip);
 			boolean deleted = zip.delete();
 			if (!deleted) {
-				org.openlca.app.util.Error.showBox(M.CouldNotOverwriteFile
-						+ ": " + zip.getName());
+				org.openlca.app.util.Error.showBox(M.CouldNotOverwriteFile + ": " + zip.getName());
 				return;
 			}
 		}
@@ -86,19 +92,16 @@ public class DbExportAction extends Action implements INavigationAction {
 		updateUI(zip, active);
 	}
 
-	private void realExport(IDatabaseConfiguration config, File zip,
-			boolean active) {
+	private void realExport(IDatabaseConfiguration config, File zip, boolean active) {
 		try {
 			if (active)
 				Database.close();
 			if (config instanceof DerbyConfiguration) {
 				File folder = DatabaseDir.getRootFolder(config.getName());
-				ZipUtil.pack(folder, zip);
-				ZipUtil.removeEntry(zip, DatabaseDir.FILE_STORAGE + "/" + RepositoryConfig.INDEX_DIR);
-				ZipUtil.removeEntry(zip, DatabaseDir.FILE_STORAGE + "/" + RepositoryConfig.PROPERTIES_FILE);
+				ZipEntrySource[] toPack = collectFileSources(folder);
+				ZipUtil.pack(toPack, zip);
 			} else if (config instanceof MySQLConfiguration) {
-				MySQLDatabaseExport export = new MySQLDatabaseExport(
-						(MySQLConfiguration) config, zip);
+				MySQLDatabaseExport export = new MySQLDatabaseExport((MySQLConfiguration) config, zip);
 				export.run();
 			}
 		} catch (Exception e) {
@@ -106,11 +109,33 @@ public class DbExportAction extends Action implements INavigationAction {
 		}
 	}
 
+	private ZipEntrySource[] collectFileSources(File folder) throws IOException {
+		List<ZipEntrySource> fileSources = new ArrayList<>();
+		Files.walkFileTree(folder.toPath(), new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				String relativePath = folder.toPath().relativize(file).toString();
+				if (exclude(relativePath))
+					return FileVisitResult.CONTINUE;
+				fileSources.add(new FileSource(relativePath, file.toFile()));
+				return FileVisitResult.CONTINUE;
+			}
+		});
+		return fileSources.toArray(new ZipEntrySource[fileSources.size()]);
+	}
+
+	private boolean exclude(String relativePath) {
+		if (relativePath.equals(DatabaseDir.FILE_STORAGE + "/" + RepositoryConfig.PROPERTIES_FILE))
+			return true;
+		if (relativePath.startsWith(DatabaseDir.FILE_STORAGE + "/" + RepositoryConfig.INDEX_DIR))
+			return true;
+		return false;
+	}
+
 	private void updateUI(final File zip, final boolean active) {
 		if (active)
 			Navigator.refresh();
 		HistoryView.refresh();
-		Info.popup(M.ExportDone, M.DatabaseWasExportedToFile
-				+ ": " + zip.getName());
+		Info.popup(M.ExportDone, M.DatabaseWasExportedToFile + ": " + zip.getName());
 	}
 }
