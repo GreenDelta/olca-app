654a354aa6c723d2822453e2f0f2d580eb52961e
msrocka <michael.srocka@gmail.com>
2018-11-07 16:57:14 +0100

    #52: Display user friendly DQI labels (instead of numbers) if the DQ system supports this

--- olca-app/src/org/openlca/app/editors/processes/InfoPage.java
+++ olca-app/src/org/openlca/app/editors/processes/InfoPage.java
@@ -119,7 +119,7 @@ class InfoPage extends ModelPage<Process> {
 
 	private Hyperlink createDqEntryRow(Composite parent) {
 		UI.formLabel(parent, toolkit, M.DataQualityEntry);
-		Hyperlink link = UI.formLink(parent, toolkit, getDqEntryLabel());
+		Hyperlink link = UI.formLink(parent, toolkit, getDqLabel());
 		Controls.onClick(link, e -> {
 			if (getModel().dqSystem == null) {
 				Error.showBox("Please select a data quality system first");
@@ -134,7 +134,8 @@ class InfoPage extends ModelPage<Process> {
 			shell.addDisposeListener(e2 -> {
 				if (Objects.equals(oldVal, getModel().dqEntry))
 					return;
-				link.setText(getDqEntryLabel());
+				link.setText(getDqLabel());
+				link.pack();
 				getEditor().setDirty(true);
 			});
 			shell.open();
@@ -151,11 +152,30 @@ class InfoPage extends ModelPage<Process> {
 		getModel().dqEntry = null;
 	}
 
-	private String getDqEntryLabel() {
-		if (getModel().dqEntry != null)
-			return getModel().dqEntry;
-		return "(not specified)";
-
+	private String getDqLabel() {
+		Process p = getModel();
+		if (p.dqEntry == null)
+			return "(not specified)";
+		if (p.dqSystem == null)
+			return p.dqEntry;
+		int[] vals = p.dqSystem.toValues(p.dqEntry);
+		if (vals == null)
+			return p.dqEntry;
+		String label = "(";
+		for (int i = 0; i < vals.length; i++) {
+			if (vals[i] == 0) {
+				label += "n.a.";
+			} else {
+				String e = p.dqSystem.getScoreLabel(vals[i]);
+				if (e == null)
+					return p.dqEntry;
+				label += e;
+			}
+			if (i < (vals.length - 1)) {
+				label += "; ";
+			}
+		}
+		return label + ")";
 	}
 
 	private void createGeographySection(Composite body) {
