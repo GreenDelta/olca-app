2527acd2d19cb05b218b8ad93fabaad3de506f13
msrocka <michael.srocka@gmail.com>
2018-11-07 15:39:14 +0100

    refdata: import data quality systems (also for ILCD) from JSON-LD files

--- olca-refdata/src/main/java/refdata/Main.java
+++ olca-refdata/src/main/java/refdata/Main.java
@@ -1,10 +1,21 @@
 package refdata;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
+import org.openlca.core.database.IDatabase;
 import org.openlca.core.database.derby.DerbyDatabase;
+import org.openlca.core.model.ModelType;
 import org.openlca.io.refdata.RefDataImport;
-import org.openlca.util.DQSystems;
+import org.openlca.jsonld.MemStore;
+import org.openlca.jsonld.input.JsonImport;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class Main {
 
@@ -29,7 +40,7 @@ public class Main {
 		if (dataDir != null) {
 			new RefDataImport(F("data/" + dataDir), db).run();
 			if ("all".equals(dataDir)) {
-				DQSystems.ecoinvent(db);
+				importDQS(db);
 			}
 		}
 		Util.embedUpdates(db);
@@ -39,6 +50,24 @@ public class Main {
 
 	private static File F(String path) {
 		return Util.F(path);
+	}
+
+	private static void importDQS(IDatabase db) throws Exception {
+		MemStore store = new MemStore();
+		String[] dqs = { "ecoinvent_dqs.json", "ilcd_dqs.json" };
+		for (String dq : dqs) {
+			System.out.println("  ... import DQS " + dq);
+			File f = F("data/dqs/" + dq);
+			try (InputStream stream = new FileInputStream(f);
+					Reader reader = new InputStreamReader(stream, "utf-8");
+					BufferedReader buffer = new BufferedReader(reader)) {
+				Gson gson = new Gson();
+				JsonObject obj = gson.fromJson(buffer, JsonObject.class);
+				store.put(ModelType.DQ_SYSTEM, obj);
+			}
+		}
+		JsonImport imp = new JsonImport(store, db);
+		imp.run();
 	}
 
 }
