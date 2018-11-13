cd8865064dc171358cc326d104156584a0d959c0
msrocka <michael.srocka@gmail.com>
2018-11-13 17:20:35 +0100

    use SWT browser widget in LocationMap + formatting
    JavaFX widget gets crazy with new heatmap ...

--- olca-app/src/org/openlca/app/results/contributions/locations/LocationMap.java
+++ olca-app/src/org/openlca/app/results/contributions/locations/LocationMap.java
@@ -2,10 +2,13 @@ package org.openlca.app.results.contributions.locations;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javafx.scene.web.WebEngine;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.ProgressAdapter;
+import org.eclipse.swt.browser.ProgressEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
@@ -13,7 +16,6 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 import org.openlca.app.M;
 import org.openlca.app.rcp.html.HtmlView;
-import org.openlca.app.rcp.html.WebPage;
 import org.openlca.app.rcp.images.Icon;
 import org.openlca.app.util.Actions;
 import org.openlca.app.util.UI;
@@ -25,11 +27,11 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 
-class LocationMap implements WebPage {
+class LocationMap {
 
 	private static final Logger log = LoggerFactory.getLogger(LocationMap.class);
 	private LocationPage page;
-	private WebEngine webkit;
+	private Browser browser;
 
 	static LocationMap create(LocationPage page, Composite body, FormToolkit tk) {
 		return new LocationMap(page, body, tk);
@@ -37,29 +39,30 @@ class LocationMap implements WebPage {
 
 	LocationMap(LocationPage page, Composite body, FormToolkit tk) {
 		this.page = page;
-		Section section = UI.section(body, tk, M.Map + " (beta)");
+		Section section = UI.section(body, tk, M.Map);
 		Actions.bind(section, new RefreshMapAction());
 		GridData gridData = UI.gridData(section, true, false);
 		gridData.widthHint = 800;
 		gridData.heightHint = 500;
 		Composite browserComp = UI.sectionClient(section, tk);
 		browserComp.setLayout(new FillLayout());
-		UI.createWebView(browserComp, this);
-	}
-
-	@Override
-	public String getUrl() {
-		return HtmlView.GMAP_HEATMAP.getUrl();
-	}
-
-	@Override
-	public void onLoaded(WebEngine webkit) {
-		this.webkit = webkit;
-		page.refreshSelection();
+		browser = new Browser(browserComp, SWT.NONE);
+		browser.setJavascriptEnabled(true);
+		AtomicBoolean loaded = new AtomicBoolean(false);
+		browser.addProgressListener(new ProgressAdapter() {
+			@Override
+			public void completed(ProgressEvent event) {
+				if (loaded.get())
+					return;
+				loaded.set(true);
+				page.refreshSelection();
+			}
+		});
+		browser.setUrl(HtmlView.GMAP_HEATMAP.getUrl());
 	}
 
 	void setInput(List<LocationItem> items) {
-		if (webkit == null)
+		if (browser == null)
 			return;
 		List<HeatmapPoint> points = new ArrayList<>();
 		for (LocationItem item : items) {
@@ -78,7 +81,7 @@ class LocationMap implements WebPage {
 		}
 		String json = new Gson().toJson(points);
 		try {
-			webkit.executeScript("setData(" + json + ")");
+			browser.execute("setData(" + json + ")");
 		} catch (Exception e) {
 			log.warn("Error setting location data", e);
 			Warning.showBox(M.MapCanNotBeDisplayed);
