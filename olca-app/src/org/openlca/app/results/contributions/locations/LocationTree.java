8135f9de39b5224d696a00f77c821a960e26f4ae
msrocka <michael.srocka@gmail.com>
2019-01-25 13:53:29 +0100

    slightly improved (?) the location contribution tree (see #66)

--- olca-app/src/org/openlca/app/results/contributions/locations/LocationTree.java
+++ olca-app/src/org/openlca/app/results/contributions/locations/LocationTree.java
@@ -2,14 +2,19 @@ package org.openlca.app.results.contributions.locations;
 
 import java.util.List;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.openlca.app.App;
 import org.openlca.app.M;
 import org.openlca.app.util.Actions;
 import org.openlca.app.util.UI;
 import org.openlca.app.util.trees.TreeClipboard;
 import org.openlca.app.util.trees.Trees;
+import org.openlca.app.util.viewers.Viewers;
+import org.openlca.core.model.descriptors.CategorizedDescriptor;
+import org.openlca.core.results.ContributionItem;
 
 /**
  * Table for showing the result contributions for locations of an analysis
@@ -27,7 +32,26 @@ class LocationTree {
 		viewer = Trees.createViewer(parent, labels, treeLabel);
 		viewer.setContentProvider(new TreeContentProvider());
 		Trees.bindColumnWidths(viewer.getTree(), 0.4, 0.3, 0.3);
-		Actions.bind(viewer, TreeClipboard.onCopy(viewer));
+
+		Action onOpen = Actions.onOpen(() -> {
+			Object obj = Viewers.getFirstSelected(viewer);
+			if (obj == null)
+				return;
+			if (obj instanceof LocationItem) {
+				LocationItem item = (LocationItem) obj;
+				if (item.contribution != null) {
+					App.openEditor(item.contribution.item);
+				}
+			}
+			if (obj instanceof ContributionItem) {
+				ContributionItem<?> item = (ContributionItem<?>) obj;
+				if (item.item instanceof CategorizedDescriptor) {
+					App.openEditor((CategorizedDescriptor) item.item);
+				}
+			}
+		});
+		Actions.bind(viewer, onOpen, TreeClipboard.onCopy(viewer));
+		Trees.onDoubleClick(viewer, e -> onOpen.run());
 		viewer.getTree().getColumns()[1].setAlignment(SWT.RIGHT);
 	}
 
