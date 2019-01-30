1fefee2ab78976246d84f0c4f96e30bfb3c2cc23
msrocka <michael.srocka@gmail.com>
2019-01-30 12:59:40 +0100

    fix the "-1 issue" of the formula cell editor
    -> with better focus handling

--- olca-app/src/org/openlca/app/components/FormulaCellEditor.java
+++ olca-app/src/org/openlca/app/components/FormulaCellEditor.java
@@ -8,6 +8,8 @@ import org.eclipse.jface.fieldassist.ContentProposalAdapter;
 import org.eclipse.jface.fieldassist.IContentProposalListener2;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
 import org.openlca.core.model.Exchange;
 import org.openlca.core.model.ImpactFactor;
 import org.openlca.core.model.Parameter;
@@ -15,23 +17,37 @@ import org.openlca.util.Strings;
 
 public class FormulaCellEditor extends TextCellEditor {
 
+	private final ContentProposalAdapter proposer;
+	private boolean hasFocus;
+
 	private String oldValue;
-	private boolean isPopupOpen = false;
 	private Object selectedElement = null;
 	private BiConsumer<Object, String> editFn;
 
 	public FormulaCellEditor(TableViewer table, Supplier<List<Parameter>> locals) {
 		super(table.getTable());
-		ContentProposalAdapter adapter = ParameterProposals.on(text, locals);
-		adapter.addContentProposalListener(new IContentProposalListener2() {
+		proposer = ParameterProposals.on(text, locals);
+		proposer.addContentProposalListener(new IContentProposalListener2() {
 			@Override
-			public void proposalPopupOpened(ContentProposalAdapter adapter) {
-				isPopupOpen = true;
+			public void proposalPopupOpened(ContentProposalAdapter a) {
 			}
 
 			@Override
-			public void proposalPopupClosed(ContentProposalAdapter adapter) {
-				isPopupOpen = false;
+			public void proposalPopupClosed(ContentProposalAdapter a) {
+				if (!hasFocus) {
+					focusLost();
+				}
+			}
+		});
+		getControl().addFocusListener(new FocusListener() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				hasFocus = false;
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				hasFocus = true;
 			}
 		});
 	}
@@ -42,20 +58,22 @@ public class FormulaCellEditor extends TextCellEditor {
 
 	@Override
 	protected void focusLost() {
-		if (!isPopupOpen) {
-			// Focus lost deactivates the cell editor.
-			// This must not happen if focus lost was caused by activating
-			// the completion proposal popup.
-			if (editFn != null) {
-				Object value = getValue();
-				String formula = value == null ? "" : value.toString();
-				if (!formula.equals(oldValue)) {
-					editFn.accept(selectedElement, formula);
-				}
+		if (proposer.isProposalPopupOpen())
+			return;
+		if (proposer.hasProposalPopupFocus())
+			return;
+		// Focus lost deactivates the cell editor.
+		// This must not happen if focus lost was caused by activating
+		// the completion proposal popup.
+		if (editFn != null) {
+			Object value = getValue();
+			String formula = value == null ? "" : value.toString();
+			if (!formula.equals(oldValue)) {
+				editFn.accept(selectedElement, formula);
 			}
-			selectedElement = null;
-			super.focusLost();
 		}
+		selectedElement = null;
+		super.focusLost();
 	}
 
 	@Override
