aa2c181ee46dd7a5ec884fecbb78f291496492f7
msrocka <michael.srocka@gmail.com>
2019-04-30 18:13:57 +0200

    Arrays.asList is ok:
    ```
    Arrays.asList(processes, methods, delete).forEach(b -> {
      Controls.onSelect(b, e -> setState.run());
    });
    ```
    
    since Java 9 there is a `List.of` method:
    
    ```
    List.of(processes, methods, delete).forEach(b -> {
      Controls.onSelect(b, e -> setState.run());
    });
    ```
    
    but this would be nicer:
    
    ```
    [processes, methods, delete].forEach(b -> {
      Controls.onSelect(b, e -> setState.run());
    });
    ```

--- olca-app/src/org/openlca/app/tools/mapping/ReplacerDialog.java
+++ olca-app/src/org/openlca/app/tools/mapping/ReplacerDialog.java
@@ -1,14 +1,18 @@
 package org.openlca.app.tools.mapping;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.openlca.app.tools.mapping.model.FlowMap;
 import org.openlca.app.tools.mapping.model.IMapProvider;
 import org.openlca.app.tools.mapping.model.ReplacerConfig;
+import org.openlca.app.util.Controls;
 import org.openlca.app.util.UI;
 
 class ReplacerDialog extends Dialog {
@@ -42,18 +46,48 @@ class ReplacerDialog extends Dialog {
 	@Override
 	protected void configureShell(Shell shell) {
 		super.configureShell(shell);
-		shell.setText("Open flow mapping");
+		shell.setText("Apply flow mapping");
 		UI.center(UI.shell(), shell);
 	}
 
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite root = (Composite) super.createDialogArea(parent);
-		UI.gridLayout(root, 2, 10, 10);
-		UI.formCheckBox(root, "Replace flows in processes");
-		UI.formCheckBox(root, "Replace flows in LCIA methods");
-		UI.formCheckBox(root, "Delete mapped and unused flows");
+		UI.gridLayout(root, 1, 10, 10);
+
+		Button processes = check(
+				root, "Replace flows in processes", conf.processes);
+		Button methods = check(
+				root, "Replace flows in LCIA methods", conf.methods);
+		Button delete = check(
+				root, "Delete mapped and unused flows", conf.deleteMapped);
+
+		Runnable setState = () -> {
+			conf.processes = processes.getSelection();
+			conf.methods = methods.getSelection();
+
+			if (conf.processes && conf.methods) {
+				delete.setEnabled(true);
+				conf.deleteMapped = delete.getSelection();
+			} else {
+				delete.setEnabled(false);
+				delete.setSelection(false);
+				conf.deleteMapped = false;
+			}
+		};
+		setState.run();
+
+		Arrays.asList(processes, methods, delete).forEach(b -> {
+			Controls.onSelect(b, e -> setState.run());
+		});
 		return root;
+	}
+
+	private Button check(Composite root, String label, boolean selected) {
+		Button b = new Button(root, SWT.CHECK);
+		b.setText(label);
+		b.setSelection(selected);
+		return b;
 	}
 
 }
