8e63f31298db872076b13cf48288600bb3e2dadb
msrocka <michael.srocka@gmail.com>
2019-05-03 16:13:30 +0200

    try to get the basic update right
    ```scheme
    (define cols
      '("F_FLOW"
        "F_UNIT"
        "F_FLOW_PROPERTY_FACTOR"
        "RESULTING_AMOUNT_VALUE"
        "RESULTING_AMOUNT_FORMULA"
        "DISTRIBUTION_TYPE"
        "PARAMETER1_VALUE"
        "PARAMETER2_VALUE"
        "PARAMETER3_VALUE"))
    
    (define (decorate pref suffix)
      (lambda (x)
        (string-append pref (string-downcase x) suffix)))
    
    (display
        (string-join
            (map (decorate "+ \" SET " " = ? ,\"") cols) "\n"))
    ```

--- olca-app/src/org/openlca/app/tools/mapping/model/Replacer.java
+++ olca-app/src/org/openlca/app/tools/mapping/model/Replacer.java
@@ -1,5 +1,8 @@
 package org.openlca.app.tools.mapping.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -17,6 +20,16 @@ public class Replacer implements Runnable {
 	private final IDatabase db;
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
+	/**
+	 * The valid entries that could be applied: source flow ID -> mapping.
+	 */
+	private final HashMap<Long, FlowMapEntry> entries = new HashMap<>();
+
+	/**
+	 * The source and target flows in the database: flow ID -> flow.
+	 */
+	private final HashMap<Long, Flow> flows = new HashMap<>();
+
 	public Replacer(ReplacerConfig conf) {
 		this.conf = conf;
 		this.db = Database.get();
@@ -29,17 +42,21 @@ public class Replacer implements Runnable {
 			return;
 		}
 
-		HashMap<Long, FlowMapEntry> index = buildIndex();
-		if (index.isEmpty()) {
+		buildIndices();
+		if (entries.isEmpty()) {
 			log.info("found no flows that can be mapped");
 			return;
 		}
-		log.info("found {} flows that can be mapped", index.size());
+		log.info("found {} flows that can be mapped", entries.size());
 
+		try {
+			inExchanges();
+		} catch (Exception e) {
+			log.error("Flow replacement failed", e);
+		}
 	}
 
-	private HashMap<Long, FlowMapEntry> buildIndex() {
-		HashMap<Long, FlowMapEntry> index = new HashMap<>();
+	private void buildIndices() {
 		FlowDao dao = new FlowDao(db);
 		for (FlowMapEntry entry : conf.mapping.entries) {
 
@@ -70,8 +87,69 @@ public class Replacer implements Runnable {
 				continue;
 			}
 
-			index.put(source.id, entry);
+			entries.put(source.id, entry);
+			flows.put(source.id, source);
+			flows.put(target.id, target);
 		}
-		return index;
+	}
+
+	private void inExchanges() throws Exception {
+
+		Connection con = db.createConnection();
+		con.setAutoCommit(false);
+
+		String query = "SELECT "
+				+ "f_flow, "
+				+ "f_unit, "
+				+ "f_flow_property_factor, "
+				+ "resulting_amount_value, "
+				+ "resulting_amount_formula, "
+				+ "distribution_type, "
+				+ "parameter1_value, "
+				+ "parameter2_value, "
+				+ "parameter3_value "
+
+				+ "FOR UPDATE OF "
+				+ "f_flow, "
+				+ "f_unit, "
+				+ "f_flow_property_factor, "
+				+ "resulting_amount_value, "
+				+ "resulting_amount_formula, "
+				+ "distribution_type, "
+				+ "parameter1_value, "
+				+ "parameter2_value, "
+				+ "parameter3_value";
+
+		Statement queryStmt = con.createStatement();
+		queryStmt.setCursorName("EXCHANGE_CURSOR");
+		ResultSet cursor = queryStmt.executeQuery(query);
+
+		String update = "UPDATE tbl_exchanges "
+				+ " SET f_flow = ? ,"
+				+ " SET f_unit = ? ,"
+				+ " SET f_flow_property_factor = ? ,"
+				+ " SET resulting_amount_value = ? ,"
+				+ " SET resulting_amount_formula = ? ,"
+				+ " SET distribution_type = ? ,"
+				+ " SET parameter1_value = ? ,"
+				+ " SET parameter2_value = ? ,"
+				+ " SET parameter3_value = ? "
+				+ "WHERE CURRENT OF EXCHANGE_CURSOR";
+		Statement updateStmt = con.prepareStatement(update);
+
+		int total = 0;
+		int changed = 0;
+		while (cursor.next()) {
+			total++;
+
+		}
+
+		cursor.close();
+		queryStmt.close();
+		updateStmt.close();
+		con.commit();
+		con.close();
+
+		log.info("Replaced flows in {} of {} exchanges", changed, total);
 	}
 }
