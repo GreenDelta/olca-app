2cc5d5feed81f0fa5183204686e8cfa726acc0fd
msrocka <michael.srocka@gmail.com>
2019-05-07 16:09:54 +0200

    the general structure is like this:
    ```scheme
    (define (replace config)
      (begin
        (if [get config 'replace-exchanges]
          (begin
            (display "replace in exchanges\n")
            (display "check amounts in product systems\n")))
        (if [get config 'replace-impacts]
          (display "replace in impacts\n"))
        (if [and
             (get config 'replace-exchanges)
             (get config 'replace-impacts)
             (get config 'delete-mapped)]
          (display "check and delete mapped"))))
    
    (replace
      (list
        (cons 'replace-exchanges #t)
        (cons 'replace-impacts   #t)
        (cons 'delete-mapped     #t)))
    ```
    
    we pass a configuration object with three flags into the
    `Replacer`.
    
    the replacement process in the exchanges and LCIA factors
    is very similar:
    
    * create an updatable cursor,
    * run through the table, replace the flows by applying
      conversion factors, and collect statistics
    * close cursor, commit the updates, log statistics
    
    ```scheme
    (define (cursor type)
      (let ([run (lambda ()
                    (display (format "run cursor ~a\n" type)))])
        (lambda (command)
          (cond
            [(equal? 'run command) (run)]))))
    
    ((cursor 'exchanges) 'run)
    ((cursor 'impacts) 'run)
    
    both cursors can run in parallel ...

--- olca-app/src/org/openlca/app/tools/mapping/model/Replacer.java
+++ olca-app/src/org/openlca/app/tools/mapping/model/Replacer.java
@@ -7,6 +7,9 @@ import java.sql.Statement;
 import java.sql.Types;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.openlca.app.db.Database;
 import org.openlca.app.tools.mapping.model.FlowMapEntry.SyncState;
@@ -38,10 +41,6 @@ public class Replacer implements Runnable {
 	// the source and target flows in the database: flow ID -> flow.
 	private final HashMap<Long, Flow> flows = new HashMap<>();
 
-	// collected statistics
-	private final Stats stats = new Stats();
-	private final HashMap<Long, Stats> flowStats = new HashMap<>();
-
 	private ConversionTable conversions;
 
 	public Replacer(ReplacerConfig conf) {
@@ -64,13 +63,28 @@ public class Replacer implements Runnable {
 		log.info("found {} flows that can be mapped", entries.size());
 
 		try {
+
+			log.info("start updatable cursors");
+			Cursor exchangeCursor = null;
+			Cursor impactCursor = null;
+			ExecutorService pool = Executors.newFixedThreadPool(4);
 			if (conf.processes) {
-				inExchanges();
-				// TODO: replace possible products in product systems (qref)
+				exchangeCursor = new Cursor(Cursor.EXCHANGES);
+				pool.execute(exchangeCursor);
 			}
 			if (conf.methods) {
-				// TODO: replace in LCIA factors
+				impactCursor = new Cursor(Cursor.IMPACTS);
+				pool.execute(impactCursor);
 			}
+			pool.shutdown();
+			int i = 0;
+			while (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+				i++;
+				log.info("waiting for cursors to finish; {} seconds", i * 10);
+			}
+			log.info("cursors finished");
+
+			// TODO: replace possible
 			if (conf.processes && conf.methods && conf.deleteMapped) {
 				// TODO: delete the flows with no failures
 			}
@@ -118,224 +132,245 @@ public class Replacer implements Runnable {
 		conversions = ConversionTable.create(db);
 	}
 
-	private void inExchanges() throws Exception {
-
-		Connection con = db.createConnection();
-		con.setAutoCommit(false);
-
-		String query = "SELECT "
-				+ "f_flow, "
-				+ "f_unit, "
-				+ "f_flow_property_factor, "
-				+ "resulting_amount_value, "
-				+ "resulting_amount_formula, "
-				+ "distribution_type, "
-				+ "parameter1_value, "
-				+ "parameter2_value, "
-				+ "parameter3_value "
-
-				+ "FROM tbl_exchanges "
-				+ "FOR UPDATE OF "
-				+ "f_flow, "
-				+ "f_unit, "
-				+ "f_flow_property_factor, "
-				+ "resulting_amount_value, "
-				+ "resulting_amount_formula, "
-				+ "distribution_type, "
-				+ "parameter1_value, "
-				+ "parameter2_value, "
-				+ "parameter3_value";
-
-		Statement queryStmt = con.createStatement();
-		queryStmt.setCursorName("EXCHANGE_CURSOR");
-		ResultSet cursor = queryStmt.executeQuery(query);
-
-		String update = "UPDATE tbl_exchanges "
-				+ " SET f_flow = ? ,"
-				+ " f_unit = ? ,"
-				+ " f_flow_property_factor = ? ,"
-				+ " resulting_amount_value = ? ,"
-				+ " resulting_amount_formula = ? ,"
-				+ " distribution_type = ? ,"
-				+ " parameter1_value = ? ,"
-				+ " parameter2_value = ? ,"
-				+ " parameter3_value = ? "
-				+ "WHERE CURRENT OF EXCHANGE_CURSOR";
-		PreparedStatement updateStmt = con.prepareStatement(update);
-
-		int total = 0;
-		int changed = 0;
-		while (cursor.next()) {
-			total++;
-
-			long flowID = cursor.getLong(1);
-			FlowMapEntry entry = entries.get(flowID);
-			if (entry == null)
-				continue;
-			Flow source = flows.get(entry.sourceFlow.flow.id);
-			Flow target = flows.get(entry.targetFlow.flow.id);
-			if (source == null || target == null)
-				continue;
-
-			// check flow property and unit of the source flow
-			FlowPropertyFactor propFactor = propFactor(
-					source, cursor.getLong("f_flow_property_factor"));
-			if (propFactor == null
-					|| propFactor.flowProperty.id != entry.sourceFlow.flowProperty.id) {
-				incStats(source.id, FAILURE);
-				continue;
-			}
-			Unit unit = unit(propFactor.flowProperty, cursor.getLong("f_unit"));
-			if (unit == null) {
-				incStats(source.id, FAILURE);
-				continue;
-			}
-
-			double factor = entry.factor;
-			if (propFactor.flowProperty.id != entry.sourceFlow.flowProperty.id
-					|| unit.id != entry.sourceFlow.unit.id) {
-				double pi = conversions.getPropertyFactor(propFactor.id);
-				double ui = conversions.getUnitFactor(unit.id);
-				double ps = conversions.getPropertyFactor(
-						propFactor(source, entry.sourceFlow).id);
-				double us = conversions.getUnitFactor(entry.sourceFlow.unit.id);
-				factor *= (ui * ps) / (pi * us);
-			}
-
-			// check the target flow property
-			FlowPropertyFactor targetPropertyFactor = propFactor(
-					target, entry.targetFlow);
-			if (targetPropertyFactor == null) {
-				incStats(source.id, FAILURE);
-				continue;
-			}
-
-			double amount = cursor.getDouble("resulting_amount_value");
-			String formula = cursor.getString("resulting_amount_formula");
-
-			Uncertainty uncertainty = null;
-			int uncertaintyType = cursor.getInt("distribution_type");
-			if (!cursor.wasNull()) {
-				UncertaintyType ut = UncertaintyType.values()[uncertaintyType];
-				switch (ut) {
-				case LOG_NORMAL:
-					uncertainty = Uncertainty.logNormal(
-							cursor.getDouble("parameter1_value"),
-							cursor.getDouble("parameter2_value"));
-					break;
-				case NORMAL:
-					uncertainty = Uncertainty.normal(
-							cursor.getDouble("parameter1_value"),
-							cursor.getDouble("parameter2_value"));
-					break;
-				case TRIANGLE:
-					uncertainty = Uncertainty.triangle(
-							cursor.getDouble("parameter1_value"),
-							cursor.getDouble("parameter2_value"),
-							cursor.getDouble("parameter3_value"));
-					break;
-				case UNIFORM:
-					uncertainty = Uncertainty.uniform(
-							cursor.getDouble("parameter1_value"),
-							cursor.getDouble("parameter2_value"));
-					break;
-				default:
-					break;
-				}
-			}
-
-			updateStmt.setLong(1, target.id); // f_flow
-			updateStmt.setLong(2, entry.targetFlow.unit.id); // f_unit
-			updateStmt.setLong(3, targetPropertyFactor.id); // f_flow_property_factor
-			updateStmt.setDouble(4, factor * amount); // resulting_amount_value
-
-			// resulting_amount_formula
-			if (Strings.nullOrEmpty(formula)) {
-				updateStmt.setString(5, null);
-			} else {
-				updateStmt.setString(5,
-						Double.toString(factor) + "* (" + formula + ")");
-			}
-
-			// uncertainty
-			if (uncertainty == null) {
-				updateStmt.setNull(6, Types.INTEGER);
-				updateStmt.setNull(7, Types.DOUBLE);
-				updateStmt.setNull(8, Types.DOUBLE);
-				updateStmt.setNull(9, Types.DOUBLE);
-			} else {
-				updateStmt.setInt(6, uncertainty.distributionType.ordinal());
-				updateStmt.setDouble(7, uncertainty.parameter1);
-				updateStmt.setDouble(8, uncertainty.parameter2);
-				if (uncertainty.parameter3 != null) {
-					updateStmt.setDouble(9, uncertainty.parameter3);
-				}
-			}
-
-			updateStmt.executeUpdate();
-			changed++;
-			incStats(source.id, REPLACEMENT);
-		}
-
-		cursor.close();
-		queryStmt.close();
-		updateStmt.close();
-		con.commit();
-		con.close();
-
-		log.info("Replaced flows in {} of {} exchanges", changed, total);
-	}
-
-	private FlowPropertyFactor propFactor(Flow flow, long factorID) {
-		if (flow == null)
-			return null;
-		for (FlowPropertyFactor f : flow.flowPropertyFactors) {
-			if (f.id == factorID)
-				return f;
-		}
-		return null;
-	}
-
-	private FlowPropertyFactor propFactor(Flow flow, FlowRef ref) {
-		if (flow == null)
-			return null;
-		for (FlowPropertyFactor f : flow.flowPropertyFactors) {
-			if (f.flowProperty == null)
-				continue;
-			if (f.flowProperty.id == ref.flowProperty.id)
-				return f;
-		}
-		return null;
-	}
-
-	private Unit unit(FlowProperty property, long unitID) {
-		if (property == null || property.unitGroup == null)
-			return null;
-		for (Unit u : property.unitGroup.units) {
-			if (u.id == unitID)
-				return u;
-		}
-		return null;
-	}
-
-	private void incStats(long flowID, int type) {
-		Stats fstats = flowStats.get(flowID);
-		if (fstats == null) {
-			fstats = new Stats();
-			flowStats.put(flowID, fstats);
-		}
-		if (type == REPLACEMENT) {
-			stats.replacements++;
-			fstats.replacements++;
-		} else {
-			stats.failures++;
-			fstats.failures++;
-		}
-	}
-
 	class Stats {
 		int failures;
 		int replacements;
+	}
+
+	private class Cursor implements Runnable {
+
+		static final int EXCHANGES = 0;
+		static final int IMPACTS = 1;
+
+		final int type;
+
+		// collected statistics
+		final Stats stats = new Stats();
+		final HashMap<Long, Stats> flowStats = new HashMap<>();
+
+		Cursor(int type) {
+			this.type = type;
+		}
+
+		public void run() {
+			try {
+
+				Connection con = db.createConnection();
+				con.setAutoCommit(false);
+
+				String query = "SELECT "
+						+ "f_flow, "
+						+ "f_unit, "
+						+ "f_flow_property_factor, "
+						+ "resulting_amount_value, "
+						+ "resulting_amount_formula, "
+						+ "distribution_type, "
+						+ "parameter1_value, "
+						+ "parameter2_value, "
+						+ "parameter3_value "
+
+						+ "FROM tbl_exchanges "
+						+ "FOR UPDATE OF "
+						+ "f_flow, "
+						+ "f_unit, "
+						+ "f_flow_property_factor, "
+						+ "resulting_amount_value, "
+						+ "resulting_amount_formula, "
+						+ "distribution_type, "
+						+ "parameter1_value, "
+						+ "parameter2_value, "
+						+ "parameter3_value";
+
+				Statement queryStmt = con.createStatement();
+				queryStmt.setCursorName("EXCHANGE_CURSOR");
+				ResultSet cursor = queryStmt.executeQuery(query);
+
+				String update = "UPDATE tbl_exchanges "
+						+ " SET f_flow = ? ,"
+						+ " f_unit = ? ,"
+						+ " f_flow_property_factor = ? ,"
+						+ " resulting_amount_value = ? ,"
+						+ " resulting_amount_formula = ? ,"
+						+ " distribution_type = ? ,"
+						+ " parameter1_value = ? ,"
+						+ " parameter2_value = ? ,"
+						+ " parameter3_value = ? "
+						+ "WHERE CURRENT OF EXCHANGE_CURSOR";
+				PreparedStatement updateStmt = con.prepareStatement(update);
+
+				int total = 0;
+				int changed = 0;
+				while (cursor.next()) {
+					total++;
+
+					long flowID = cursor.getLong(1);
+					FlowMapEntry entry = entries.get(flowID);
+					if (entry == null)
+						continue;
+					Flow source = flows.get(entry.sourceFlow.flow.id);
+					Flow target = flows.get(entry.targetFlow.flow.id);
+					if (source == null || target == null)
+						continue;
+
+					// check flow property and unit of the source flow
+					FlowPropertyFactor propFactor = propFactor(
+							source, cursor.getLong("f_flow_property_factor"));
+					if (propFactor == null
+							|| propFactor.flowProperty.id != entry.sourceFlow.flowProperty.id) {
+						incStats(source.id, FAILURE);
+						continue;
+					}
+					Unit unit = unit(propFactor.flowProperty, cursor.getLong("f_unit"));
+					if (unit == null) {
+						incStats(source.id, FAILURE);
+						continue;
+					}
+
+					double factor = entry.factor;
+					if (propFactor.flowProperty.id != entry.sourceFlow.flowProperty.id
+							|| unit.id != entry.sourceFlow.unit.id) {
+						double pi = conversions.getPropertyFactor(propFactor.id);
+						double ui = conversions.getUnitFactor(unit.id);
+						double ps = conversions.getPropertyFactor(
+								propFactor(source, entry.sourceFlow).id);
+						double us = conversions.getUnitFactor(entry.sourceFlow.unit.id);
+						factor *= (ui * ps) / (pi * us);
+					}
+
+					// check the target flow property
+					FlowPropertyFactor targetPropertyFactor = propFactor(
+							target, entry.targetFlow);
+					if (targetPropertyFactor == null) {
+						incStats(source.id, FAILURE);
+						continue;
+					}
+
+					double amount = cursor.getDouble("resulting_amount_value");
+					String formula = cursor.getString("resulting_amount_formula");
+
+					Uncertainty uncertainty = null;
+					int uncertaintyType = cursor.getInt("distribution_type");
+					if (!cursor.wasNull()) {
+						UncertaintyType ut = UncertaintyType.values()[uncertaintyType];
+						switch (ut) {
+						case LOG_NORMAL:
+							uncertainty = Uncertainty.logNormal(
+									cursor.getDouble("parameter1_value"),
+									cursor.getDouble("parameter2_value"));
+							break;
+						case NORMAL:
+							uncertainty = Uncertainty.normal(
+									cursor.getDouble("parameter1_value"),
+									cursor.getDouble("parameter2_value"));
+							break;
+						case TRIANGLE:
+							uncertainty = Uncertainty.triangle(
+									cursor.getDouble("parameter1_value"),
+									cursor.getDouble("parameter2_value"),
+									cursor.getDouble("parameter3_value"));
+							break;
+						case UNIFORM:
+							uncertainty = Uncertainty.uniform(
+									cursor.getDouble("parameter1_value"),
+									cursor.getDouble("parameter2_value"));
+							break;
+						default:
+							break;
+						}
+					}
+
+					updateStmt.setLong(1, target.id); // f_flow
+					updateStmt.setLong(2, entry.targetFlow.unit.id); // f_unit
+					updateStmt.setLong(3, targetPropertyFactor.id); // f_flow_property_factor
+					updateStmt.setDouble(4, factor * amount); // resulting_amount_value
+
+					// resulting_amount_formula
+					if (Strings.nullOrEmpty(formula)) {
+						updateStmt.setString(5, null);
+					} else {
+						updateStmt.setString(5,
+								Double.toString(factor) + "* (" + formula + ")");
+					}
+
+					// uncertainty
+					if (uncertainty == null) {
+						updateStmt.setNull(6, Types.INTEGER);
+						updateStmt.setNull(7, Types.DOUBLE);
+						updateStmt.setNull(8, Types.DOUBLE);
+						updateStmt.setNull(9, Types.DOUBLE);
+					} else {
+						updateStmt.setInt(6, uncertainty.distributionType.ordinal());
+						updateStmt.setDouble(7, uncertainty.parameter1);
+						updateStmt.setDouble(8, uncertainty.parameter2);
+						if (uncertainty.parameter3 != null) {
+							updateStmt.setDouble(9, uncertainty.parameter3);
+						}
+					}
+
+					updateStmt.executeUpdate();
+					changed++;
+					incStats(source.id, REPLACEMENT);
+				}
+
+				cursor.close();
+				queryStmt.close();
+				updateStmt.close();
+				con.commit();
+				con.close();
+
+				log.info("Replaced flows in {} of {} exchanges", changed, total);
+			} catch (Exception e) {
+				String t = type == EXCHANGES ? "exchanges" : "LCIA factors";
+				log.error("Flow replacement in " + t + " failed", e);
+			}
+		}
+
+		private FlowPropertyFactor propFactor(Flow flow, long factorID) {
+			if (flow == null)
+				return null;
+			for (FlowPropertyFactor f : flow.flowPropertyFactors) {
+				if (f.id == factorID)
+					return f;
+			}
+			return null;
+		}
+
+		private FlowPropertyFactor propFactor(Flow flow, FlowRef ref) {
+			if (flow == null)
+				return null;
+			for (FlowPropertyFactor f : flow.flowPropertyFactors) {
+				if (f.flowProperty == null)
+					continue;
+				if (f.flowProperty.id == ref.flowProperty.id)
+					return f;
+			}
+			return null;
+		}
+
+		private Unit unit(FlowProperty property, long unitID) {
+			if (property == null || property.unitGroup == null)
+				return null;
+			for (Unit u : property.unitGroup.units) {
+				if (u.id == unitID)
+					return u;
+			}
+			return null;
+		}
+
+		private void incStats(long flowID, int type) {
+			Stats fstats = flowStats.get(flowID);
+			if (fstats == null) {
+				fstats = new Stats();
+				flowStats.put(flowID, fstats);
+			}
+			if (type == REPLACEMENT) {
+				stats.replacements++;
+				fstats.replacements++;
+			} else {
+				stats.failures++;
+				fstats.failures++;
+			}
+		}
 	}
 
 }
