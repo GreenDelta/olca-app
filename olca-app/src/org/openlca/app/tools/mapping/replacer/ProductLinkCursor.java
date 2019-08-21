4d41e47fae2da090fd4921f68806d546bf36e297
msrocka <michael.srocka@gmail.com>
2019-08-21 16:52:13 +0200

    when replacing product and waste flows flows that are
    linked in product systems, setting the provider in the
    mapping is really important. otherwise, the product systems
    may become invalid... a warning in the replacer would be
    good ...

---
+++ olca-app/src/org/openlca/app/tools/mapping/replacer/ProductLinkCursor.java
@@ -0,0 +1,70 @@
+package org.openlca.app.tools.mapping.replacer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.openlca.io.maps.FlowMapEntry;
+
+class ProductLinkCursor extends UpdatableCursor {
+
+	private final Replacer replacer;
+
+	ProductLinkCursor(Replacer replacer) {
+		super(replacer.db);
+		this.replacer = replacer;
+	}
+
+	@Override
+	boolean next(ResultSet cursor, PreparedStatement update) {
+		long flowID = -1;
+		try {
+
+			// select the mapping entry if exists
+			flowID = cursor.getLong("f_flow");
+			FlowMapEntry entry = replacer.entries.get(flowID);
+			if (entry == null)
+				return false;
+
+			// check whether we should update the link
+			long processID = cursor.getLong("f_process");
+			if (!replacer.processes.contains(processID))
+				return false;
+
+			// select the provider
+			long provider = 0L;
+			if (entry.targetFlow.provider != null) {
+				provider = entry.targetFlow.provider.id;
+			} else {
+				provider = cursor.getLong("f_provider");
+			}
+
+			update.setLong(1, provider);
+			update.setLong(2, entry.targetFlow.flow.id);
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	String querySQL() {
+		return "SELECT "
+				+ " f_product_system,"
+				+ " f_provider,"
+				+ " f_flow,"
+				+ " f_process"
+				+ " FROM tbl_process_links"
+				+ " FOR UPDATE OF "
+				+ " f_provider, "
+				+ " f_flow";
+	}
+
+	@Override
+	String updateSQL() {
+		return "UPDATE tbl_process_links"
+				+ " SET f_provider = ? , "
+				+ " f_flow = ? ";
+	}
+
+}
