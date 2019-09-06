0dd79ab4795016c44920ccabf3e8d7906307d6e4
msrocka <michael.srocka@gmail.com>
2019-09-06 16:54:58 +0200

    replace the JavaFX view in the Python editor
    when we want to return a value from a JavaScript function,
    we have to call `browser.evaluate("return fn();")`:
    https://stackoverflow.com/a/30474856/599575

--- olca-app/src/org/openlca/app/devtools/python/PythonEditor.java
+++ olca-app/src/org/openlca/app/devtools/python/PythonEditor.java
@@ -2,15 +2,24 @@ package org.openlca.app.devtools.python;
 
 import java.util.UUID;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.openlca.app.App;
 import org.openlca.app.devtools.IScriptEditor;
-import org.openlca.app.devtools.ScriptEditorPage;
 import org.openlca.app.editors.Editors;
 import org.openlca.app.editors.SimpleEditorInput;
 import org.openlca.app.editors.SimpleFormEditor;
 import org.openlca.app.rcp.html.HtmlView;
 import org.openlca.app.rcp.images.Icon;
+import org.openlca.app.util.UI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PythonEditor extends SimpleFormEditor implements IScriptEditor {
 
@@ -18,7 +27,8 @@ public class PythonEditor extends SimpleFormEditor implements IScriptEditor {
 	private Page page;
 
 	public static void open() {
-		Editors.open(new SimpleEditorInput(TYPE, UUID.randomUUID().toString(), "Python"), TYPE);
+		Editors.open(new SimpleEditorInput(
+				TYPE, UUID.randomUUID().toString(), "Python"), TYPE);
 	}
 
 	@Override
@@ -32,16 +42,42 @@ public class PythonEditor extends SimpleFormEditor implements IScriptEditor {
 		App.run("Eval script", () -> Python.eval(script));
 	}
 
-	private class Page extends ScriptEditorPage {
+	private class Page extends FormPage {
+
+		private Browser browser;
 
 		public Page() {
-			super(PythonEditor.this, "PythonEditorPage", "Python", Icon.PYTHON.get());
+			super(PythonEditor.this, "PythonEditorPage", "Python");
 		}
 
 		@Override
-		public String getUrl() {
-			return HtmlView.PYTHON_EDITOR.getUrl();
+		protected void createFormContent(IManagedForm mform) {
+			ScrolledForm form = UI.formHeader(
+					mform, getTitle(), Icon.PYTHON.get());
+			FormToolkit toolkit = mform.getToolkit();
+			Composite body = UI.formBody(form, toolkit);
+			body.setLayout(new FillLayout());
+			try {
+				browser = new Browser(body, SWT.NONE);
+				browser.setJavascriptEnabled(true);
+				browser.setUrl(HtmlView.PYTHON_EDITOR.getUrl());
+			} catch (Exception e) {
+				Logger log = LoggerFactory.getLogger(getClass());
+				log.error("failed to create browser in Python editor", e);
+			}
 		}
 
+		public String getScript() {
+			try {
+				Object script = browser.evaluate("return getContent();");
+				if (script == null)
+					return "";
+				return script.toString();
+			} catch (Exception e) {
+				Logger log = LoggerFactory.getLogger(getClass());
+				log.error("failed to get script content", e);
+				return "";
+			}
+		}
 	}
 }
