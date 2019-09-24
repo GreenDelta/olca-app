2ffc4817e8025f15899ee1b96926c5f5b1f50199
msrocka <michael.srocka@gmail.com>
2019-09-24 05:32:14 +0200

    `org.eclipse.equinox.ds` was replaced with
    `org.apache.felix.scr` in the latest Eclipse versions; see
    https://stackoverflow.com/a/54788602. this throws errors like
    this when validating the target platform:
    
    ```
    Missing constraint osgi.service org.osgi.service.event.EventAdmin
    ```
    
    we have to replace this also in the plugin start config:
    
    ```xml
    <configurations>
       <plugin id="org.eclipse.core.runtime" autoStart="true" startLevel="4"
    />
       <plugin id="org.eclipse.equinox.common" autoStart="true"
    startLevel="2" />
       <plugin id="org.eclipse.equinox.ds" autoStart="true" startLevel="2"
    />
       <plugin id="org.eclipse.equinox.event" autoStart="true"
    startLevel="2" />
       <plugin id="org.eclipse.equinox.simpleconfigurator" autoStart="true"
    startLevel="1" />
    </configurations>
    ```
    
    also, we should add the required plugins to the app manifest.
    
    update the platform to Eclipse 4.12, we need to add the following
    things:
    
    * Equinox Target Components
    * Eclipse RCP Target Components:
      * Eclipse RCP
      * Eclipse RCP Plugin Dev. Resources
    
    also, we switch to the Notifier from the Nebula project ... with Mylyn
    we
    always get strange dependency errors
    
    https://wiki.eclipse.org/Nebula_Notifier

--- olca-app/src/org/openlca/app/util/Popup.java
+++ olca-app/src/org/openlca/app/util/Popup.java
@@ -3,21 +3,16 @@ package org.openlca.app.util;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.mylyn.commons.ui.dialogs.AbstractNotificationPopup;
-import org.eclipse.swt.SWT;
+import org.eclipse.nebula.widgets.opal.notifier.Notifier;
+import org.eclipse.nebula.widgets.opal.notifier.NotifierColorsFactory.NotifierTheme;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.progress.UIJob;
 import org.openlca.app.rcp.images.Icon;
-import org.openlca.util.Strings;
 
 /**
- * A pop-up for messages. For spezialized versions look at inheriting classes
+ * A pop-up for messages. For specialized versions look at inheriting classes
  * like {@link InformationPopup}.
  */
 class Popup {
@@ -25,7 +20,6 @@ class Popup {
 	private String message;
 	private String title;
 	private Icon icon = Icon.INFO;
-	private String defaultTitle = "Popup";
 
 	public Popup() {
 
@@ -45,7 +39,7 @@ class Popup {
 	}
 
 	Popup defaultTitle(String t) {
-		this.defaultTitle = t;
+		this.title = t;
 		return this;
 	}
 
@@ -54,34 +48,14 @@ class Popup {
 		return this;
 	}
 
-	private String getDefaultTitle() {
-		return defaultTitle;
-	}
-
 	public Image getPopupShellImage() {
 		return icon.get();
 	}
 
-	private void createLabel(Composite composite) {
-		Label label = new Label(composite, SWT.WRAP);
-		label.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		label.setText(Strings.cut(message, 500));
-		label.setBackground(composite.getBackground());
-	}
-
 	/**
 	 * Override this to add something below the label.
-	 * 
-	 * @param composite
 	 */
 	protected void makeLink(Composite composite) {
-	}
-
-	/**
-	 * Default implementation just calls makeLink.
-	 */
-	private void makeLink(PopupImpl popupImpl, Composite composite) {
-		makeLink(composite);
 	}
 
 	void show() {
@@ -91,55 +65,11 @@ class Popup {
 				Display display = getDisplay();
 				if (display == null || display.isDisposed())
 					return Status.CANCEL_STATUS;
-				new PopupImpl(display).open();
+				Notifier.notify(icon.get(), title, message,
+						NotifierTheme.GRAY_THEME);
 				return Status.OK_STATUS;
 			}
 		};
 		job.schedule();
-	}
-
-	private class PopupImpl extends AbstractNotificationPopup {
-
-		private PopupImpl(Display display) {
-			super(display);
-		}
-
-		@Override
-		protected String getPopupShellTitle() {
-			if (title != null)
-				return title;
-			return getDefaultTitle();
-		}
-
-		@Override
-		protected void initializeBounds() {
-			// Georg: Workaround for a sizing bug in the extended implementation
-			super.initializeBounds();
-			Point currentSize = getShell().getSize();
-			Point currentLoc = getShell().getLocation();
-
-			Point newSize = getShell().computeSize(400, SWT.DEFAULT);
-
-			int widthDiff = newSize.x - currentSize.x;
-			int heightDiff = newSize.y - currentSize.y;
-			Point newLoc = new Point(currentLoc.x - widthDiff, //
-					currentLoc.y - heightDiff);
-
-			getShell().setLocation(newLoc);
-			getShell().setSize(newSize);
-		}
-
-		@Override
-		protected Image getPopupShellImage(int maximumHeight) {
-			return Popup.this.getPopupShellImage();
-		}
-
-		@Override
-		protected void createContentArea(Composite composite) {
-			composite.setLayout(new GridLayout(1, true));
-			createLabel(composite);
-			makeLink(this, composite);
-		}
-
 	}
 }
