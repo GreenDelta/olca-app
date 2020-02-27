e5ccde0b1ff94958967308e931128145ee96419f
msrocka <michael.srocka@gmail.com>
2020-02-27 21:05:01 +0100

    next: calculate the aggregated parameter
    values for the respective locations; then,
    calculate the LCIA factors for the respective
    parameter and location combinations using the
    formulas from the bindings... we should document
    this
    
    also:
     * update then the LCIA factor table
     * support the calculation for a subset
       of the database locations (mainly for
       testing as the intersection calculation
       can take some time)
     * show the calculated default factors in
       the setup page (need communication between
       the tables ...same as between the tabs
       for the factors)
    
    _

--- olca-app/src/org/openlca/app/editors/lcia/geo/GeoFactorCalculator.java
+++ olca-app/src/org/openlca/app/editors/lcia/geo/GeoFactorCalculator.java
@@ -1,12 +1,13 @@
 package org.openlca.app.editors.lcia.geo;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.openlca.app.db.Database;
 import org.openlca.core.database.IDatabase;
-import org.openlca.core.database.LocationDao;
 import org.openlca.core.model.ImpactCategory;
 import org.openlca.core.model.ImpactFactor;
 import org.openlca.core.model.Location;
@@ -17,6 +18,7 @@ import org.openlca.geo.geojson.FeatureCollection;
 import org.openlca.geo.geojson.MsgPack;
 import org.openlca.util.BinUtils;
 import org.openlca.util.Pair;
+import org.openlca.util.Triple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,9 +54,6 @@ class GeoFactorCalculator implements Runnable {
 			return;
 		}
 
-		clearFactors();
-		TLongDoubleHashMap defaults = calcDefaultValues();
-
 		// calculate the intersections
 		FeatureCollection coll = setup.getFeatures();
 		if (coll == null || coll.features.isEmpty()) {
@@ -62,12 +61,16 @@ class GeoFactorCalculator implements Runnable {
 					+ "intersection calculation");
 			return;
 		}
-		IntersectionCalculator calc = IntersectionCalculator.on(coll);
-		LocationDao locDao = new LocationDao(db);
-		List<Pair<Location, List<Pair<Feature, Double>>>> intersections = locDao.getAll()
-				.parallelStream()
-				.map(loc -> Pair.of(loc, getIntersections(loc, calc)))
-				.collect(Collectors.toList());
+
+		clearFactors();
+		TLongDoubleHashMap defaults = calcDefaultValues();
+
+//		IntersectionCalculator calc = IntersectionCalculator.on(coll);
+//		LocationDao locDao = new LocationDao(db);
+//		List<Pair<Location, List<Pair<Feature, Double>>>> intersections = locDao.getAll()
+//				.parallelStream()
+//				.map(loc -> Pair.of(loc, getIntersections(loc, calc)))
+//				.collect(Collectors.toList());
 	}
 
 	/**
@@ -120,7 +123,51 @@ class GeoFactorCalculator implements Runnable {
 		return defaults;
 	}
 
-	private List<Pair<Feature, Double>> getIntersections(
+	/**
+	 * Calculates the parameter values for the given locations from the respective
+	 * intersections with the given feature collection and the aggregation function
+	 * that is defined in the respective parameter.
+	 */
+	private List<Triple<Location, GeoParam, Double>> calcParamVals(
+			List<Location> locations, FeatureCollection coll) {
+		IntersectionCalculator calc = IntersectionCalculator.on(coll);
+		Map<Location, List<Pair<Feature, Double>>> map = locations.stream()
+				.map(loc -> Pair.of(loc, calcIntersections(loc, calc)))
+				.collect(Collectors.toMap(p -> p.first, p -> p.second));
+
+		List<Triple<Location, GeoParam, Double>> triples = new ArrayList<>();
+		map.forEach((loc, pairs) -> {
+			for (GeoParam param : setup.params) {
+				if (pairs.isEmpty()) {
+					triples.add(Triple.of(loc, param, null));
+					continue;
+				}
+
+				List<Double> vals = new ArrayList<>();
+				List<Double> shares = new ArrayList<>();
+				for (Pair<Feature, Double> pair : pairs) {
+					Feature f = pair.first;
+					Double share = pair.second;
+					if (f.properties == null)
+						continue;
+					Object valObj = f.properties.get(param.name);
+					if (!(valObj instanceof Number))
+						continue;
+					vals.add(((Number) valObj).doubleValue());
+					shares.add(share);
+				}
+				Double aggVal = aggregate(param, vals, shares);
+				triples.add(Triple.of(loc, param, aggVal));
+
+			}
+		});
+		return triples;
+	}
+
+	/**
+	 * Calculates the intersection of the given location.
+	 */
+	private List<Pair<Feature, Double>> calcIntersections(
 			Location loc, IntersectionCalculator calc) {
 		if (loc.geodata == null) {
 			log.info("No geodata for location {} found", loc);
@@ -151,6 +198,12 @@ class GeoFactorCalculator implements Runnable {
 					+ "intersections for location " + loc, e);
 			return Collections.emptyList();
 		}
+	}
+
+	private Double aggregate(GeoParam param,
+			List<Double> vals, List<Double> shares) {
+
+		return null;
 	}
 
 }
