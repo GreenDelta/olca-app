0e6d430592c397d21bdad2a4e8a98031d89639bf
msrocka <michael.srocka@gmail.com>
2020-10-09 12:19:18 +0200

    render the flow icons
    * product | waste | elementary
    * adding icons is easy as the `ExchangeFigure` already extends `Label`
    * maybe this should be a settings option?, depends on the performance

--- olca-app/src/org/openlca/app/editors/graphical/model/ExchangeFigure.java
+++ olca-app/src/org/openlca/app/editors/graphical/model/ExchangeFigure.java
@@ -10,6 +10,7 @@ import org.eclipse.swt.graphics.Image;
 import org.openlca.app.M;
 import org.openlca.app.editors.graphical.layout.Animation;
 import org.openlca.app.rcp.images.Icon;
+import org.openlca.app.rcp.images.Images;
 import org.openlca.app.util.Labels;
 import org.openlca.app.util.Numbers;
 import org.openlca.core.model.Exchange;
@@ -21,45 +22,45 @@ class ExchangeFigure extends Label {
 	private static final Color BACKGROUND_COLOR = ColorConstants.white;
 	private static final Color TEXT_COLOR = ColorConstants.gray;
 	private static final Color TEXT_HIGHLIGHTED_COLOR = ColorConstants.lightBlue;
-	private ExchangeNode node;
+	private final ExchangeNode node;
 
 	ExchangeFigure(ExchangeNode node) {
 		this.node = node;
 		if (node.isDummy())
 			return;
-		Exchange exchange = node.exchange;
+		var exchange = node.exchange;
 		setBorder(new LineBorder(ColorConstants.white, 1));
-		setForegroundColor(exchange.isAvoided ? BACKGROUND_COLOR : TEXT_COLOR);
+		setForegroundColor(exchange.isAvoided
+				? BACKGROUND_COLOR
+				: TEXT_COLOR);
 		setBackgroundColor(BACKGROUND_COLOR);
-		setToolTip(new Label(getToolTipText()));
+		setToolTip(new Label(tooltip()));
+		var flowType = flowType();
+		if (flowType != null) {
+			setIcon(Images.get(flowType));
+		}
 	}
 
-	private String getToolTipText() {
-		String text = getPrefix() + ": " + node.getName() + "\n";
-		if (node.exchange.flow == null)
-			return text;
-		if (node.exchange.flow.category != null) {
+	private String tooltip() {
+		var exchange = node.exchange;
+		if (exchange == null || exchange.flow == null)
+			return "";
+		var type = flowType();
+		var prefix = type == null
+				? "?"
+				: Labels.of(type);
+		if (exchange.isAvoided) {
+			prefix += " - avoided";
+		}
+		var text = prefix + ": " + node.getName() + "\n";
+		if (exchange.flow.category != null) {
 			text += M.Category + ": " + Labels.getShortCategory(
-					Descriptor.of(node.exchange.flow)) + "\n";
+					Descriptor.of(exchange.flow)) + "\n";
 		}
 		text += M.Amount + ": "
-				+ Numbers.format(node.exchange.amount)
-				+ " " + Labels.name(node.exchange.unit);
+				+ Numbers.format(exchange.amount)
+				+ " " + Labels.name(exchange.unit);
 		return text;
-	}
-
-	private String getPrefix() {
-		FlowType type = node.exchange.flow != null
-				? node.exchange.flow.flowType
-				: null;
-		if (type == null)
-			return "?";
-		if (node.exchange.isAvoided) {
-			return type == FlowType.PRODUCT_FLOW
-					? M.AvoidedProductFlow
-					: M.AvoidedWasteFlow;
-		}
-		return Labels.of(type);
 	}
 
 	void setHighlighted(boolean value) {
@@ -67,16 +68,25 @@ class ExchangeFigure extends Label {
 			setForegroundColor(TEXT_HIGHLIGHTED_COLOR);
 		} else {
 			setBackgroundColor(BACKGROUND_COLOR);
-			setForegroundColor(node.exchange.isAvoided ? BACKGROUND_COLOR : TEXT_COLOR);
+			setForegroundColor(node.exchange.isAvoided
+					? BACKGROUND_COLOR
+					: TEXT_COLOR);
 		}
 	}
 
+	private FlowType flowType() {
+		return node.exchange != null && node.exchange.flow != null
+				? node.exchange.flow.flowType
+				: null;
+	}
+
 	@Override
-	protected void paintFigure(Graphics graphics) {
+	protected void paintFigure(Graphics g) {
 		if (node.parent().isMinimized() && !Animation.isRunning())
 			return;
+
 		if (node.isDummy() || !node.exchange.isAvoided) {
-			super.paintFigure(graphics);
+			super.paintFigure(g);
 			return;
 		}
 		int x = getLocation().x;
@@ -86,10 +96,10 @@ class ExchangeFigure extends Label {
 		Image iconLeft = Icon.EXCHANGE_BG_LEFT.get();
 		Image iconMiddle = Icon.EXCHANGE_BG_MIDDLE.get();
 		Image iconRight = Icon.EXCHANGE_BG_RIGHT.get();
-		graphics.drawImage(iconLeft, new Point(x, y + 2));
+		g.drawImage(iconLeft, new Point(x, y + 2));
 		for (int i = margin; i < width - margin; i++)
-			graphics.drawImage(iconMiddle, new Point(x + i, y + 2));
-		graphics.drawImage(iconRight, new Point(x + width - margin, y + 2));
-		super.paintFigure(graphics);
+			g.drawImage(iconMiddle, new Point(x + i, y + 2));
+		g.drawImage(iconRight, new Point(x + width - margin, y + 2));
+		super.paintFigure(g);
 	}
 }
