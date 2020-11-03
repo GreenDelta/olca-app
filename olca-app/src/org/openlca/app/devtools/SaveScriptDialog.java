44f73d071a998e41d38ee19fd3d2f4b5e8c56445
msrocka <michael.srocka@gmail.com>
2020-11-03 15:18:30 +0100

    handle different cases when storing scripts

--- olca-app/src/org/openlca/app/devtools/SaveScriptDialog.java
+++ olca-app/src/org/openlca/app/devtools/SaveScriptDialog.java
@@ -2,23 +2,27 @@ package org.openlca.app.devtools;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
-import com.ibm.icu.text.CharsetDetector;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.forms.FormDialog;
 import org.eclipse.ui.forms.IManagedForm;
 import org.openlca.app.db.Database;
+import org.openlca.app.navigation.Navigator;
+import org.openlca.app.rcp.Workspace;
 import org.openlca.app.util.Controls;
 import org.openlca.app.util.MsgBox;
 import org.openlca.app.util.UI;
 
+import com.ibm.icu.text.CharsetDetector;
+
 public class SaveScriptDialog extends FormDialog {
 
+	private final String script;
 	private String name;
-	private String script;
 	private boolean asGlobal = true;
 
 	public static void forImportOf(File file) {
@@ -43,18 +47,22 @@ public class SaveScriptDialog extends FormDialog {
 	}
 
 	public static void forScriptOf(String name, String script) {
-
+		var dialog = new SaveScriptDialog(name, script);
+		dialog.open();
 	}
 
-
-	private SaveScriptDialog() {
+	private SaveScriptDialog(String name, String script) {
 		super(UI.shell());
+		this.name = name == null
+				? "script.py"
+				: name;
+		this.script = script;
 	}
 
 	@Override
 	protected void configureShell(Shell shell) {
 		super.configureShell(shell);
-		shell.setText("Import script");
+		shell.setText("Save script");
 	}
 
 	@Override
@@ -66,32 +74,82 @@ public class SaveScriptDialog extends FormDialog {
 	protected void createFormContent(IManagedForm mform) {
 		var tk = mform.getToolkit();
 		var body = UI.formBody(mform.getForm(), tk);
-		UI.gridLayout(body, 1);
-		tk.createLabel(body, "Import file `"
-				+ file.getName() + "` as script?");
+		UI.gridLayout(body, 2);
+		var text = UI.formText(body, tk, "File name:");
+		text.setText(name);
+		text.addModifyListener(e -> name = text.getText());
 
 		// add a `save as local` script option when
 		// a database is open
 		var db = Database.get();
-		if (Database.get() != null) {
-			var global = tk.createButton(
-					body, "As global script", SWT.RADIO);
-			global.setSelection(true);
-			Controls.onSelect(
-					global, e -> asGlobal = global.getSelection());
-			var local = tk.createButton(
-					body, "As script in database " + db.getName(),
-					SWT.RADIO);
-			local.setSelection(false);
-			Controls.onSelect(
-					local, e -> this.asGlobal = !local.getSelection());
-		}
+		if (db == null)
+			return;
+
+		UI.filler(body, tk);
+		var global = tk.createButton(
+				body, "As global script", SWT.RADIO);
+		global.setSelection(true);
+		Controls.onSelect(
+				global, e -> asGlobal = global.getSelection());
+
+		UI.filler(body, tk);
+		var local = tk.createButton(
+				body, "As script in database " + db.getName(),
+				SWT.RADIO);
+		local.setSelection(false);
+		Controls.onSelect(
+				local, e -> this.asGlobal = !local.getSelection());
 	}
 
 	@Override
 	protected void okPressed() {
+		if (name.isEmpty()) {
+			MsgBox.error("Empty name",
+					"An empty name is not allowed");
+			return;
+		}
 
+		var file = file();
+		if (file.exists()) {
+			MsgBox.error("Script already exists",
+					"The script " + name + " already exists");
+			return;
+		}
 
-		super.okPressed();
+		try {
+			Files.writeString(file.toPath(),
+					script, StandardCharsets.UTF_8);
+			Navigator.refresh();
+			super.okPressed();
+		} catch (Exception e) {
+			MsgBox.error("Failed to save script "
+					+ name + ": " + e.getMessage());
+		}
+	}
+
+	private File file() {
+		var db = Database.get();
+		if (asGlobal || db == null) {
+			var dir = new File(Workspace.getDir(), "scripts");
+			if (!dir.exists() && !dir.mkdirs())
+				throw new RuntimeException(
+						"Could not create `scripts` folder: " + dir);
+			return new File(dir, name);
+		}
+
+		// jump to the global folder when no database
+		// local folder can be created
+		var dbDir = db.getFileStorageLocation();
+		if (dbDir == null) {
+			asGlobal = true;
+			return file();
+		}
+		var dir = new File(dbDir, "scripts");
+		if (!dir.exists() && !dir.mkdirs()) {
+			asGlobal = true;
+			return file();
+		}
+
+		return new File(dir, name);
 	}
 }
--- olca-app/src/org/openlca/app/tools/FileImport.java
+++ olca-app/src/org/openlca/app/tools/FileImport.java
@@ -9,6 +9,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.forms.FormDialog;
 import org.eclipse.ui.forms.IManagedForm;
 import org.openlca.app.db.Database;
+import org.openlca.app.devtools.SaveScriptDialog;
 import org.openlca.app.util.Controls;
 import org.openlca.app.util.MsgBox;
 import org.openlca.app.util.UI;
@@ -30,67 +31,10 @@ public class FileImport {
 		var name = file.getName().toLowerCase();
 		if (name.endsWith(".py")
 				|| name.endsWith(".sql")) {
-			new ScriptDialog(file).open();
+			SaveScriptDialog.forImportOf(file);
 			return;
 		}
 
 		var format = Format.detect(file).orElse(null);
 	}
-
-	private static class ScriptDialog extends FormDialog {
-
-		private final File file;
-		private boolean asGlobal = true;
-
-		ScriptDialog(File file) {
-			super(UI.shell());
-			this.file = file;
-		}
-
-		@Override
-		protected void configureShell(Shell shell) {
-			super.configureShell(shell);
-			shell.setText("Import script");
-		}
-
-		@Override
-		protected Point getInitialSize() {
-			return new Point(600, 350);
-		}
-
-		@Override
-		protected void createFormContent(IManagedForm mform) {
-			var tk = mform.getToolkit();
-			var body = UI.formBody(mform.getForm(), tk);
-			UI.gridLayout(body, 1);
-			tk.createLabel(body, "Import file `"
-					+ file.getName() + "` as script?") ;
-
-			// add a `save as local` script option when
-			// a database is open
-			var db = Database.get();
-			if (Database.get() != null) {
-				var global = tk.createButton(
-						body, "As global script", SWT.RADIO);
-				global.setSelection(true);
-				Controls.onSelect(
-						global, e -> asGlobal = global.getSelection());
-				var local = tk.createButton(
-						body, "As script in database " + db.getName(),
-						SWT.RADIO);
-				local.setSelection(false);
-				Controls.onSelect(
-						local, e -> this.asGlobal = !local.getSelection());
-			}
-		}
-
-		@Override
-		protected void okPressed() {
-
-
-			super.okPressed();
-		}
-	}
-
-
 }
