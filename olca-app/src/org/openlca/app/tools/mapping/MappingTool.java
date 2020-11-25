7a5d639629fd3d1d4b1c8b775ced51a36b244e65
msrocka <michael.srocka@gmail.com>
2020-11-25 15:06:26 +0100

    #121 save-as function for mappings

--- olca-app/src/org/openlca/app/tools/mapping/MappingTool.java
+++ olca-app/src/org/openlca/app/tools/mapping/MappingTool.java
@@ -3,7 +3,11 @@ package org.openlca.app.tools.mapping;
 import java.io.File;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
@@ -11,15 +15,20 @@ import org.eclipse.ui.forms.editor.FormPage;
 import org.openlca.app.components.FileChooser;
 import org.openlca.app.db.AppCache;
 import org.openlca.app.db.Cache;
+import org.openlca.app.db.Database;
 import org.openlca.app.editors.Editors;
 import org.openlca.app.editors.SimpleEditorInput;
 import org.openlca.app.editors.SimpleFormEditor;
+import org.openlca.app.navigation.Navigator;
 import org.openlca.app.tools.mapping.model.IProvider;
 import org.openlca.app.tools.mapping.model.ProviderType;
 import org.openlca.app.util.ErrorReporter;
 import org.openlca.app.util.MsgBox;
+import org.openlca.app.util.UI;
+import org.openlca.core.database.MappingFileDao;
 import org.openlca.core.model.MappingFile;
 import org.openlca.io.maps.FlowMap;
+import org.openlca.util.Strings;
 
 public class MappingTool extends SimpleFormEditor {
 
@@ -29,6 +38,7 @@ public class MappingTool extends SimpleFormEditor {
 	AtomicBoolean checked = new AtomicBoolean(true);
 
 	private MappingPage page;
+	private boolean dirty;
 
 	/**
 	 * If the mapping is stored in the database, this field contains
@@ -111,6 +121,97 @@ public class MappingTool extends SimpleFormEditor {
 			checked.set(mapping.entries.isEmpty());
 		} catch (Exception e) {
 			ErrorReporter.on("Failed to initialize mapping tool", e);
+		}
+	}
+
+	protected void setDirty() {
+		// the dirty state can only change if this is
+		// a mapping from the database
+		if (mappingFile == null)
+			return;
+		dirty = true;
+		editorDirtyStateChanged();
+	}
+
+	@Override
+	public boolean isDirty() {
+		return dirty;
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		if (mappingFile == null)
+			return;
+		var db = Database.get();
+		if (db == null)
+			return;
+		try {
+			mapping.updateContentOf(mappingFile);
+			new MappingFileDao(db).update(mappingFile);
+			dirty = false;
+			editorDirtyStateChanged();
+		} catch (Exception e) {
+			ErrorReporter.on("Failed to save mapping file "
+					+ mappingFile.name, e);
+		}
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return true;
+	}
+
+	@Override
+	public void doSaveAs() {
+		var db = Database.get();
+		if (db == null)
+			return;
+
+		// guess a new mapping name
+		var dao = new MappingFileDao(db);
+		var existing = dao.getNames()
+				.stream()
+				.map(String::toLowerCase)
+				.collect(Collectors.toSet());
+		String proposedName;
+		var i = existing.size();
+		do {
+			i++;
+			proposedName = "flow_mapping_" + i + ".csv";
+		} while (existing.contains(proposedName));
+
+		// open a friendly dialog
+		var dialog = new InputDialog(
+				UI.shell(),
+				"Save mapping in database",
+				"Please provide a unique name for the new mapping file",
+				proposedName,
+				name -> {
+					if (Strings.nullOrEmpty(name))
+						return "The name cannot be empty";
+					if (existing.contains(name.toLowerCase().trim()))
+						return "A flow mapping with this name already exists";
+					return null;
+				});
+		if (dialog.open() != Window.OK)
+			return;
+
+		// store it under this name
+		try {
+			var name = dialog.getValue().trim();
+			var newMapping = new MappingFile();
+			newMapping.name = name;
+			mapping.updateContentOf(newMapping);
+			dao.insert(newMapping);
+			Navigator.refresh();
+
+			// set it as the mapping file of this editor
+			// if applicable
+			if (mappingFile == null) {
+				mappingFile = newMapping;
+			}
+		} catch (Exception e) {
+			ErrorReporter.on("Failed to save mapping file in database", e);
 		}
 	}
 
