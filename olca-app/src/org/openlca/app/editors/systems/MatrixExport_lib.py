06c9bca0be5531ff0a12a1e825e7312a8399fa12
msrocka <michael.srocka@gmail.com>
2021-02-20 12:12:36 +0100

    matrix export: LCIA example

--- olca-app/src/org/openlca/app/editors/systems/MatrixExport_lib.py
+++ olca-app/src/org/openlca/app/editors/systems/MatrixExport_lib.py
@@ -6,9 +6,11 @@ License 2.0 (MPL 2.0; see https://github.com/GreenDelta/olca-app).
 from __future__ import annotations
 
 import csv
+import os
 from typing import Iterator, List, Tuple
 
 import numpy
+import numpy.linalg
 import scipy.sparse
 
 
@@ -110,6 +112,7 @@ class ImpactEntry:
         e.impact_id = row[0]
         e.impact_name = row[1]
         e.impact_unit = row[2]
+        return e
 
     @staticmethod
     def index_of(file_path: str) -> List[ImpactEntry]:
@@ -133,3 +136,58 @@ def _csv_rows_of(f: str) -> Iterator[Tuple[int, List[str]]]:
         for row in reader:
             i += 1
             yield (i, row)
+
+
+class ExportFolder:
+
+    def __init__(self, folder: str):
+        self.folder = folder
+
+    def tech_index(self) -> List[TechEntry]:
+        path = os.path.join(self.folder, 'index_A.csv')
+        if not os.path.exists(path):
+            return []
+        return TechEntry.index_of(path)
+
+    def flow_index(self) -> List[FlowEntry]:
+        path = os.path.join(self.folder, 'index_B.csv')
+        if not os.path.exists(path):
+            return []
+        return FlowEntry.index_of(path)
+
+    def impact_index(self) -> List[ImpactEntry]:
+        path = os.path.join(self.folder, 'index_C.csv')
+        if not os.path.exists(path):
+            return []
+        return ImpactEntry.index_of(path)
+
+    def load(self, name: str):
+        path = os.path.join(self.folder, name)
+        if os.path.exists(path):
+            return matrix_of(path)
+        p = path + '.npy'
+        if os.path.exists(p):
+            return matrix_of(p)
+        p = path + '.npz'
+        if os.path.exists(p):
+            return matrix_of(p)
+        return None
+
+
+class Matrix:
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    f = 'f'
+
+
+def solve(A, f):
+    """Note that we currently convert sparse matrices to a dense format."""
+    M = A
+    if scipy.sparse.issparse(A):
+        M = A.todense()
+    return numpy.linalg.solve(M, f)
+
+
+def invert(A):
+    pass
--- olca-app/src/org/openlca/app/editors/systems/MatrixExport_main.py
+++ olca-app/src/org/openlca/app/editors/systems/MatrixExport_main.py
@@ -1,15 +1,37 @@
+"""
+This script gives some examples for using the openLCA matrix export from Python.
+You need to have NumPy and SciPy installed, e.g. via pip
+
+  pip install -U numpy scipy
+
+Note that all caclulations in these examples are currently done with dense
+matrices. Sparse matrices are converted to a dense format in these calculations.
+If you want to use faster calculations for sparce matrices checkout the direct
+and iterative solvers from the SciPy package:
+
+https://docs.scipy.org/doc/scipy/reference/sparse.linalg.html
+"""
+
 from lib import *
 
-import scipy.sparse.linalg as linalg
+def lcia_example():
+    """Calculates and prints the LCIA result."""
+    folder = ExportFolder('.')
+
+    # load the matrices
+    A = folder.load(Matrix.A)
+    B = folder.load(Matrix.B)
+    C = folder.load(Matrix.C)
+    f = folder.load(Matrix.f)
+
+    # calculate the LCIA result
+    scaling = solve(A, f)
+    g = B @ scaling
+    h = C @ g
+
+    for i in folder.impact_index():
+        print('%s , %.5f , %s' % (i.impact_name, h[i.index], i.impact_unit))
+
 
 if __name__ == '__main__':
-    A = matrix_of('A.npz')
-    f = matrix_of('f.npy')
-    s = linalg.spsolve(A, f, use_umfpack=True)
-    B = matrix_of('B.npz')
-    g = B @ s
-    C = matrix_of('C.npz')
-    h = C @ g
-    impact_index = ImpactEntry.index_of('index_C.csv')
-    for e in impact_index:
-        print('%s\t%.5f\t%s' % (e.impact_name, h[e.index], e.unit))
+    lcia_example()
