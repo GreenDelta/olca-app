58f420ae4bbcb8258e6041abf27836ed6894e5b4
msrocka <michael.srocka@gmail.com>
2021-10-16 10:10:28 +0200

    save the EPD product
    * [x] determine the category path and sync it for the product and result

--- olca-app/src/org/openlca/app/editors/results/openepd/input/ImportDialog.java
+++ olca-app/src/org/openlca/app/editors/results/openepd/input/ImportDialog.java
@@ -1,5 +1,8 @@
 package org.openlca.app.editors.results.openepd.input;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import org.eclipse.swt.graphics.Point;
@@ -15,18 +18,27 @@ import org.openlca.app.editors.results.openepd.model.Ec3CategoryIndex;
 import org.openlca.app.editors.results.openepd.model.Ec3Epd;
 import org.openlca.app.editors.results.openepd.model.Ec3ImpactModel;
 import org.openlca.app.util.Controls;
+import org.openlca.app.util.ErrorReporter;
 import org.openlca.app.util.MsgBox;
 import org.openlca.app.util.UI;
+import org.openlca.core.database.CategoryDao;
 import org.openlca.core.database.IDatabase;
+import org.openlca.core.model.Category;
+import org.openlca.core.model.Flow;
 import org.openlca.core.model.FlowProperty;
+import org.openlca.core.model.ModelType;
 import org.openlca.core.model.ResultFlow;
+import org.openlca.util.Strings;
 
 public class ImportDialog extends FormDialog {
 
 	final IDatabase db;
 	final Ec3Epd epd;
 	final Ec3ImpactModel impactModel;
+	private final Ec3CategoryIndex categories;
 	private final ResultFlow product;
+	private String categoryPath;
+	private final List<ResultSection> sections = new ArrayList<>();
 
 	public static void show(Ec3Epd epd) {
 		show(epd, Ec3CategoryIndex.empty());
@@ -40,14 +52,16 @@ public class ImportDialog extends FormDialog {
 			MsgBox.error(M.NoDatabaseOpened);
 			return -1;
 		}
-		return new ImportDialog(epd, db).open();
+		return new ImportDialog(epd, categories, db).open();
 	}
 
-	private ImportDialog(Ec3Epd epd, IDatabase db) {
+	private ImportDialog(Ec3Epd epd, Ec3CategoryIndex categories, IDatabase db) {
 		super(UI.shell());
 		this.epd = Objects.requireNonNull(epd);
 		this.db = Objects.requireNonNull(db);
 		this.impactModel = Ec3ImpactModel.get();
+		this.categories = Objects.requireNonNull(categories);
+		this.categoryPath = categories.pathOf(epd.category);
 		product = Util.initQuantitativeReference(epd, db);
 	}
 
@@ -67,27 +81,36 @@ public class ImportDialog extends FormDialog {
 		var tk = mForm.getToolkit();
 		var body = UI.formBody(mForm.getForm(), tk);
 		createProductSection(body, tk);
-
 		var resultSections = ResultSection.initAllOf(this);
 		for (var section : resultSections) {
+			sections.add(section);
 			section.render(body, tk);
 		}
-
 	}
 
 	private void createProductSection(Composite body, FormToolkit tk) {
 		var comp = UI.formSection(body, tk, M.Product);
+
+		// name
 		var nameText = UI.formText(comp, tk, M.Name);
 		Controls.set(nameText, product.flow.name, name -> product.flow.name = name);
+
+		// category
+		var categoryText = UI.formText(comp, tk, M.Category);
+		Controls.set(categoryText, categoryPath, path -> categoryPath = path);
+
+		// amount
 		var amountText = UI.formText(comp, tk, M.Amount);
 		Controls.set(amountText, product.amount, amount -> product.amount = amount);
 
+		// quantity
 		var quantityCombo = EntityCombo.of(
 				UI.formCombo(comp, tk, M.Quantity), FlowProperty.class, db)
 			.select(product.flowPropertyFactor != null
 				? product.flowPropertyFactor.flowProperty
 				: null);
 
+		// unit
 		var unitCombo = EntityCombo.of(
 				UI.formCombo(comp, tk, M.Unit), Util.allowedUnitsOf(product))
 			.select(product.unit)
@@ -98,8 +121,55 @@ public class ImportDialog extends FormDialog {
 			unitCombo.update(Util.allowedUnitsOf(product))
 				.select(product.unit);
 		});
-
 	}
 
+	@Override
+	protected void okPressed() {
+		var productFlow = createProduct();
+		if (productFlow == null)
+			return;
+
+		super.okPressed();
+	}
+
+	private Flow createProduct() {
+		var productFlow = product.flow;
+		var error = validateProduct(productFlow);
+		if (error != null) {
+			MsgBox.error("Invalid product", error);
+			return null;
+		}
+		try {
+			productFlow.category = syncCategory(ModelType.FLOW);
+			return db.insert(productFlow);
+		} catch (Exception e) {
+			ErrorReporter.on("failed to save product flow", e);
+			return null;
+		}
+	}
+
+	private Category syncCategory(ModelType type) {
+		if (categoryPath == null)
+			return null;
+		var path = Arrays.stream(categoryPath.split("/"))
+			.map(String::trim)
+			.filter(p -> !p.isEmpty())
+			.toArray(String[]::new);
+		return path.length == 0
+			? null
+			: CategoryDao.sync(db, type, path);
+	}
+
+	private String validateProduct(Flow flow) {
+		if (flow == null)
+			return "No product flow defined";
+		if (Strings.nullOrEmpty(flow.name))
+			return "Product name is empty.";
+		if (flow.getReferenceFactor() == null)
+			return "Product flow has no flow property";
+		if (flow.getReferenceUnit() == null)
+			return "Product flow has no reference unit";
+		return null;
+	}
 
 }
