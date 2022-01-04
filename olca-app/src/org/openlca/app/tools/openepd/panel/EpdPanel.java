8957c1e932033d8a0560fe150a9d95f030483968
msrocka <michael.srocka@gmail.com>
2022-01-04 16:30:11 +0100

    added import and download actions

--- olca-app/src/org/openlca/app/tools/openepd/panel/EpdPanel.java
+++ olca-app/src/org/openlca/app/tools/openepd/panel/EpdPanel.java
@@ -7,20 +7,29 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.openlca.app.App;
+import org.openlca.app.components.FileChooser;
 import org.openlca.app.editors.Editors;
 import org.openlca.app.editors.SimpleEditorInput;
 import org.openlca.app.editors.SimpleFormEditor;
 import org.openlca.app.rcp.images.Icon;
+import org.openlca.app.tools.openepd.input.ImportDialog;
 import org.openlca.app.tools.openepd.model.Api;
 import org.openlca.app.tools.openepd.model.Ec3CategoryIndex;
 import org.openlca.app.tools.openepd.model.Ec3Epd;
+import org.openlca.app.util.Actions;
 import org.openlca.app.util.Controls;
+import org.openlca.app.util.MsgBox;
 import org.openlca.app.util.UI;
+import org.openlca.app.viewers.Viewers;
 import org.openlca.app.viewers.tables.Tables;
+import org.openlca.jsonld.Json;
+
+import com.google.gson.JsonObject;
 
 public class EpdPanel extends SimpleFormEditor {
 
@@ -45,34 +54,30 @@ public class EpdPanel extends SimpleFormEditor {
 		@Override
 		protected void createFormContent(IManagedForm mform) {
 			var form = UI.formHeader(mform, "Building Transparency - openEPD",
-					Icon.EC3_WIZARD.get());
+				Icon.EC3_WIZARD.get());
 			var tk = mform.getToolkit();
 			var body = UI.formBody(form, tk);
 			var loginPanel = LoginPanel.create(body, tk);
 
 			var section = UI.section(body, tk, "Find EPDs");
 			UI.gridData(section, true, true);
+
+			// search panel
 			var comp = UI.sectionClient(section, tk, 1);
 			var searchComp = tk.createComposite(comp);
 			UI.fillHorizontal(searchComp);
 			UI.gridLayout(searchComp, 4);
-
 			var searchText = tk.createText(searchComp, "");
 			UI.fillHorizontal(searchText);
-			var searchButton = tk.createButton(searchComp, "Search", SWT.NONE );
+			var searchButton = tk.createButton(searchComp, "Search", SWT.NONE);
 			searchButton.setImage(Icon.SEARCH.get());
-
 			tk.createLabel(searchComp, "Max. count:");
 			var spinner = new Spinner(searchComp, SWT.BORDER);
 			spinner.setValues(100, 10, 1000, 0, 50, 100);
 			tk.adapt(spinner);
 
 			// descriptor table
-			table = Tables.createViewer(
-				comp, "EPD", "Manufacturer", "Category", "Declared unit");
-			Tables.bindColumnWidths(table, 0.25, 0.25, 0.25, 0.25);
-			UI.gridData(table.getControl(), true, true);
-			table.setLabelProvider(new TableLabel());
+			table = createTable(comp, loginPanel);
 
 			// search handler
 			Runnable onSearch = () -> {
@@ -102,6 +107,70 @@ public class EpdPanel extends SimpleFormEditor {
 			Controls.onReturn(searchText, $ -> onSearch.run());
 
 			form.reflow(true);
+		}
+
+		private TableViewer createTable(Composite comp, LoginPanel loginPanel) {
+			var table = Tables.createViewer(
+				comp, "EPD", "Manufacturer", "Category", "Declared unit");
+			Tables.bindColumnWidths(table, 0.25, 0.25, 0.25, 0.25);
+			UI.gridData(table.getControl(), true, true);
+			table.setLabelProvider(new TableLabel());
+
+			var onImport = Actions.create(
+				"Import EPD results", Icon.IMPORT.descriptor(), () -> {
+					var epd = FullEpd.fetch(table, loginPanel);
+					if (epd.isEmpty())
+						return;
+					ImportDialog.show(epd.get(), categories);
+				});
+
+			var onSaveFile = Actions.create(
+				"Save as file", Icon.FILE.descriptor(), () -> {
+					var epd = FullEpd.fetch(table, loginPanel);
+					if (epd.isEmpty())
+						return;
+					var file = FileChooser.forSavingFile(
+						"Save OpenEPD", epd.descriptor.name + ".json");
+					if (file == null)
+						return;
+					Json.write(epd.json, file);
+				});
+
+			Actions.bind(table, onImport, onSaveFile);
+			return table;
+		}
+
+		private record FullEpd(Ec3Epd descriptor, JsonObject json) {
+
+			Ec3Epd get() {
+				return json != null
+					? Ec3Epd.fromJson(json).orElse(null)
+					: null;
+			}
+
+			static FullEpd empty() {
+				return new FullEpd(null, null);
+			}
+
+			boolean isEmpty() {
+				return descriptor == null || json == null;
+			}
+
+			static FullEpd fetch(TableViewer table, LoginPanel loginPanel) {
+				Ec3Epd epd = Viewers.getFirstSelected(table);
+				if (epd == null)
+					return empty();
+				var client = loginPanel.login().orElse(null);
+				if (client == null)
+					return empty();
+				var json = App.exec(
+					"Download EPD", () -> Api.getRawEpd(client, epd.id));
+				if (json.isEmpty()) {
+					MsgBox.error("Failed to download EPD " + epd.id);
+					return empty();
+				}
+				return new FullEpd(epd, json.get());
+			}
 		}
 
 		private class TableLabel extends BaseLabelProvider
