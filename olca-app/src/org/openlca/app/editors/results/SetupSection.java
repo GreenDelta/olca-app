7d5fb8907d81c0d865abf82f82580aa3e39da1cc
msrocka <michael.srocka@gmail.com>
2022-01-10 17:32:37 +0100

    remove unused components

--- olca-app/src/org/openlca/app/editors/results/SetupSection.java
+++ olca-app/src/org/openlca/app/editors/results/SetupSection.java
@@ -1,41 +1,20 @@
 package org.openlca.app.editors.results;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ImageHyperlink;
-import org.openlca.app.App;
 import org.openlca.app.M;
 import org.openlca.app.components.ModelLink;
 import org.openlca.app.rcp.images.Icon;
-import org.openlca.app.util.Colors;
-import org.openlca.app.util.Controls;
-import org.openlca.app.util.Labels;
 import org.openlca.app.util.Numbers;
 import org.openlca.app.util.UI;
 import org.openlca.app.viewers.combo.AllocationCombo;
 import org.openlca.core.model.AllocationMethod;
 import org.openlca.core.model.CalculationSetup;
-import org.openlca.core.model.Flow;
-import org.openlca.core.model.FlowPropertyFactor;
 import org.openlca.core.model.ImpactMethod;
 import org.openlca.core.model.ProductSystem;
-import org.openlca.core.model.Unit;
-import org.openlca.util.Pair;
-import org.openlca.util.Strings;
 
-class SetupSection {
-
-	private final ResultEditor editor;
-
-	SetupSection(ResultEditor editor) {
-		this.editor = editor;
-	}
+record SetupSection(ResultEditor editor) {
 
 	private CalculationSetup setup() {
 		var result = editor.getModel();
@@ -48,8 +27,6 @@ class SetupSection {
 	void render(Composite parent, FormToolkit tk) {
 		var comp = UI.formSection(parent, tk, "Calculation setup", 2);
 		UI.gridData(comp, false, false);
-		var flowLink = new FlowLink();
-		var unitCombo = new UnitCombo();
 
 		// product system
 		UI.formLabel(comp, tk, M.ProductSystem);
@@ -58,8 +35,6 @@ class SetupSection {
 			.renderOn(comp, tk)
 			.onChange(system -> {
 				setup().withTarget(system);
-				flowLink.update();
-				unitCombo.update();
 				editor.setDirty();
 			});
 
@@ -99,125 +74,5 @@ class SetupSection {
 		var calcBtn = tk.createButton(comp, "Recalculate", SWT.PUSH);
 		calcBtn.setImage(Icon.RUN.get());
 		calcBtn.setEnabled(false);
-	}
-
-	private class FlowLink {
-
-		private ImageHyperlink link;
-
-		private FlowLink renderOn(Composite parent, FormToolkit tk) {
-			var comp = tk.createComposite(parent, SWT.FILL);
-			UI.gridLayout(comp, 1, 0, 0);
-			link = tk.createImageHyperlink(comp, SWT.TOP);
-			link.setForeground(Colors.linkBlue());
-			Controls.onClick(link, $ -> {
-				var flow = setup().flow();
-				if (flow != null) {
-					App.open(flow);
-				}
-			});
-			return this;
-		}
-
-		void update() {
-			if (link == null)
-				return;
-			var flow = setup().flow();
-			var text = flow == null
-				? M.None
-				: Labels.name(flow);
-			link.setText(Strings.cut(text, 120));
-			link.setToolTipText(text);
-			link.getParent().pack();
-			link.setEnabled(flow != null);
-		}
-	}
-
-	private class UnitCombo {
-
-		private final List<Pair<Unit, FlowPropertyFactor>> units = new ArrayList<>();
-		private Combo combo;
-
-		UnitCombo renderOn(Composite comp, FormToolkit tk) {
-			combo = new Combo(comp, SWT.BORDER);
-			tk.adapt(combo);
-			UI.gridData(combo, false, false).widthHint = 80;
-
-			Controls.onSelect(combo, $ -> {
-				var idx = combo.getSelectionIndex();
-				if (idx < 0)
-					return;
-				var pair = units.get(idx);
-				setup().withUnit(pair.first);
-				setup().withFlowPropertyFactor(pair.second);
-				editor.setDirty();
-			});
-			return this;
-		}
-
-		void update() {
-			if (combo == null)
-				return;
-
-			var flow = setup().flow();
-			updateUnits(flow);
-			if (units.isEmpty()) {
-				combo.setItems();
-				return;
-			}
-
-			var multiProp = flow.flowPropertyFactors.size() > 1;
-			var selectedUnit = setup().unit();
-			var selectedFact = setup().flowPropertyFactor();
-			var items = new String[units.size()];
-			var selectedIdx = -1;
-
-			for (int i = 0; i < units.size(); i++) {
-				var pair = units.get(i);
-				var unit = pair.first;
-				var fact = pair.second;
-				var item = Labels.name(unit);
-				if (multiProp) {
-					item += " (" + Labels.name(fact.flowProperty) + ")";
-				}
-				items[i] = item;
-				if (Objects.equals(unit, selectedUnit)
-					&& Objects.equals(fact, selectedFact)) {
-					selectedIdx = i;
-				}
-			}
-
-			combo.setItems(items);
-			if (selectedIdx >= 0) {
-				combo.select(selectedIdx);
-			}
-		}
-
-		private void updateUnits(Flow flow) {
-			units.clear();
-			if (flow == null)
-				return;
-
-			for (var fact : flow.flowPropertyFactors) {
-				var prop = fact.flowProperty;
-				if (prop == null || prop.unitGroup == null)
-					continue;
-				for (var unit : prop.unitGroup.units) {
-					units.add(Pair.of(unit, fact));
-				}
-			}
-
-			units.sort((pair1, pair2) -> {
-				var u1 = Labels.name(pair1.first);
-				var u2 = Labels.name(pair2.first);
-				var c = Strings.compare(u1, u2);
-				if (c != 0) {
-					return c;
-				}
-				var p1 = Labels.name(pair1.second.flowProperty);
-				var p2 = Labels.name(pair2.second.flowProperty);
-				return Strings.compare(p1, p2);
-			});
-		}
 	}
 }
