f4c55d1b3694eb26eb8c2b08a669bd0a39946bb7
msrocka <michael.srocka@gmail.com>
2022-01-21 14:13:42 +0100

    #149 open memory error dialog in UI thread

--- olca-app/src/org/openlca/app/wizards/calculation/MemoryError.java
+++ olca-app/src/org/openlca/app/wizards/calculation/MemoryError.java
@@ -3,13 +3,11 @@ package org.openlca.app.wizards.calculation;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.forms.FormDialog;
 import org.eclipse.ui.forms.IManagedForm;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.Hyperlink;
+import org.openlca.app.App;
 import org.openlca.app.M;
 import org.openlca.app.util.Controls;
 import org.openlca.app.util.UI;
@@ -17,8 +15,9 @@ import org.openlca.app.util.UI;
 class MemoryError {
 
 	public static void show() {
-		Dialog dialog = new Dialog();
-		dialog.open();
+		App.runInUI("Memory error in calculation...", () -> {
+			new Dialog().open();
+		});
 	}
 
 	private static class Dialog extends FormDialog {
@@ -38,20 +37,26 @@ class MemoryError {
 
 		@Override
 		protected Point getInitialSize() {
-			return new Point(400, 280);
+			return new Point(450, 350);
+		}
+
+		@Override
+		protected void configureShell(Shell newShell) {
+			super.configureShell(newShell);
+			newShell.setText(M.OutOfMemory);
 		}
 
 		@Override
 		protected void createFormContent(IManagedForm mform) {
-			String message = M.CouldNotAllocateMemoryError;
-			FormToolkit toolkit = mform.getToolkit();
+			var tk = mform.getToolkit();
+			var comp = UI.formBody(mform.getForm(), tk);
 			mform.getForm().setText(M.OutOfMemory);
-			Composite comp = UI.formBody(mform.getForm(), mform.getToolkit());
 			UI.gridLayout(comp, 1);
-			Label label = toolkit.createLabel(comp, message, SWT.WRAP);
-			UI.gridData(label, true, false);
-			Hyperlink link = UI.formLink(comp, toolkit, "Open preference dialog");
-			Controls.onClick(link, e -> openPreferences());
+			var label = tk.createLabel(
+					comp, M.CouldNotAllocateMemoryError, SWT.WRAP);
+			UI.fillHorizontal(label);
+			var link = UI.formLink(comp, tk, "Open preference dialog");
+			Controls.onClick(link, $ -> openPreferences());
 		}
 
 		private void openPreferences() {
