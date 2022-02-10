5060259806776211642deda492f2ac7cde2ff930
Sebastian Greve <greve@greendelta.com>
2022-02-10 17:36:17 +0100

    Reverting to master branch

--- olca-app/src/org/openlca/app/collaboration/ui/navigation/actions/ConnectAction.java
+++ olca-app/src/org/openlca/app/collaboration/ui/navigation/actions/ConnectAction.java
@@ -7,12 +7,14 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.transport.URIish;
 import org.openlca.app.collaboration.api.RepositoryConfig;
 import org.openlca.app.collaboration.ui.Announcements;
+import org.openlca.app.collaboration.util.Constants;
 import org.openlca.app.db.Database;
 import org.openlca.app.db.Repository;
 import org.openlca.app.navigation.actions.INavigationAction;
 import org.openlca.app.navigation.elements.DatabaseElement;
 import org.openlca.app.navigation.elements.INavigationElement;
 import org.openlca.app.util.Input;
+import org.openlca.util.Strings;
 
 public class ConnectAction extends Action implements INavigationAction {
 
@@ -24,9 +26,12 @@ public class ConnectAction extends Action implements INavigationAction {
 	@Override
 	public void run() {
 		var url = Input.promptString("Connect to git repository", "Please enter the git URL to connect to", "");
+		if (Strings.nullOrEmpty(url))
+			return;
 		try {
-			var git = Git.init().setBare(true).setGitDir(RepositoryConfig.getGirDir(Database.get())).call();
-			git.remoteAdd().setName("origin").setUri(new URIish(url)).call();
+			var git = Git.init().setInitialBranch(Constants.DEFAULT_BRANCH).setBare(true)
+					.setGitDir(RepositoryConfig.getGirDir(Database.get())).call();
+			git.remoteAdd().setName(Constants.DEFAULT_REMOTE).setUri(new URIish(url)).call();
 			Repository.connect(Database.get());
 			Announcements.check();
 		} catch (Exception e) {
--- olca-app/src/org/openlca/app/collaboration/util/Constants.java
+++ olca-app/src/org/openlca/app/collaboration/util/Constants.java
@@ -3,7 +3,7 @@ package org.openlca.app.collaboration.util;
 public interface Constants {
 
 	public static String DEFAULT_REMOTE = "origin";
-	public static String DEFAULT_BRANCH = "main";
+	public static String DEFAULT_BRANCH = "master";
 	public static String LOCAL_BRANCH = "HEAD";
 	public static String LOCAL_REF = "refs/heads/" + DEFAULT_BRANCH;
 	public static String REMOTE_BRANCH = DEFAULT_REMOTE + "/" + DEFAULT_BRANCH;
