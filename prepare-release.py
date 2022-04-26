095b35a8d6a0d05563cf4d4b45442a0c75a92147
msrocka <michael.srocka@gmail.com>
2022-04-26 10:07:28 +0200

    let the release script run on Linux

--- prepare-release.py
+++ prepare-release.py
@@ -5,15 +5,23 @@
 # * the HTML pages
 # * the current modules interface for the Jython interpreter
 
-from os import name
+import os
 from subprocess import call
 
+
 def main():
-    call('update_modules.bat')
-    call('mvn.cmd package', cwd='./olca-refdata')
-    call('npm.cmd install', cwd='./olca-app-html')
-    call('npm.cmd run build', cwd='./olca-app-html')
-    call('node gen-jython-bindings.js')
+    if os.name == 'posix':
+        call('./update_modules.sh')
+        call(['mvn', 'package'], cwd='./olca-refdata')
+        call(['npm', 'install'], cwd='./olca-app-html')
+        call(['npm', 'run', 'build'], cwd='./olca-app-html')
+        call(['node', 'gen-jython-bindings.js'])
+    else:
+        call('update_modules.bat')
+        call('mvn.cmd package', cwd='./olca-refdata')
+        call('npm.cmd install', cwd='./olca-app-html')
+        call('npm.cmd run build', cwd='./olca-app-html')
+        call('node gen-jython-bindings.js')
 
 
 if __name__ == '__main__':
