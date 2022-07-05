66642896dd9351db3a679d59b35081a7976fc8d3
François Le Rall <lerall.francois@gmail.com>
2022-07-05 11:59:26 +0200

    Add a PageChangedListener to focus on the reference  when opening the _Model graph_ tab.  - Create and add a PageChangedListener that is remove when the event is fired for the first time.

--- olca-app/src/org/openlca/app/editors/systems/ProductSystemEditor.java
+++ olca-app/src/org/openlca/app/editors/systems/ProductSystemEditor.java
@@ -1,5 +1,8 @@
 package org.openlca.app.editors.systems;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eclipse.jface.dialogs.IPageChangedListener;
 import org.openlca.app.M;
 import org.openlca.app.editors.ModelEditor;
 import org.openlca.app.editors.graphical.GraphicalEditorInput;
@@ -21,10 +24,28 @@ public class ProductSystemEditor extends ModelEditor<ProductSystem> {
 			addPage(new ProductSystemInfoPage(this));
 			// addPage(new ProductSystemParameterPage(this));
 			addPage(new ParameterPage2(this));
+
 			var descriptor = getEditorInput().getDescriptor();
 			GraphicalEditorInput gInput = new GraphicalEditorInput(descriptor);
-			int gIdx = addPage(new GraphEditor(this), gInput);
+			var graphEditor = new GraphEditor(this);
+			int gIdx = addPage(graphEditor, gInput);
 			setPageText(gIdx, M.ModelGraph);
+			// Add a page listener to focus the graph on the reference node when it is
+			// activated the first time.
+			var graphInit = new AtomicReference<IPageChangedListener>();
+			IPageChangedListener fn = e -> {
+				if (e.getSelectedPage() != graphEditor)
+					return;
+				var listener = graphInit.get();
+				if (listener == null)
+					return;
+				graphEditor.focusOnReferenceNode();
+				removePageChangedListener(listener);
+				graphInit.set(null);
+			};
+			graphInit.set(fn);
+			addPageChangedListener(fn);
+
 			addPage(new StatisticsPage(this));
 			addCommentPage();
 		} catch (Exception e) {
