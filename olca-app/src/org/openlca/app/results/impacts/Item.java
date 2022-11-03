9ca714e0dc84465908f5ff056e1d9e01796c4925
msrocka <michael.srocka@gmail.com>
2022-11-03 12:05:14 +0100

    impact analysis: immutable tree items with factory methods
    that can be used as `BiFunction` instances

--- olca-app/src/org/openlca/app/results/impacts/Item.java
+++ olca-app/src/org/openlca/app/results/impacts/Item.java
@@ -1,192 +1,143 @@
 package org.openlca.app.results.impacts;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 import org.openlca.core.matrix.index.EnviFlow;
 import org.openlca.core.matrix.index.TechFlow;
 import org.openlca.core.model.descriptors.ImpactDescriptor;
 import org.openlca.core.results.LcaResult;
 
-class Item {
+/**
+ * Describes an item in the impact tree. The tree has 3 levels: roots, inner
+ * nodes, and leafs. A root contains the total impact result. The inner nodes
+ * and leafs contain the contributions of technosphere flows and intervention
+ * flows to that impact result.
+ */
+record Item(
+		LcaResult result,
+		ImpactDescriptor impact,
+		Item parent,
+		TechFlow techFlow,
+		EnviFlow enviFlow,
+		double impactResult,
+		double inventoryResult) {
 
-	private final LcaResult result;
-	private final ImpactDescriptor impact;
-	private final Item parent;
-
-	private final TechFlow techFlow;
-	private final EnviFlow enviFlow;
-
-	private double impactResult;
-	private double inventoryResult;
-
-	private Item(LcaResult result, ImpactDescriptor impact) {
-		this.result = result;
-		this.impact = impact;
-		this.parent = null;
-		this.techFlow = null;
-		this.enviFlow = null;
+	private static Item rootOf(LcaResult result, ImpactDescriptor impact) {
+		double value = result.getTotalImpactValueOf(impact);
+		return new Item(result, impact, null, null, null, value, 0);
 	}
 
-	private Item(Item parent, TechFlow techFlow) {
-		this.parent = parent;
-		this.result = parent.result;
-		this.techFlow = techFlow;
-		this.impact = null;
-		this.enviFlow = null;
+	private static Item techNodeOf(Item root, TechFlow techFlow) {
+		var result = root.result;
+		double value = result.getDirectImpactOf(root.impact, techFlow);
+		return new Item(result, root.impact, root, techFlow, null, value, 0);
 	}
 
-	private Item(Item parent, EnviFlow enviFlow) {
-		this.parent = parent;
-		this.result = parent.result;
-		this.impact = null;
-		this.techFlow = null;
-		this.enviFlow = enviFlow;
+	private static Item enviNodeOf(Item parent, EnviFlow enviFlow) {
+		var result = parent.result;
+		var impact = parent.impact;
+		double impactVal = result.getFlowImpactOf(impact, enviFlow);
+		double inventoryVal = result.getTotalFlowValueOf(enviFlow);
+		return new Item(
+				result, impact, parent, null, enviFlow, impactVal, inventoryVal);
+	}
+
+	private static Item techLeafOf(Item parent, TechFlow techFlow) {
+		var result = parent.result;
+		var enviFlow = parent.enviFlow;
+		double inventoryVal = result.getDirectFlowOf(enviFlow, techFlow);
+		double impactVal = parent.impactFactor() * inventoryVal;
+		return new Item(
+				result, parent.impact, parent, techFlow, null, impactVal, inventoryVal);
+	}
+
+	private static Item enviLeafOf(Item parent, EnviFlow enviFlow) {
+		var result = parent.result;
+		var impact = parent.impact;
+		var impactFactor = result.getImpactFactorOf(impact, enviFlow);
+		var inventoryVal = result.getDirectFlowOf(enviFlow, parent.techFlow);
+		double impactVal = impactFactor * inventoryVal;
+		return new Item(
+				result, parent.impact, parent, null, enviFlow, impactVal, inventoryVal);
 	}
 
 	static List<Item> rootsOf(LcaResult result, List<ImpactDescriptor> impacts) {
-		var items = new ArrayList<Item>(impacts.size());
-		for (var impact : impacts) {
-			var item = new Item(result, impact);
-			item.impactResult = result.getTotalImpactValueOf(impact);
-			items.add(item);
-		}
-		return items;
+		return impacts.stream()
+				.map(impact -> rootOf(result, impact))
+				.toList();
 	}
+
+	List<Item> techNodesOf(List<TechFlow> techFlows, double cutoff) {
+		return isRoot()
+				? collect(techFlows, cutoff, Item::techNodeOf)
+				: Collections.emptyList();
+	}
+
+	List<Item> techLeafsOf(List<TechFlow> techFlows, double cutoff) {
+		return isEnviItem()
+				? collect(techFlows, cutoff, Item::techLeafOf)
+				: Collections.emptyList();
+	}
+
+	List<Item> enviNodesOf(List<EnviFlow> enviFlows, double cutoff) {
+		return isRoot()
+				? collect(enviFlows, cutoff, Item::enviNodeOf)
+				: Collections.emptyList();
+	}
+
+	List<Item> enviLeafsOf(List<EnviFlow> enviFlows, double cutoff) {
+		return isTechItem()
+				? collect(enviFlows, cutoff, Item::enviLeafOf)
+				: Collections.emptyList();
+	}
+
+	private <E> List<Item> collect(
+			List<E> elements, double cutoff, BiFunction<Item, E, Item> fn) {
+		var cutoffFilter = cutoffFilterOf(cutoff);
+		return elements.stream()
+				.map(e -> fn.apply(this, e))
+				.filter(cutoffFilter)
+				.sorted((i1, i2) -> Double.compare(i2.impactResult, i1.impactResult))
+				.toList();
+	}
+
+	private Predicate<Item> cutoffFilterOf(double cutoff) {
+		if (cutoff == 0)
+			return item -> true;
+		double absMax = Math.abs(result.getTotalImpactValueOf(impact) * cutoff);
+		return item -> Math.abs(item.impactResult) > absMax;
+	}
+
 
 	boolean isRoot() {
-		return impact != null;
+		return parent == null;
 	}
 
-	ImpactDescriptor impact() {
-		return parent != null
-				? parent.impact()
-				: impact;
+	boolean isInnerNode() {
+		return parent != null && parent().isRoot();
 	}
 
-	private LcaResult result() {
-		return parent != null
-				? parent.result
-				: result;
+	boolean isLeaf() {
+		return parent != null && parent.isInnerNode();
 	}
 
 	boolean isEnviItem() {
 		return enviFlow != null;
 	}
 
-	EnviFlow enviFlow() {
-		return enviFlow != null
-				? enviFlow
-				: parent != null ? parent.enviFlow : null;
-	}
-
 	boolean isTechItem() {
 		return techFlow != null;
 	}
 
-	TechFlow techFlow() {
-		return techFlow != null
-				? techFlow
-				: parent != null ? parent.techFlow : null;
-	}
-
-	boolean isLeaf() {
-		return techFlow != null && enviFlow != null;
-	}
-
 	double impactFactor() {
-		var enviFlow = enviFlow();
-		return enviFlow != null
-				? result.getImpactFactorOf(impact(), enviFlow)
-				: 0;
+		if (isRoot())
+			return 0;
+		return isEnviItem()
+				? result.getImpactFactorOf(impact, enviFlow)
+				: parent.impactFactor();
 	}
 
-	List<Item> techNodesOf(List<TechFlow> techFlows, double cutoff) {
-		var collector = new Collector(this, cutoff);
-		for (var techFlow : techFlows) {
-			double value = result.getDirectImpactOf(impact(), techFlow);
-			collector.next(value, () -> new Item(this, techFlow));
-		}
-		return collector.get();
-	}
-
-	List<Item> enviNodesOf(List<EnviFlow> enviFlows, double cutoff) {
-		var collector = new Collector(this, cutoff);
-		for (var enviFlow : enviFlows) {
-			double value = result.getFlowImpactOf(impact(), enviFlow);
-			collector.next(value, () -> {
-				var item = new Item(this, enviFlow);
-				item.inventoryResult = result.getTotalFlowValueOf(enviFlow);
-				return item;
-			});
-		}
-		return collector.get();
-	}
-
-	List<Item> enviLeafsOf(List<EnviFlow> enviFlows, double cutoff) {
-		if (!isTechItem())
-			return Collections.emptyList();
-		var collector = new Collector(this, cutoff);
-		for (var enviFlow : enviFlows) {
-			double factor = result.getImpactFactorOf(impact(), enviFlow);
-			double inventoryResult = result.getDirectFlowOf(enviFlow, this.techFlow);
-			double value = factor * inventoryResult;
-			collector.next(value, () -> {
-				var item = new Item(this, enviFlow);
-				item.inventoryResult = inventoryResult;
-				return item;
-			});
-		}
-		return collector.get();
-	}
-
-	List<Item> techLeafsOf(List<TechFlow> techFlows, double cutoff) {
-		if (!isEnviItem())
-			return Collections.emptyList();
-		var collector = new Collector(this, cutoff);
-		var factor = impactFactor();
-		for (var techFlow : techFlows) {
-			var inventoryResult = result.getDirectFlowOf(this.enviFlow, techFlow);
-			double value = factor * inventoryResult;
-			collector.next(value, () -> {
-				var item = new Item(this, techFlow);
-				item.inventoryResult = inventoryResult;
-				return item;
-			});
-		}
-		return collector.get();
-	}
-
-	private static class Collector {
-
-		private final boolean addAll;
-		private final double absMax;
-		private final List<Item> items = new ArrayList<>();
-
-		Collector(Item parent, double cutoff) {
-			if (cutoff == 0) {
-				addAll = true;
-				absMax = 0;
-			} else {
-				double total = parent.result.getTotalImpactValueOf(parent.impact());
-				addAll = false;
-				absMax = Math.abs(cutoff * total);
-			}
-		}
-
-		void next(double value, Supplier<Item> fn) {
-			if (!addAll && Math.abs(value) < absMax)
-				return;
-			var item = fn.get();
-			item.impactResult = value;
-			items.add(item);
-		}
-
-		List<Item> get() {
-			items.sort((i1, i2) -> Double.compare(i2.impactResult, i1.impactResult));
-			return items;
-		}
-	}
 }
