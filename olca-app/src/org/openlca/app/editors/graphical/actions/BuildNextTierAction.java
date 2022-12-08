71e103c9b023905f479f861f4eb5732a5198afd2
François Le Rall <lerall.francois@gmail.com>
2022-12-08 14:46:19 +0100

    Fix Build actions (BuildNextTier and BuildSupplyChain):  - make the action selection sensible (ExchangeItem and Node),  - display the Nodes once the supply chain is built.

--- olca-app/src/org/openlca/app/editors/graphical/actions/BuildNextTierAction.java
+++ olca-app/src/org/openlca/app/editors/graphical/actions/BuildNextTierAction.java
@@ -1,106 +1,51 @@
 package org.openlca.app.editors.graphical.actions;
 
-import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CommandStack;
-import org.eclipse.gef.ui.actions.WorkbenchPartAction;
 import org.openlca.app.M;
-import org.openlca.app.db.Database;
 import org.openlca.app.editors.graphical.GraphEditor;
-import org.openlca.app.editors.graphical.edit.NodeEditPart;
-import org.openlca.app.editors.graphical.model.ExchangeItem;
-import org.openlca.app.editors.graphical.model.Node;
-import org.openlca.app.editors.graphical.model.commands.ExpandCommand;
 import org.openlca.app.editors.graphical.model.commands.MassCreationCommand;
-import org.openlca.core.database.FlowDao;
-import org.openlca.core.database.ProcessDao;
-import org.openlca.core.matrix.linking.ProviderLinking;
-import org.openlca.core.model.Exchange;
 import org.openlca.core.model.ProcessLink;
-import org.openlca.core.model.ProcessType;
-import org.openlca.core.model.descriptors.ProcessDescriptor;
 import org.openlca.core.model.descriptors.RootDescriptor;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 
-import static org.openlca.app.tools.graphics.model.Side.INPUT;
 
-public class BuildNextTierAction extends WorkbenchPartAction
-		implements IBuildAction {
+public class BuildNextTierAction extends BuildAction {
 
-	private final FlowDao flowDao;
-	private final ProcessDao processDao;
 	private final GraphEditor editor;
-	private List<NodeEditPart> nodeParts;
-	private ProcessType preferredType = ProcessType.UNIT_PROCESS;
-	private ProviderLinking providers = ProviderLinking.ONLY_DEFAULTS;
+	private ArrayList<RootDescriptor> newProviders;
+	private ArrayList<ProcessLink> newConnections;
 
 	public BuildNextTierAction(GraphEditor part) {
 		super(part);
 		editor = part;
 		setId(GraphActionIds.BUILD_NEXT_TIER);
 		setText(M.BuildNextTier);
-		flowDao = new FlowDao(Database.get());
-		processDao = new ProcessDao(Database.get());
-	}
-
-	@Override
-	public void setNodeParts(List<NodeEditPart> parts) {
-		this.nodeParts = parts;
-	}
-
-	@Override
-	public void setPreferredType(ProcessType preferredType) {
-		this.preferredType = preferredType;
-	}
-
-	@Override
-	public void setProviderMethod(ProviderLinking providers) {
-		this.providers = providers;
 	}
 
 	@Override
 	public void run() {
-		if (nodeParts == null || nodeParts.isEmpty())
-			return;
 		var graph = editor.getModel();
-		List<RootDescriptor> providers = new ArrayList<>();
-		List<ProcessLink> newConnections = new ArrayList<>();
-		for (var part : nodeParts)
-			collectFor(part.getModel(), providers, newConnections);
-		Command command = MassCreationCommand.nextTier(providers, newConnections, graph);
-		for (var part : nodeParts)
-			command = command.chain(new ExpandCommand(part.getModel(), INPUT));
-		var stack = (CommandStack) editor.getAdapter(CommandStack.class);
-		stack.execute(command);
-		editor.setDirty();
+
+		newProviders = new ArrayList<>();
+		newConnections = new ArrayList<>();
+		collect();
+
+		var command = MassCreationCommand.nextTier(newProviders, newConnections, graph);
+
+		if (command.canExecute())
+			execute(command);
 	}
 
-	private void collectFor(Node node,
-		List<RootDescriptor> providers, List<ProcessLink> newConnections) {
-		for (var eNode : getLinkCandidates(node)) {
-			var provider = findProvider(eNode.exchange);
+	private void collect() {
+		for (var exchange : mapExchangesToProcess.keySet()) {
+			var process = mapExchangesToProcess.get(exchange);
+			var provider = findProvider(exchange);
 			if (provider == null)
 				continue;
-			if (!providers.contains(provider)) {
-				providers.add(provider);
+			if (!newProviders.contains(provider)) {
+				newProviders.add(provider);
 			}
-			var link = new ProcessLink();
-			link.flowId = eNode.exchange.flow.id;
-			link.exchangeId = eNode.exchange.id;
-			link.processId = node.descriptor.id;
-			link.providerId = provider.id;
-			if (provider.type == null) {
-				link.providerType = ProcessLink.ProviderType.PROCESS;
-			} else {
-				link.providerType = switch (provider.type) {
-					case PRODUCT_SYSTEM -> ProcessLink.ProviderType.SUB_SYSTEM;
-					case RESULT -> ProcessLink.ProviderType.RESULT;
-					default -> ProcessLink.ProviderType.PROCESS;
-				};
-			}
+			var link = getLink(exchange, process, provider);
 
 			if (!newConnections.contains(link)) {
 				newConnections.add(link);
@@ -108,56 +53,9 @@ public class BuildNextTierAction extends WorkbenchPartAction
 		}
 	}
 
-	private List<ExchangeItem> getLinkCandidates(Node node) {
-		List<ExchangeItem> nodes = new ArrayList<>();
-		var ioPanes = editor.getGraphFactory().createIOPanes(node.descriptor);
-		for (var pane : ioPanes.values()) {
-			for (var exchangeItem : pane.getExchangeItems()) {
-				if (exchangeItem.exchange == null || exchangeItem.isConnected())
-					continue;
-				if (exchangeItem.isWaste() && !exchangeItem.exchange.isInput)
-					nodes.add(exchangeItem);
-				else if (!exchangeItem.isWaste() && exchangeItem.exchange.isInput)
-					nodes.add(exchangeItem);
-			}
-		}
-		return nodes;
-	}
-
-	private RootDescriptor findProvider(Exchange e) {
-		if (e.flow == null)
-			return null;
-		if (providers == ProviderLinking.ONLY_DEFAULTS) {
-			if (e.defaultProviderId == 0L)
-				return null;
-			return processDao.getDescriptor(e.defaultProviderId);
-		}
-		if (providers == ProviderLinking.PREFER_DEFAULTS
-			&& e.defaultProviderId != 0L)
-			return processDao.getDescriptor(e.defaultProviderId);
-
-		ProcessDescriptor bestMatch = null;
-		for (var d : getProviders(e)) {
-			if (d.processType == preferredType)
-				return d;
-			if (bestMatch != null)
-				continue;
-			bestMatch = d;
-		}
-		return bestMatch;
-	}
-
-	private List<ProcessDescriptor> getProviders(Exchange e) {
-		if (e == null || e.flow == null)
-			return Collections.emptyList();
-		Set<Long> providerIds = e.isInput
-			? flowDao.getWhereOutput(e.flow.id)
-			: flowDao.getWhereInput(e.flow.id);
-		return processDao.getDescriptors(providerIds);
-	}
-
 	@Override
 	protected boolean calculateEnabled() {
-		return false;
+		return true;
 	}
+
 }
