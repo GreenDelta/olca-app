953e812cc0527b174208b50a2a7bcb79a40da761
François Le Rall <lerall.francois@gmail.com>
2022-12-15 18:00:08 +0100

    Add question to the RemoveNodeCommand.

--- olca-app/src/org/openlca/app/editors/graphical/model/commands/DeleteNodeCommand.java
+++ olca-app/src/org/openlca/app/editors/graphical/model/commands/DeleteNodeCommand.java
@@ -3,9 +3,15 @@ package org.openlca.app.editors.graphical.model.commands;
 import java.util.List;
 
 import org.eclipse.gef.commands.Command;
+import org.openlca.app.db.Database;
 import org.openlca.app.editors.graphical.model.Graph;
 import org.openlca.app.editors.graphical.model.GraphLink;
 import org.openlca.app.editors.graphical.model.Node;
+import org.openlca.app.util.Labels;
+import org.openlca.app.util.MsgBox;
+import org.openlca.app.util.Question;
+import org.openlca.core.database.usage.ExchangeUseSearch;
+import org.openlca.core.database.usage.ProcessUseSearch;
 
 public class DeleteNodeCommand extends Command {
 	/** Node to remove. */
@@ -52,6 +58,13 @@ public class DeleteNodeCommand extends Command {
 
 	@Override
 	public void execute() {
+		var b = Question.ask("Remove process",
+				"Remove " + Labels.name(child.descriptor)
+						+ " from product system " + Labels.name(parent.getProductSystem())
+						+ "?");
+		if (!b)
+			return;
+
 		// store a copy of incoming & outgoing links before proceeding
 		links = child.getAllLinks().stream().map(GraphLink.class::cast).toList();
 		redo();
