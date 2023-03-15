5479c6490261bca64ce9a54b48ba90c18847cef3
François Le Rall <lerall.francois@gmail.com>
2023-03-15 11:35:20 +0100

    Remove unused import.

--- olca-app/src/org/openlca/app/wizards/io/ExcelImportWizard.java
+++ olca-app/src/org/openlca/app/wizards/io/ExcelImportWizard.java
@@ -13,7 +13,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.forms.events.HyperlinkAdapter;
