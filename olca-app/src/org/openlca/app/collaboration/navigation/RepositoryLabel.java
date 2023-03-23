112ce485753cc50d3534e4786d606497017bc39c
Sebastian Greve <greve@greendelta.com>
2023-03-23 14:38:41 +0100

    Looking for deleted elements when identifying repository changes

--- olca-app/src/org/openlca/app/collaboration/navigation/RepositoryLabel.java
+++ olca-app/src/org/openlca/app/collaboration/navigation/RepositoryLabel.java
@@ -1,16 +1,21 @@
 package org.openlca.app.collaboration.navigation;
 
+import java.util.stream.Collectors;
+
 import org.eclipse.swt.graphics.Image;
 import org.openlca.app.db.Cache;
 import org.openlca.app.db.Database;
 import org.openlca.app.db.Repository;
+import org.openlca.app.navigation.elements.CategoryElement;
 import org.openlca.app.navigation.elements.DatabaseElement;
 import org.openlca.app.navigation.elements.INavigationElement;
 import org.openlca.app.navigation.elements.ModelElement;
+import org.openlca.app.navigation.elements.ModelTypeElement;
 import org.openlca.app.navigation.elements.NavigationRoot;
 import org.openlca.app.rcp.images.Images;
 import org.openlca.app.rcp.images.Overlay;
 import org.openlca.core.database.config.DatabaseConfig;
+import org.openlca.git.GitIndex;
 import org.openlca.git.util.Constants;
 import org.openlca.util.Strings;
 
@@ -71,19 +76,51 @@ public class RepositoryLabel {
 		for (var child : elem.getChildren())
 			if (hasChanged(child) || ((child instanceof ModelElement e) && isNew(e)))
 				return true;
-		return false;
+		return containsDeleted(elem);
 	}
 
 	private static boolean isNew(ModelElement e) {
-		return !Repository.get().gitIndex.has(Cache.getPathCache(), e.getContent());
+		return !index().has(Cache.getPathCache(), e.getContent());
 	}
 
 	private static boolean hasChanged(ModelElement e) {
 		if (isNew(e))
 			return false;
-		var entry = Repository.get().gitIndex.get(Cache.getPathCache(), e.getContent());
+		var entry = index().get(Cache.getPathCache(), e.getContent());
 		return e.getContent().lastChange != entry.lastChange()
 				|| e.getContent().version != entry.version();
+	}
+
+	private static boolean containsDeleted(INavigationElement<?> elem) {
+		if (elem instanceof ModelElement)
+			return false;
+		for (var child : elem.getChildren())
+			if (containsDeleted(child))
+				return true;
+		if (!(elem instanceof ModelTypeElement) && !(elem instanceof CategoryElement))
+			return false;
+		var fromIndex = index().getSubPaths(getPath(elem));
+		var fromNavigation = elem.getChildren()
+				.stream().map(e -> getPath(e))
+				.collect(Collectors.toSet());
+		for (var entry : fromIndex)
+			if (!fromNavigation.contains(entry))
+				return true;
+		return false;
+	}
+
+	private static String getPath(INavigationElement<?> elem) {
+		if (elem instanceof ModelTypeElement e)
+			return index().getPath(e.getContent());
+		if (elem instanceof CategoryElement e)
+			return index().getPath(e.getContent());
+		if (elem instanceof ModelElement e)
+			return index().getPath(Cache.getPathCache(), e.getContent());
+		return null;
+	}
+
+	private static GitIndex index() {
+		return Repository.get().gitIndex;
 	}
 
 }
