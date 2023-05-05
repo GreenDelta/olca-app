1bde09771d56e5cfd901833ee6f9f3511ee108d0
msrocka <michael.srocka@gmail.com>
2023-05-05 13:14:49 +0200

    more credits; remove the python script

--- olca-app-build/credits/credits-gen.js
+++ olca-app-build/credits/credits-gen.js
@@ -1,23 +1,59 @@
+// This script builds the `about.html` page that contains the license
+// information and third party credits. When building the `about.html` page the
+// respective third party licenses are fetched from the respective websites and
+// cached in the `credits/cache` folder. This cache folder and the about page
+// are not put under version control.
+
+// in orger to collect our Maven dependencies, you can run
+// mvn project-info-reports:dependencies
+
 const fs = require('fs/promises');
 const path = require('path');
 const http = require('http');
 const https = require('https');
-const creditsJsonPath = path.join(__dirname, 'credits.json');
-const cacheDirPath = path.join(__dirname, 'cache');
-const aboutTemplatePath = path.join(__dirname, 'about_template.html');
-const aboutHtmlPath = path.join(__dirname, 'about.html');
 
-/** Used to generate a normalised name for license cache file */
-function cacheFilename(projectName) {
-  return `${projectName.split(' ').join('_')}_license.txt`;
+async function main() {
+  console.log("build credits file: about.html");
+
+  // make sure that the `cache` folder exists
+  const cacheDir = path.join(__dirname, 'cache');
+  await fs.mkdir(cacheDir).catch(() => {});
+
+  // generate the credit blocks
+  const creditsPath = path.join(__dirname, 'credits.json');
+  const { credits } = JSON.parse(await fs.readFile(creditsPath, 'utf8'));
+  let creditBlocks = '';
+  for (const credit of credits) {
+    const licenseText = await getLicenseText(credit);
+    creditBlocks += creditBlockOf(credit, licenseText);
+  }
+
+  // generate the file
+  const templatePath = path.join(__dirname, 'about_template.html');
+  const template = await fs.readFile(templatePath, 'utf8');
+  const aboutText = template.replace('#{credits}#', creditBlocks);
+  const aboutPath = path.join(__dirname, 'about.html');
+  await fs.writeFile(aboutPath, aboutText);
 }
 
-/** Fetches a license from url and returns the text */
-function fetchLicense(url) {
+async function getLicenseText(credit) {
+  const { project, licenseUrl } = credit;
+  const cachedFile = path.join(__dirname, 'cache', `${project}_license.txt`);
+  try {
+    return await fs.readFile(cachedFile);
+  } catch {
+    const licenseText = await fetchLicenseFrom(licenseUrl);
+    await fs.writeFile(cachedFile, licenseText);
+    return licenseText;
+  }
+}
+
+function fetchLicenseFrom(url) {
+  console.log(`  fetch license: ${url}`);
   function makeRequest(url, resolve, reject) {
-    const requestClient = url.startsWith('http://') ? http : https;
-    requestClient.get(url, (resp) => {
-      if(resp.statusCode === 301 || resp.statusCode === 302) {
+    const client = url.startsWith('http://') ? http : https;
+    client.get(url, (resp) => {
+      if (resp.statusCode === 301 || resp.statusCode === 302) {
         return makeRequest(resp.headers.location, resolve, reject)
       } else {
         let data = '';
@@ -26,32 +62,10 @@ function fetchLicense(url) {
       }
     }).on("error", (err) => reject(err));
   }
-
   return new Promise((resolve, reject) => makeRequest(url, resolve, reject));
 }
 
-/** Generates a file under cache directory containing the license text */
-async function cacheLicense(licenseFilename, licenseText) {
-  const licenseCachePath = path.join(cacheDirPath, licenseFilename);
-  return await fs.writeFile(licenseCachePath, licenseText);
-}
-
-/** Fetches license from cache if available otherwise retrieves it from internet and caches it */
-async function getLicenseText(credit) {
-  const { project, licenseUrl } = credit;
-  const cacheFile = cacheFilename(project);
-  const licenseCachePath = path.join(cacheDirPath, cacheFile);
-  try {
-    return await fs.readFile(licenseCachePath);
-  } catch(error) {
-    const licenseTxt = await fetchLicense(licenseUrl);
-    await cacheLicense(cacheFile, licenseTxt);
-    return licenseTxt;
-  }
-}
-
-/** Generates html block that also contains the license text */
-function generateHtmlForCredit(credit, licenseTxt) {
+function creditBlockOf(credit, licenseText) {
   return `
     <div style='width: 100%; margin-bottom: 10px;'>
       <div class='block'>
@@ -74,28 +88,11 @@ function generateHtmlForCredit(credit, licenseTxt) {
       </div>
       <div data-description='licence-content' data-id='${credit.project}' class='licence-details hide'>
         <div>
-          <pre style='word-wrap: break-word; white-space: pre-wrap;'>${licenseTxt}</pre>
+          <pre style='word-wrap: break-word; white-space: pre-wrap;'>${licenseText}</pre>
         </div>
       </div>
     </div>
   `
 }
 
-/** Injects the html code generated from `generateHtmlForCredit` in template file and writes it */
-async function generateAboutHtmlWithLicenseText(licenseHtml) {
-  const templateHtml = await fs.readFile(aboutTemplatePath, 'utf8');
-  const aboutHtml = templateHtml.replace('#{credits}#', licenseHtml);
-  await fs.writeFile(aboutHtmlPath, aboutHtml);
-}
-
-// Main function that reads credits from json and generates an html file for it
-(async () => {
-  const { credits } = JSON.parse(await fs.readFile(creditsJsonPath, 'utf8'));
-  await fs.mkdir(cacheDirPath).catch(() => { /* Ignore */ })
-  let licenseHtml = '';
-  for(const credit of credits) {
-    const licenseTxt = await getLicenseText(credit);
-    licenseHtml += generateHtmlForCredit(credit, licenseTxt);
-  }
-  await generateAboutHtmlWithLicenseText(licenseHtml);
-})();
+main();
