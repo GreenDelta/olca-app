1ae97ee9cd6665e7189ade900066d19f28d0d552
Sebastian Greve <greve@greendelta.com>
2023-08-23 15:07:31 +0200

    Storing ssl certificates in truststore to enable Jgit to use the added certificates

--- olca-app/src/org/openlca/app/rcp/SslCertificates.java
+++ olca-app/src/org/openlca/app/rcp/SslCertificates.java
@@ -15,17 +15,18 @@ class SslCertificates {
 
 	static void load() {
 		log.debug("Loading external ssl certificates");
-		File certificates = new File(Workspace.root(), "ssl-certificates");
+		var certificates = new File(Workspace.root(), "ssl-certificates");
 		if (!certificates.exists() || !certificates.isDirectory()) {
 			log.debug("No certificates found in workspace");
 			return;
 		}
-		for (File file : certificates.listFiles()) {
+		var added = false;
+		for (var file : certificates.listFiles()) {
 			if (file.isDirectory()) {
 				log.debug("Skipping directory: " + file.getName());
 				continue;
 			}
-			String name = file.getName();
+			var name = file.getName();
 			if (!name.endsWith(".cer") && !name.endsWith(".crt") && !name.endsWith(".pem")) {
 				log.debug("Skipping file: " + file.getName() + " (filename must end with .cer, .crt or .pem)");
 				continue;
@@ -33,11 +34,18 @@ class SslCertificates {
 			name = name.substring(0, file.getName().lastIndexOf("."));
 			log.debug("Loading certificate " + name);
 			try (InputStream stream = new FileInputStream(file)) {
-				Ssl.addCertificate(name, stream);
+				added = added || Ssl.addCertificate(name, stream);
 				log.debug("Sucessfully added certificate " + name);
 			} catch (IOException e) {
 				log.error("Error loading certificate " + name, e);
 			}
+		}
+		if (!added)
+			return;
+		try {
+			Ssl.saveKeyStore();
+		} catch (Exception e) {
+			log.error("Error saving keystore", e);
 		}
 	}
 
