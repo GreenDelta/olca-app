521be33a29cfa0c2f4bfc7c7035ab1e6ec5d7ce8
François Le Rall <lerall.francois@gmail.com>
2023-08-25 08:43:22 +0200

    Add PIP package licenses in the about.html.

--- olca-app-build/credits/credits-gen.js
+++ olca-app-build/credits/credits-gen.js
@@ -11,19 +11,20 @@ const fs = require('fs/promises');
 const path = require('path');
 const http = require('http');
 const https = require('https');
+const AdmZip = require("adm-zip");
 
 async function main() {
   console.log("build credits file: about.html");
 
   // make sure that the `cache` folder exists
   const cacheDir = path.join(__dirname, 'cache');
-  await fs.mkdir(cacheDir).catch(() => {});
+  await fs.mkdir(cacheDir).catch(() => { });
 
   // generate the credit blocks
   const creditsPath = path.join(__dirname, 'credits.json');
-  const { credits } = JSON.parse(await fs.readFile(creditsPath, 'utf8'));
+  const { credits, pipCredits } = JSON.parse(await fs.readFile(creditsPath, 'utf8'));
   let creditBlocks = '';
-  for (const credit of credits) {
+  for (const credit of [...credits, ...pipCredits]) {
     const licenseText = await getLicenseText(credit);
     creditBlocks += creditBlockOf(credit, licenseText);
   }
@@ -37,12 +38,14 @@ async function main() {
 }
 
 async function getLicenseText(credit) {
-  const { project, licenseUrl } = credit;
+  const { project, licenseUrl, pipUrl } = credit;
   const cachedFile = path.join(__dirname, 'cache', `${project}_license.txt`);
   try {
     return await fs.readFile(cachedFile);
   } catch {
-    const licenseText = await fetchLicenseFrom(licenseUrl);
+    const licenseText = licenseUrl != null
+      ? await fetchLicenseFrom(licenseUrl)
+      : await fetchPipLicenseFrom(pipUrl);
     await fs.writeFile(cachedFile, licenseText);
     return licenseText;
   }
@@ -63,6 +66,35 @@ function fetchLicenseFrom(url) {
     }).on("error", (err) => reject(err));
   }
   return new Promise((resolve, reject) => makeRequest(url, resolve, reject));
+}
+
+function fetchPipLicenseFrom(url) {
+  console.log(`  fetch pip package: ${url}`);
+
+  function makeRequest(url, resolve, reject) {
+    const client = url.startsWith('http://') ? http : https;
+    client.get(url, (resp) => {
+      if (resp.statusCode === 301 || resp.statusCode === 302) {
+        return makeRequest(resp.headers.location, resolve, reject)
+      } else {
+        let data = []
+        resp.on('data', (chunk) => data.push(chunk))
+        resp.on('end', () => resolve(getLicenseFromPackage(data)))
+      }
+    }).on("error", (err) => reject(err));
+  }
+
+  return new Promise((resolve, reject) => makeRequest(url, resolve, reject));
+}
+
+async function getLicenseFromPackage(data) {
+  const buf = Buffer.concat(data);
+  const zip = new AdmZip(buf);
+  let license = "";
+  for (entry of zip.getEntries())
+    if (entry.name.endsWith("LICENSE.txt")) {
+      return entry.getData().toString("utf8");
+  }
 }
 
 function creditBlockOf(credit, licenseText) {
