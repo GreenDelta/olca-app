e321450551a29f99e89189c4ae1b41deb830da62
msrocka <michael.srocka@gmail.com>
2024-05-21 15:04:25 +0200

    soda - add basic paging

--- olca-app/src/org/openlca/app/tools/soda/SearchBar.java
+++ olca-app/src/org/openlca/app/tools/soda/SearchBar.java
@@ -6,14 +6,17 @@ import java.util.function.Consumer;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.openlca.app.App;
+import org.openlca.app.rcp.images.Icon;
 import org.openlca.app.util.Controls;
 import org.openlca.app.util.MsgBox;
 import org.openlca.app.util.UI;
 import org.openlca.ilcd.descriptors.Descriptor;
 import org.openlca.ilcd.io.SodaClient;
+import org.openlca.ilcd.io.SodaQuery;
 import org.openlca.util.Strings;
 
 class SearchBar {
@@ -21,6 +24,8 @@ class SearchBar {
 	private final SodaClient client;
 	private final TypeCombo typeCombo;
 	private final Text searchText;
+	private final Spinner pageSpin;
+	private final Spinner sizeSpin;
 	private Consumer<List<Descriptor<?>>> consumer;
 
 	private SearchBar(Connection con, Composite parent, FormToolkit tk) {
@@ -28,7 +33,7 @@ class SearchBar {
 
 		var comp = tk.createComposite(parent);
 		UI.fillHorizontal(comp);
-		var grid = UI.gridLayout(comp, 4);
+		var grid = UI.gridLayout(comp, 8);
 		grid.marginWidth = 0;
 		grid.marginHeight = 0;
 
@@ -37,9 +42,16 @@ class SearchBar {
 		UI.fillHorizontal(searchText);
 		searchText.setMessage("Search dataset ...");
 
+		UI.label(comp, tk, "Page: ");
+		pageSpin = new Spinner(comp, SWT.BORDER);
+		pageSpin.setValues(1, 1, 100, 0, 1, 10);
+		UI.label(comp, tk, "Page size: ");
+		sizeSpin = new Spinner(comp, SWT.BORDER);
+		sizeSpin.setValues(50, 50, 500, 0, 50, 50);
+
 		var button = tk.createButton(comp, "Search", SWT.NONE);
-		Controls.onSelect(button,
-				e -> runSearch());
+		button.setImage(Icon.SEARCH.get());
+		Controls.onSelect(button,	e -> runSearch());
 		searchText.addTraverseListener(e -> {
 			if (e.detail == SWT.TRAVERSE_RETURN) {
 				runSearch();
@@ -61,12 +73,21 @@ class SearchBar {
 		if (clazz == null)
 			return;
 		var name = Strings.nullIfEmpty(searchText.getText());
+		var page = pageSpin.getSelection() - 1;
+		var size = sizeSpin.getSelection();
+		var q = new SodaQuery()
+				.withStartIndex( page * size)
+				.withPageSize(size);
+		if (name != null) {
+			q.withSearch(true)
+					.withName(name);
+		}
 
 		var err = new String[1];
 		var result = new ArrayList<Descriptor<?>>();
 		App.runWithProgress("Search datasets ...", () -> {
 			try {
-				var list = client.search(clazz, name);
+				var list = client.query(clazz, q);
 				result.addAll(list.getDescriptors());
 			} catch (Exception e) {
 				err[0] = e.getMessage();
