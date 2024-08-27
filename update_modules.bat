88ac403b5d96c28ce3e3d7c408f1ef8c4094fbc5
Sebastian Greve <greve@greendelta.com>
2024-08-27 12:59:25 +0200

    Removed cs client from update_modules script

--- update_modules.bat
+++ update_modules.bat
@@ -3,14 +3,9 @@
 set current_path=%cd%
 set app_path=%current_path%\olca-app
 set modules_path=%cd%\..\olca-modules
-set cs_client_path=%current_path%\..\cs-client
 
 echo "install olca-modules from %modules_path%" 
 cd %modules_path%
-call mvn install -DskipTests=true
-
-echo "install cs-client from %cs_client_path%"
-cd %cs_client_path%
 call mvn install -DskipTests=true
 
 echo "update packages in %app_path%/libs"
