9727dbb94c68a662501df1b288302ff5101dc6f8
msrocka <michael.srocka@gmail.com>
2024-10-10 16:19:03 +0200

    prefs: do not show lib-download button
    when the MKL is loaded

--- olca-app/src/org/openlca/app/preferences/ConfigPage.java
+++ olca-app/src/org/openlca/app/preferences/ConfigPage.java
@@ -14,10 +14,11 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.openlca.app.M;
-import org.openlca.app.rcp.WindowLayout;
 import org.openlca.app.components.graphics.themes.Themes;
+import org.openlca.app.rcp.WindowLayout;
 import org.openlca.app.util.Controls;
 import org.openlca.app.util.UI;
+import org.openlca.core.matrix.solvers.mkl.MKL;
 import org.openlca.nativelib.Module;
 import org.openlca.nativelib.NativeLib;
 import org.openlca.util.OS;
@@ -31,7 +32,7 @@ public class ConfigPage extends PreferencePage implements
 
 	private boolean isDirty = false;
 	private Combo languageCombo;
-	private Combo graphicsThemeCombo;
+	private Combo graphThemeCombo;
 	private Combo themeCombo;
 	private Text memoryText;
 	private ConfigIniFile iniFile;
@@ -67,13 +68,13 @@ public class ConfigPage extends PreferencePage implements
 		}
 		createMemoryText(comp);
 		createShowHidePage(comp);
-		createGraphicsTheme(comp);
+		createGraphThemeCombo(comp);
 
 		UI.filler(comp);
 		var bComp = UI.composite(comp);
 		UI.gridLayout(bComp, 1, 5, 0);
 		createResetWindow(bComp);
-		if (!NativeLib.isLoaded(Module.UMFPACK)) {
+		if (!MKL.isLoaded() && !NativeLib.isLoaded(Module.UMFPACK)) {
 			createNativeLib(bComp);
 		}
 
@@ -120,21 +121,21 @@ public class ConfigPage extends PreferencePage implements
 				"hide.welcome.page", hideStart.getSelection()));
 	}
 
-	private void createGraphicsTheme(Composite composite) {
+	private void createGraphThemeCombo(Composite composite) {
 		var label = new Label(composite, SWT.NONE);
 		var gd = UI.gridData(label, false, false);
 		gd.verticalAlignment = SWT.TOP;
 		gd.verticalIndent = 2;
 		label.setText(M.GraphicalEditorTheme);
 
-		graphicsThemeCombo = new Combo(composite, SWT.READ_ONLY);
-		UI.gridData(graphicsThemeCombo, true, false);
+		graphThemeCombo = new Combo(composite, SWT.READ_ONLY);
+		UI.gridData(graphThemeCombo, true, false);
 		var themes = Themes.getValidThemeNames();
-		graphicsThemeCombo.setItems(themes.toArray(new String[0]));
+		graphThemeCombo.setItems(themes.toArray(new String[0]));
 		var theme = Preferences.get(Preferences.GRAPHICAL_EDITOR_THEME);
-		selectGraphicsTheme(theme);
-		Controls.onSelect(graphicsThemeCombo, (e) -> {
-			int idx = graphicsThemeCombo.getSelectionIndex();
+		selectGraphTheme(theme);
+		Controls.onSelect(graphThemeCombo, (e) -> {
+			int idx = graphThemeCombo.getSelectionIndex();
 			if (idx < 0)
 				return;
 			var selected = themes.get(idx);
@@ -259,11 +260,11 @@ public class ConfigPage extends PreferencePage implements
 		memoryText.setText(Integer.toString(maxMem));
 		iniFile.language(defaultLang);
 		iniFile.maxMemory(maxMem);
-		var graphicsTheme = Preferences.getStore().getDefaultString(
+		var graphTheme = Preferences.getStore().getDefaultString(
 				Preferences.GRAPHICAL_EDITOR_THEME);
 		Preferences.getStore().setValue(
-				Preferences.GRAPHICAL_EDITOR_THEME, graphicsTheme);
-		selectGraphicsTheme(graphicsTheme);
+				Preferences.GRAPHICAL_EDITOR_THEME, graphTheme);
+		selectGraphTheme(graphTheme);
 		super.performDefaults();
 		performApply();
 	}
@@ -300,10 +301,10 @@ public class ConfigPage extends PreferencePage implements
 	}
 
 
-	private void selectGraphicsTheme(String theme) {
+	private void selectGraphTheme(String theme) {
 		if (theme == null)
 			return;
-		String[] items = graphicsThemeCombo.getItems();
+		String[] items = graphThemeCombo.getItems();
 		int item = -1;
 		for (int i = 0; i < items.length; i++) {
 			if (Objects.equals(theme, items[i])) {
@@ -312,7 +313,7 @@ public class ConfigPage extends PreferencePage implements
 			}
 		}
 		if (item != -1) {
-			graphicsThemeCombo.select(item);
+			graphThemeCombo.select(item);
 		}
 	}
 
