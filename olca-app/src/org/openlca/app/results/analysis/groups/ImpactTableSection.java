e33d4b8d1ae300db3a91aa7e5e9dbf99662b5755
msrocka <michael.srocka@gmail.com>
2024-11-15 13:07:12 +0100

    agroups: move impact table to own section

---
+++ olca-app/src/org/openlca/app/results/analysis/groups/ImpactTableSection.java
@@ -0,0 +1,106 @@
+package org.openlca.app.results.analysis.groups;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.openlca.app.M;
+import org.openlca.app.components.ContributionImage;
+import org.openlca.app.rcp.images.Images;
+import org.openlca.app.util.Labels;
+import org.openlca.app.util.Numbers;
+import org.openlca.app.util.UI;
+import org.openlca.app.viewers.tables.Tables;
+import org.openlca.core.model.AnalysisGroup;
+
+class ImpactTableSection {
+
+	private final List<AnalysisGroup> groups;
+	private TableViewer table;
+
+	ImpactTableSection(List<AnalysisGroup> groups) {
+		this.groups = groups;
+	}
+
+	void render(Composite body, FormToolkit tk) {
+		var section = UI.section(body, tk, M.ImpactAssessmentResults);
+		UI.gridData(section, true, true);
+		var comp = UI.sectionClient(section, tk, 1);
+
+		var headers = new String[2 + groups.size()];
+		var widths = new double[2 + groups.size()];
+		headers[0] = M.ImpactCategory;
+		headers[1] = M.Unit;
+		widths[0] = 0.4;
+		widths[1] = 0.1;
+		for (int i = 0; i < groups.size(); i++) {
+			headers[2 + i] = groups.get(i).name;
+			widths[2 + i] = 0.5 / groups.size();
+		}
+
+		table = Tables.createViewer(comp, headers);
+		Tables.bindColumnWidths(table, widths);
+		table.setLabelProvider(new LabelProvider(groups));
+	}
+
+	void setInput(List<ImpactGroupResult> input) {
+		table.setInput(input);
+	}
+
+	private static final class LabelProvider extends ColumnLabelProvider
+			implements ITableLabelProvider {
+
+		private final List<AnalysisGroup> groups;
+		private final ContributionImage bar;
+
+		private LabelProvider(List<AnalysisGroup> groups) {
+			this.groups = groups;
+			this.bar = new ContributionImage();
+		}
+
+		@Override
+		public void dispose() {
+			bar.dispose();
+			super.dispose();
+		}
+
+		@Override
+		public Image getColumnImage(Object obj, int col) {
+			if (!(obj instanceof ImpactGroupResult r))
+				return null;
+			if (col == 0)
+				return Images.get(r.impact());
+			if (col == 1)
+				return null;
+			double v = getValue(col, r);
+			double max = r.max();
+			double share = max != 0 ? v / max : 0;
+			return bar.get(share);
+		}
+
+		@Override
+		public String getColumnText(Object obj, int col) {
+			if (!(obj instanceof ImpactGroupResult r))
+				return null;
+			return switch (col) {
+				case 0 -> Labels.name(r.impact());
+				case 1 -> r.impact().referenceUnit;
+				default -> Numbers.format(getValue(col, r));
+			};
+		}
+
+		private double getValue(int col, ImpactGroupResult r) {
+			int i = col - 2;
+			if (i < 0 || i >= groups.size())
+				return 0;
+			var group = groups.get(i);
+			var val = r.values().get(group.name);
+			return val != null ? val : 0;
+		}
+	}
+
+}
