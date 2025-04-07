312c441edb043c17fd37c1e525b72b7b2d35012f
msrocka <michael.srocka@gmail.com>
2025-04-07 17:26:08 +0200

    select results as default providers in exchange table

--- olca-app/src/org/openlca/app/editors/processes/exchanges/ProviderCombo.java
+++ olca-app/src/org/openlca/app/editors/processes/exchanges/ProviderCombo.java
@@ -1,27 +1,18 @@
 package org.openlca.app.editors.processes.exchanges;
 
-import java.util.Collections;
-import java.util.Set;
-
 import org.openlca.app.db.Cache;
-import org.openlca.app.db.Database;
 import org.openlca.app.editors.processes.ProcessEditor;
 import org.openlca.app.util.Labels;
 import org.openlca.app.viewers.tables.modify.ComboBoxCellModifier;
-import org.openlca.core.database.EntityCache;
-import org.openlca.core.database.FlowDao;
-import org.openlca.core.database.IDatabase;
+import org.openlca.core.matrix.index.TechFlow;
 import org.openlca.core.model.Exchange;
 import org.openlca.core.model.FlowType;
 import org.openlca.core.model.ProviderType;
-import org.openlca.core.model.descriptors.ProcessDescriptor;
 import org.openlca.core.model.descriptors.RootDescriptor;
 import org.openlca.util.Strings;
 
 class ProviderCombo extends ComboBoxCellModifier<Exchange, RootDescriptor> {
 
-	private final IDatabase db = Database.get();
-	private final EntityCache cache = Cache.getEntityCache();
 	private final ProcessEditor editor;
 
 	ProviderCombo(ProcessEditor editor) {
@@ -34,38 +25,37 @@ class ProviderCombo extends ComboBoxCellModifier<Exchange, RootDescriptor> {
 			return false;
 		var type = e.flow.flowType;
 		return e.isInput
-			? type == FlowType.PRODUCT_FLOW
-			: type == FlowType.WASTE_FLOW;
+				? type == FlowType.PRODUCT_FLOW
+				: type == FlowType.WASTE_FLOW;
 	}
 
 	@Override
 	protected RootDescriptor[] getItems(Exchange e) {
-		var providerIds = getProviderIds(e);
-		if (providerIds.isEmpty())
-			return new ProcessDescriptor[0];
-		var providers = cache.getAll(ProcessDescriptor.class, providerIds)
-			.values().stream()
-			.sorted((p1, p2) -> Strings.compare(Labels.name(p1), Labels.name(p2)))
-			.toList();
-		var array = new ProcessDescriptor[providers.size() + 1];
-		for (int i = 0; i < providers.size(); i++) {
-			array[i + 1] = providers.get(i);
-		}
-		return array;
+		var map = Cache.getMatrixCache().getProviderMap();
+		if (map == null || e == null || e.flow == null)
+			return new RootDescriptor[0];
+		return map.getProvidersOf(e.flow.id)
+				.stream()
+				.map(TechFlow::provider)
+				.sorted((p1, p2) -> Strings.compare(Labels.name(p1), Labels.name(p2)))
+				.toArray(RootDescriptor[]::new);
 	}
 
 	@Override
-	protected ProcessDescriptor getItem(Exchange e) {
-		return e.defaultProviderId != 0
-			? cache.get(ProcessDescriptor.class, e.defaultProviderId)
-			: null;
+	protected RootDescriptor getItem(Exchange e) {
+		if (e.defaultProviderId == 0L)
+			return null;
+		var map = Cache.getMatrixCache().getProviderMap();
+		return map != null
+				? map.getProvider(e.defaultProviderId)
+				: null;
 	}
 
 	@Override
 	protected String getText(RootDescriptor d) {
 		return d != null
-			? Labels.name(d)
-			: "";
+				? Labels.name(d)
+				: "";
 	}
 
 	@Override
@@ -78,14 +68,5 @@ class ProviderCombo extends ComboBoxCellModifier<Exchange, RootDescriptor> {
 				? ProviderType.of(d.type)
 				: ProviderType.PROCESS;
 		editor.setDirty(true);
-	}
-
-	private Set<Long> getProviderIds(Exchange e) {
-		if (e == null || e.flow == null)
-			return Collections.emptySet();
-		var dao = new FlowDao(db);
-		return e.isInput
-			? dao.getWhereOutput(e.flow.id)
-			: dao.getWhereInput(e.flow.id);
 	}
 }
