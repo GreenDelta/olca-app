33c5c75080fdfde9fd94f43fb24bb3d9091e8094
msrocka <michael.srocka@gmail.com>
2025-05-20 11:32:54 +0200

    better sorting of inventory results

--- olca-app/src/org/openlca/app/editors/results/ResultEditor.java
+++ olca-app/src/org/openlca/app/editors/results/ResultEditor.java
@@ -1,5 +1,8 @@
 package org.openlca.app.editors.results;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.ui.forms.IManagedForm;
@@ -10,6 +13,7 @@ import org.openlca.app.editors.ModelPage;
 import org.openlca.app.util.ErrorReporter;
 import org.openlca.app.util.Labels;
 import org.openlca.app.util.UI;
+import org.openlca.core.model.FlowResult;
 import org.openlca.core.model.Result;
 import org.openlca.util.Strings;
 
@@ -54,16 +58,61 @@ public class ResultEditor extends ModelEditor<Result> {
 			tk.adapt(sash);
 
 			new ImpactSection(editor).render(sash, tk);
-			editor.getModel().flowResults.sort((ri, rj) -> {
-				var ni = Labels.name(ri.flow);
-				var nj = Labels.name(rj.flow);
-				return Strings.compare(ni, nj);
-			});
+			var result = editor.getModel();
+			result.flowResults.sort(new FlowResultComparator(result));
+
 			FlowSection.forInputs(editor).render(sash, tk);
 			FlowSection.forOutputs(editor).render(sash, tk);
 
 			body.setFocus();
 			form.reflow(true);
+		}
+	}
+
+	private record FlowResultComparator(Result result)
+			implements Comparator<FlowResult> {
+
+		@Override
+		public int compare(FlowResult ri, FlowResult rj) {
+
+			// list the reference flow first
+			if (Objects.equals(result.referenceFlow, ri))
+				return -1;
+			if (Objects.equals(result.referenceFlow, rj))
+				return 1;
+
+			// check flow types
+			int ti = typeOf(ri);
+			int tj = typeOf(rj);
+			if (ti != tj)
+				return ti - tj;
+
+			// check the flow name
+			var ni = Labels.name(ri.flow);
+			var nj = Labels.name(rj.flow);
+			var c =  Strings.compare(ni, nj);
+			if (c != 0)
+				return c;
+
+			// check the category
+			var pi = ri.flow != null && ri.flow.category != null
+					? ri.flow.category.toPath()
+					: null;
+			var pj = rj.flow != null && rj.flow.category != null
+					? rj.flow.category.toPath()
+					: null;
+			return Strings.compare(pi, pj);
+		}
+
+		private int typeOf(FlowResult r) {
+			if (r.flow == null)
+				return -1;
+			return switch (r.flow.flowType) {
+				case PRODUCT_FLOW -> 0;
+				case WASTE_FLOW -> 1;
+				case ELEMENTARY_FLOW -> 2;
+				case null -> -1;
+			};
 		}
 	}
 }
