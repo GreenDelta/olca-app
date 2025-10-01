83459d19f638adf72c374f8cf61f2e63eece654a
msrocka <michael.srocka@gmail.com>
2025-10-01 16:53:49 +0200

    concept for coupled calc.

--- olca-app/src/org/openlca/app/editors/sd/interop/CoupledResult.java
+++ olca-app/src/org/openlca/app/editors/sd/interop/CoupledResult.java
@@ -1,4 +1,51 @@
 package org.openlca.app.editors.sd.interop;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openlca.core.matrix.index.ImpactIndex;
+import org.openlca.core.results.LcaResult;
+import org.openlca.sd.eqn.Id;
+import org.openlca.sd.eqn.SimulationState;
+import org.openlca.sd.eqn.Var;
+
 public class CoupledResult {
+
+	private final Map<Id, Var> vars = new HashMap<>();
+	private Map<Id, List<Double>> varResults = new HashMap<>();
+	private final List<Double> time = new ArrayList<>();
+
+	private ImpactIndex impactIdx;
+	private List<Double>[] impactResults;
+
+	void add(SimulationState state) {
+		if (state == null)
+			return;
+	}
+
+	void add(List<LcaResult> rs) {
+		if (rs == null || rs.isEmpty())
+			return;
+
+		if (impactIdx == null) {
+			for (var r : rs) {
+				if (r.hasImpacts()) {
+					impactIdx = r.impactIndex();
+					break;
+				}
+			}
+		}
+		if (impactIdx == null)
+			return;
+
+		for (var r : rs) {
+			if (!r.hasImpacts())
+				continue;
+			if (impactIdx == null) {
+				impactIdx = r.impactIndex();
+			}
+		}
+	}
 }
--- olca-app/src/org/openlca/app/editors/sd/interop/CoupledSimulator.java
+++ olca-app/src/org/openlca/app/editors/sd/interop/CoupledSimulator.java
@@ -1,9 +1,18 @@
 package org.openlca.app.editors.sd.interop;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
+import org.openlca.app.App;
+import org.openlca.app.db.Database;
+import org.openlca.app.db.Libraries;
+import org.openlca.core.math.SystemCalculator;
 import org.openlca.core.model.CalculationSetup;
+import org.openlca.core.model.ParameterRedef;
+import org.openlca.sd.eqn.SimulationState;
 import org.openlca.sd.eqn.Simulator;
+import org.openlca.sd.eqn.cells.NumCell;
 import org.openlca.sd.xmile.Xmile;
 import org.openlca.util.Res;
 
@@ -11,10 +20,14 @@ public class CoupledSimulator {
 
 	private final Simulator simulator;
 	private final SimulationSetup setup;
+	private final SystemCalculator calculator;
 
-	private CoupledSimulator(Simulator simulator, SimulationSetup setup) {
+	private CoupledSimulator(
+			Simulator simulator, SimulationSetup setup, SystemCalculator calculator
+	) {
 		this.simulator = simulator;
 		this.setup = setup;
+		this.calculator = calculator;
 	}
 
 	public static Res<CoupledSimulator> of(
@@ -23,25 +36,65 @@ public class CoupledSimulator {
 		var simulator = Simulator.of(xmile);
 		if (simulator.hasError())
 			return simulator.wrapError("failed to create simulator");
-		return Res.of(new CoupledSimulator(simulator.value(), setup));
+
+		var calculator = new SystemCalculator(Database.get())
+				.withSolver(App.getSolver());
+		Libraries.readersForCalculation()
+				.ifPresent(calculator::withLibraries);
+		return Res.of(new CoupledSimulator(simulator.value(), setup, calculator));
 	}
 
 	public void forEach(Consumer<Res<CoupledResult>> fn) {
 		simulator.forEach(res -> {
 			if (res.hasError()) {
-				fn.accept(res.wrapError("simulation error"));
+				fn.accept(res.wrapError("Simulation error"));
 				return;
 			}
+			var simState = res.value();
 
 			for (var b : setup.systemBindings()) {
+				var params = paramsOf(simState, b);
+				if (params.hasError()) {
+					fn.accept(params.wrapError("Variable binding error"));
+					return;
+				}
+
 				var calcSetup = CalculationSetup.of(b.system())
+						.withParameters(params.value())
 						.withAllocation(b.allocation())
 						.withImpactMethod(setup.method())
 						.withAmount(b.amount()); // TODO: the amount can be bound to a var
 
+				try {
+					var result = calculator.calculate(calcSetup);
+				} catch (Exception e) {
+					fn.accept(Res.error(
+							"Calculation of system failed: " + b.system().name, e));
+					return;
+				}
 			}
-
 		});
 	}
 
+	private Res<List<ParameterRedef>> paramsOf(
+			SimulationState simState, SystemBinding binding
+	) {
+		var params = new ArrayList<ParameterRedef>();
+		for (var vb : binding.varBindings()) {
+			if (vb.varId() == null || vb.parameter() == null)
+				continue;
+			var cell = simState.valueOf(vb.varId()).orElse(null);
+			if (cell == null)
+				return Res.error("Variable not found: " + vb.varId());
+			if (!(cell instanceof NumCell(double num))) {
+				return Res.error("Variable does not evaluate to a number: "
+						+ vb.varId());
+			}
+
+			var param = vb.parameter().copy();
+			param.value = num;
+			params.add(param);
+		}
+		return Res.of(params);
+	}
 }
