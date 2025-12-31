a00548e7e5b702f0fbdf196776b626bc8e5aa4dc
trbar <96570778+trbar@users.noreply.github.com>
2025-12-31 12:36:29 +0100

    [fix]: add relevant eclipse bug issue

--- olca-app/src/org/openlca/app/search/ToolbarSpacerHandler.java
+++ olca-app/src/org/openlca/app/search/ToolbarSpacerHandler.java
@@ -8,6 +8,7 @@ import org.eclipse.core.commands.ExecutionException;
  * A dummy handler for a spacer command in the search toolbar. This is used to
  * fix alignment issues on Windows when a ControlContribution is the first
  * element in a toolbar at high zoom levels (>200%).
+ * https://bugs.eclipse.org/bugs/show_bug.cgi?id=465732#c5
  */
 public class ToolbarSpacerHandler extends AbstractHandler {
 
