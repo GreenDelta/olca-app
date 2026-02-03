8a4779c9b497edb0d43d0b05f2dd1a552086e290
msrocka <michael.srocka@gmail.com>
2026-02-03 08:42:16 +0100

    this should fix the script init issue
    at least it works on Linux now

--- olca-app-html/src/python/python.tsx
+++ olca-app-html/src/python/python.tsx
@@ -3,7 +3,50 @@ import { render } from "react-dom";
 import { EditorView } from "@codemirror/view";
 import { EditorState } from "@codemirror/state";
 import { extensionsOf, Theme } from "./codemirror-extensions";
-import { initBrowserAPI } from "./browser-api";
+
+/// Extend the Window object; these methods are used for the communication with
+/// the Java side.
+declare global {
+  interface Window {
+
+    /// Returns the current editor content, the script.
+    getContent?: () => string;
+
+    /// Set the editor content.
+    setContent?: (code: string) => void;
+
+    /// A listener that is called when the content changed.
+    _onChange?: (code: string) => void;
+
+    /// A listener that is called when the script should be executed.
+    _onRun?: (script: string) => void;
+
+    /// A listener that is called when the script should be saved.
+    _onSave?: () => void;
+  }
+}
+
+/// `setContent` could be called before React is ready. Thus, we register
+/// the content methods temporarily here to capture the initial content. They
+/// are then replaced later when the EditorView was initialized.
+let _content: string | null = null;
+window.setContent = (code: string) => {
+  _content = code;
+};
+window.getContent = () => _content || "";
+
+const initBrowserAPI = (view: EditorView) => {
+  const initialContent = window.getContent ? window.getContent() : null;
+  window.getContent = () => view.state.doc.toString();
+  window.setContent = (code: string) =>
+    view.dispatch({
+      changes: { from: 0, to: view.state.doc.length, insert: code },
+    });
+  if (initialContent) {
+    window.setContent(initialContent);
+  }
+};
+
 
 const PythonEditor = ({ theme }: { theme: Theme }) => {
   const editorRef = useRef<HTMLDivElement>(null);
