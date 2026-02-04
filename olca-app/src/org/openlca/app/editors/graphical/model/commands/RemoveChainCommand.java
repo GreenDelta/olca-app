30aad31ddae841f555ac56585a8b172e9c91975b
msrocka <michael.srocka@gmail.com>
2026-02-04 16:09:41 +0100

    cleanup only nodes that are model-reachable

--- olca-app/src/org/openlca/app/editors/graphical/model/commands/RemoveChainCommand.java
+++ olca-app/src/org/openlca/app/editors/graphical/model/commands/RemoveChainCommand.java
@@ -4,6 +4,7 @@ import static org.openlca.app.components.graphics.model.Component.*;
 
 import java.util.ArrayDeque;
 import java.util.HashSet;
+import java.util.Set;
 
 import org.eclipse.gef.commands.Command;
 import org.openlca.app.App;
@@ -85,40 +86,45 @@ public class RemoveChainCommand extends Command {
 	}
 
 	/// Removes visual nodes from the graph that are no longer visually
-	/// connected to the reference process.
+	/// connected to the reference process, but only if they have a real
+	/// chain of process links to the reference process. Nodes that were
+	/// manually added without such a chain should remain visible.
 	private void removeDisconnectedNodes() {
 		var ref = graph.getReferenceNode();
 		if (ref == null)
 			return;
 
-		// collect all reachable nodes
-		var reachable = new HashSet<Node>();
+		// collect all visually reachable nodes from the reference
+		var visReachable = new HashSet<Node>();
 		var queue = new ArrayDeque<Node>();
 		queue.add(ref);
-		reachable.add(ref);
+		visReachable.add(ref);
 		while (!queue.isEmpty()) {
 			var next = queue.poll();
 			for (var l : next.getAllLinks()) {
 				if (!(l instanceof GraphLink link)) continue;
 				var source = link.getSourceNode();
-				if (source != null && reachable.add(source)) {
+				if (source != null && visReachable.add(source)) {
 					queue.add(source);
 				}
 				var target = link.getTargetNode();
-				if (target != null && reachable.add(target)) {
+				if (target != null && visReachable.add(target)) {
 					queue.add(target);
 				}
 			}
 		}
 
-		// remove and unlink nodes that are not reachable
+		// remove nodes that are not visually reachable but are
+		// model-reachable, means they can be reached by expanding
+		// a chain of nodes starting from the reference process
+		var modReachable = modelReachableOf(ref.descriptor.id);
 		var removals = new HashSet<Node>();
 		for (var node : graph.getNodes()) {
-			if (!reachable.contains(node)) {
+			if (!visReachable.contains(node)
+					&& modReachable.contains(node.descriptor.id)) {
 				removals.add(node);
 			}
 		}
-
 		for (var node : removals) {
 			for (var l : node.getAllLinks()) {
 				if (!(l instanceof GraphLink link)) continue;
@@ -126,5 +132,26 @@ public class RemoveChainCommand extends Command {
 			}
 			graph.removeChildQuietly(node);
 		}
+	}
+
+	/// Collects all processes that can be reached from the given
+	/// reference process.
+	private Set<Long> modelReachableOf(long refId) {
+		var linked = new HashSet<Long>();
+		var queue = new ArrayDeque<Long>();
+		queue.add(refId);
+		linked.add(refId);
+		while (!queue.isEmpty()) {
+			var next = queue.poll();
+			for (var link : graph.linkSearch.getAllLinks(next)) {
+				if (linked.add(link.providerId)) {
+					queue.add(link.providerId);
+				}
+				if (linked.add(link.processId)) {
+					queue.add(link.processId);
+				}
+			}
+		}
+		return linked;
 	}
 }
