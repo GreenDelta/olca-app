986813f6b30adb9beea8e725053de1f7bab4a466
msrocka <michael.srocka@gmail.com>
2026-03-10 10:18:32 +0100

    nicer panel layout

--- olca-app/src/org/openlca/app/editors/sd/editor/graph/actions/StockFlowPanel.java
+++ olca-app/src/org/openlca/app/editors/sd/editor/graph/actions/StockFlowPanel.java
@@ -3,19 +3,18 @@ package org.openlca.app.editors.sd.editor.graph.actions;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.openlca.app.M;
 import org.openlca.app.rcp.images.Icon;
 import org.openlca.app.util.Controls;
-import org.openlca.app.util.Input;
 import org.openlca.app.util.UI;
 import org.openlca.sd.model.Id;
 import org.openlca.sd.model.Stock;
 
 class StockFlowPanel {
 
-	private final List inList;
-	private final List outList;
 	private final Stock stock;
 
 	StockFlowPanel(Composite parent, FormToolkit tk, Stock stock) {
@@ -24,61 +23,63 @@ class StockFlowPanel {
 		UI.filler(parent, tk);
 
 		var comp = UI.composite(parent, tk);
-		UI.gridLayout(comp, 2);
+		UI.gridLayout(comp, 2, 5, 0);
 		UI.gridData(comp, true, false);
 
-		createHeader(comp, tk, "Input flows");
-		createHeader(comp, tk, "Output flows");
+		UI.label(comp, tk, "Input flows");
+		UI.label(comp, tk, "Output flows");
 
-		inList = new List(comp, SWT.BORDER | SWT.V_SCROLL | SWT.SINGLE);
-		UI.gridData(inList, true, false).heightHint = 40;
-		updateList(inList, stock.inFlows());
+		ListBox.create(comp, stock.inFlows());
+		ListBox.create(comp, stock.outFlows());
 
-		outList = new List(comp, SWT.BORDER | SWT.V_SCROLL | SWT.SINGLE);
-		UI.gridData(outList, true, false).heightHint = 40;
-		updateList(outList, stock.inFlows());
-
-		createActions(comp, tk, inList, stock.inFlows());
-		createActions(comp, tk, outList, stock.outFlows());
 	}
 
-	private void createHeader(Composite comp, FormToolkit tk, String text) {
-		var label = tk.createLabel(comp, text, SWT.NONE);
-		UI.gridData(label, true, false);
-	}
+	private record ListBox(
+		List widget, java.util.List<Id> data
+	) {
 
-	private void createActions(Composite comp, FormToolkit tk, List list, java.util.List<Id> data) {
-		var bar = UI.composite(comp, tk);
-		UI.gridLayout(bar, 2, 0, 0);
-		UI.gridData(bar, true, false);
+		static void create(Composite comp, java.util.List<Id> data) {
 
-		var add = tk.createButton(bar, null, SWT.PUSH);
-		add.setImage(Icon.ADD.get());
-		add.setToolTipText(M.Add);
-		Controls.onSelect(add, $ -> {
-			var name = Input.promptString("Add flow", "Flow name", "");
-			if (name != null && !name.isBlank()) {
-				var id = Id.of(name);
-				if (!data.contains(id)) {
-					data.add(id);
-					updateList(list, data);
+			var widget = new List(comp, SWT.BORDER | SWT.V_SCROLL | SWT.SINGLE);
+			var box = new ListBox(widget, data);
+			var gd = UI.gridData(widget, true, false);
+			gd.widthHint = 1;
+			gd.heightHint = 60;
+
+			var menu = new Menu(widget);
+			widget.setMenu(menu);
+
+			var addItem = new MenuItem(menu, SWT.NONE);
+			addItem.setText(M.Add);
+			addItem.setImage(Icon.ADD.get());
+			Controls.onSelect(addItem, $ -> {
+				// TODO -> open a dialog.
+			});
+
+			var delItem = new MenuItem(menu, SWT.NONE);
+			delItem.setText(M.Remove);
+			delItem.setImage(Icon.DELETE.get());
+			Controls.onSelect(delItem, $ -> {
+				var idx = widget.getSelectionIndex();
+				if (idx < 0) return;
+				var item = widget.getItem(idx);
+				if (item != null) {
+					var flow = Id.of(item);
+					data.removeIf(flow::equals);
+					box.updateItems();
 				}
-			}
-		});
+			});
 
-		var remove = tk.createButton(bar, null, SWT.PUSH);
-		remove.setImage(Icon.DELETE.get());
-		remove.setToolTipText(M.Remove);
-		Controls.onSelect(remove, $ -> {
-			int idx = list.getSelectionIndex();
-			if (idx >= 0) {
-				data.remove(idx);
-				updateList(list, data);
-			}
-		});
+			box.updateItems();
+		}
+
+		private void updateItems() {
+			var items = data.stream()
+				.map(Id::label)
+				.sorted()
+				.toArray(String[]::new);
+			widget.setItems(items);
+		}
 	}
 
-	private void updateList(List list, java.util.List<Id> data) {
-		list.setItems(data.stream().map(Id::label).toArray(String[]::new));
-	}
 }
